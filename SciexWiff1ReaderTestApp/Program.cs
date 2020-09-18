5a00e8d297a208723dfbd74f8fc26ab1345b441f
Yuki Matsuzawa <yuki.matsuzawa@riken.jp>
2020-09-18 18:04:14 +0900

    added wiff1 reader test

--- SciexWiff1ReaderTestApp/Program.cs
+++ SciexWiff1ReaderTestApp/Program.cs
@@ -1,14 +1,43 @@
 ﻿using CompMs.RawDataHandler.Wiff1Net4;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
 namespace SciexWiff1ReaderTestApp {
     class Program {
         static void Main(string[] args) {
-            new Wiff1ReaderDotNet4().ReadSciexWiff1Data(@"D:\msdial_demofiles\Wiff1Swath\Posi_Swath_Chlamydomonas_1.wiff",
-                0, 0.0, out string errorString);
+            /*
+            var spectrum = new Wiff1ReaderDotNet4().ReadSciexWiff1Data(@"\\mtbdt\Mtb_info\data\MS-DIAL demo files\Raw\HILIC-Pos-SWATH-25Da.wiff", 0, 1, out string errorString);
+            foreach (var spec in spectrum) {
+                Console.WriteLine($"{spec.Index} {spec.ScanStartTime} {spec.BasePeakMz} {spec.BasePeakIntensity}");
+            }
+            */
+            var spectrum = new Wiff1ReaderDotNet4().ReadSciexWiff1Data(args[0], int.Parse(args[1]), double.Parse(args[2]), out string errorString);
+            Send(spectrum, errorString);
+        }
+
+        static void Send(List<Wiff1RawSpectrum> spectrum, string errorString) {
+            using (var stream = new NamedPipeServerStream("Wiff1Reader", PipeDirection.Out, 1)) {
+
+                byte[] buffer;
+                IFormatter formatter = new BinaryFormatter();
+                using (var memory = new MemoryStream()) {
+                    formatter.Serialize(memory, spectrum);
+                    buffer = memory.ToArray();
+                }
+
+                stream.WaitForConnection();
+                using (var writer = new BinaryWriter(stream)) {
+                    writer.Write(buffer.Length);
+                    writer.Write(buffer);
+                    writer.Write(errorString);
+                }
+            }
         }
     }
 }
---
+++ WiffReaderClientTestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using CompMs.RawDataHandler.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiffReaderClientTestApp
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            using (var rawDataAccess = new RawDataAccess(@"\\mtbdt\Mtb_info\data\MS-DIAL demo files\Raw\HILIC-Pos-SWATH-25Da.wiff", 0, false)) {
+                rawDataAccess.DataDump(@"\\mtbdt\Mtb_info\data\MS-DIAL demo files\Raw\HILIC-Pos-SWATH-25Da.wiff");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
