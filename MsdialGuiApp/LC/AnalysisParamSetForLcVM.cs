447b7003c2bc74d971172802726f8c989dd6fdad
Yuki Matsuzawa <yuki.matsuzawa@riken.jp>
2020-11-20 09:50:14 +0900

    Add LipidDbSetWindow for identification

--- MsdialGuiApp/LC/AnalysisParamSetForLcVM.cs
+++ MsdialGuiApp/LC/AnalysisParamSetForLcVM.cs
@@ -1,20 +1,25 @@
-﻿using CompMs.Common.DataObj.Property;
+﻿using CompMs.App.Msdial.Common;
+using CompMs.App.Msdial.Lipidomics;
+using CompMs.Common.DataObj.Property;
 using CompMs.Common.Parameter;
 using CompMs.Common.Query;
 using CompMs.CommonMVVM;
 using CompMs.CommonMVVM.Common;
 using CompMs.MsdialCore.DataObj;
 using CompMs.MsdialLcmsApi.Parameter;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
+using System.Windows.Input;
 
 namespace CompMs.App.Msdial.LC
 {
-    class AnalysisParamSetForLcVM : ViewModelBase
-    {
+    class AnalysisParamSetForLcVM : ViewModelBase {
         #region Property
         public MsdialLcmsParameterVM Param {
             get => param;
@@ -41,11 +46,6 @@ namespace CompMs.App.Msdial.LC
             set => SetProperty(ref analysisFiles, value);
         }
 
-        public string TargetFormulaLibraryFilePath {
-            get => targetFormulaLibraryFilePath;
-            set => SetProperty(ref targetFormulaLibraryFilePath, value);
-        }
-
         public ObservableCollection<MzSearchQueryVM> ExcludedMassList {
             get => excludedMassList;
             set => SetProperty(ref excludedMassList, value);
@@ -63,7 +63,6 @@ namespace CompMs.App.Msdial.LC
         MsdialLcmsParameterVM param;
         MsRefSearchParameterBaseVM mspSearchParam, textDbSearchParam;
         string alignmentResultFileName;
-        string targetFormulaLibraryFilePath;
         ObservableCollection<AnalysisFileBean> analysisFiles;
         ObservableCollection<MzSearchQueryVM> excludedMassList;
         ObservableCollection<AdductIonVM> searchedAdductIons;
@@ -81,7 +80,7 @@ namespace CompMs.App.Msdial.LC
             AnalysisFiles = new ObservableCollection<AnalysisFileBean>(files);
 
             ExcludedMassList = new ObservableCollection<MzSearchQueryVM>(
-                parameter.ExcludedMassList?.Select(query => new MzSearchQueryVM { Mass=query.Mass, Tolerance=query.MassTolerance})
+                parameter.ExcludedMassList?.Select(query => new MzSearchQueryVM { Mass = query.Mass, Tolerance = query.MassTolerance })
                          .Concat(Enumerable.Repeat<MzSearchQueryVM>(null, 200).Select(_ => new MzSearchQueryVM()))
             );
             SearchedAdductIons = new ObservableCollection<AdductIonVM>(parameter.SearchedAdductIons?.Select(ion => new AdductIonVM(ion)));
@@ -142,12 +141,125 @@ namespace CompMs.App.Msdial.LC
                 SearchedAdductIons.Add(new AdductIonVM(vm.AdductIon));
             }
         }
+
+        public ICommand MspCommand {
+            get {
+                if (source.TargetOmics == CompMs.Common.Enum.TargetOmics.Lipidomics)
+                    return LipidDBSetCommand;
+                return MspFileSelectCommand;
+            }
+        }
+
+        public DelegateCommand<Window> LipidDBSetCommand {
+            get => lipidDBSetCommand ?? (lipidDBSetCommand = new DelegateCommand<Window>(LipidDBSet));
+        }
+        private DelegateCommand<Window> lipidDBSetCommand;
+
+        private void LipidDBSet(Window owner) {
+            var mainDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (Directory.GetFiles(mainDirectory, "*." + SaveFileFormat.lbm + "*", SearchOption.TopDirectoryOnly).Length != 1)
+            {
+                MessageBox.Show("There is no LBM file or several LBM files are existed in this application folder. Please see the tutorial.",
+                                "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            var vm = new LipidDbSetVM(source.LipidQueryContainer, source.IonMode);
+            var window = new LipidDbSetWindow
+            {
+                Owner = owner,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                DataContext = vm,
+            };
+            window.ShowDialog();
+        }
+
+        public DelegateCommand MspFileSelectCommand {
+            get => mspFileSelectCommand ?? (mspFileSelectCommand = new DelegateCommand(MspFileSelect));
+        }
+        private DelegateCommand mspFileSelectCommand;
+
+        private void MspFileSelect() {
+            var ofd = new OpenFileDialog
+            {
+                Title = "Import a library file",
+                Filter = "MSP file(*.msp)|*.msp*",
+                RestoreDirectory = true,
+                Multiselect = false,
+            };
+
+            if (ofd.ShowDialog() == true) {
+                Param.MspFilePath = ofd.FileName;
+            }
+        }
+
+        public DelegateCommand TextDBFileSelectCommand {
+            get => textDBFileSelectCommand ?? (textDBFileSelectCommand = new DelegateCommand(TextDBFileSelect));
+        }
+        private DelegateCommand textDBFileSelectCommand;
+
+        private void TextDBFileSelect() {
+            var ofd = new OpenFileDialog
+            {
+                Title = "Import a library file for post identification",
+                Filter = "Text file(*.txt)|*.txt;",
+                RestoreDirectory = true,
+                Multiselect = false,
+            };
+
+            if (ofd.ShowDialog() == true) {
+                Param.TextDBFilePath = ofd.FileName;
+            }
+        }
+
+        public DelegateCommand IsotopeTextDBFileSelectCommand {
+            get => isotopeTextDBFileSelectCommand ?? (isotopeTextDBFileSelectCommand = new DelegateCommand(IsotopeTextDBFileSelect));
+        }
+        private DelegateCommand isotopeTextDBFileSelectCommand;
+
+        private void IsotopeTextDBFileSelect() {
+            var ofd = new OpenFileDialog
+            {
+                Title = "Import target formulas library",
+                Filter = "Text file(*.txt)|*.txt;",
+                RestoreDirectory = true,
+                Multiselect = false,
+            };
+
+            if (ofd.ShowDialog() == true) {
+                Param.IsotopeTextDBFilePath = ofd.FileName;
+            }
+        }
         #endregion
     }
 
     class MsdialLcmsParameterVM : DynamicViewModelBase<MsdialLcmsParameter>
     {
         public MsdialLcmsParameterVM(MsdialLcmsParameter innerModel) : base(innerModel) { }
+
+        public string MspFilePath {
+            get => InnerModel.MspFilePath;
+            set {
+                if (InnerModel.MspFilePath == value) return;
+                InnerModel.MspFilePath = value;
+                OnPropertyChanged(nameof(MspFilePath));
+            }
+        }
+        public string TextDBFilePath {
+            get => InnerModel.TextDBFilePath;
+            set {
+                if (InnerModel.TextDBFilePath == value) return;
+                InnerModel.TextDBFilePath = value;
+                OnPropertyChanged(nameof(TextDBFilePath));
+            }
+        }
+        public string IsotopeTextDBFilePath {
+            get => InnerModel.IsotopeTextDBFilePath;
+            set {
+                if (InnerModel.IsotopeTextDBFilePath == value) return;
+                InnerModel.IsotopeTextDBFilePath = value;
+                OnPropertyChanged(nameof(IsotopeTextDBFilePath));
+            }
+        }
     }
 
     class MsRefSearchParameterBaseVM : DynamicViewModelBase<MsRefSearchParameterBase>
