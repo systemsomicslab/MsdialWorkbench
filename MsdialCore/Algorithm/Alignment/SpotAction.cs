f58d7a2d77db2488d3be684d4ea1cbcd5fefc2b2
Yuki Matsuzawa <yuki.matsuzawa@riken.jp>
2021-01-04 23:52:13 +0900

    Add ISpotAction interface.
    used for AlignmentSpotProperty

---
+++ MsdialCore/Algorithm/Alignment/SpotAction.cs
@@ -0,0 +1,20 @@
+﻿using CompMs.MsdialCore.DataObj;
+using CompMs.MsdialCore.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompMs.MsdialCore.Algorithm.Alignment
+{
+    public class RepresentativeSetter : ISpotAction
+    {
+        public void Process(IEnumerable<AlignmentSpotProperty> spots) {
+            foreach (var spot in spots) {
+                foreach (var child in spot.AlignmentDriftSpotFeatures) {
+                    DataObjConverter.SetRepresentativeProperty(child);
+                }
+                DataObjConverter.SetRepresentativeProperty(spot);
+            }
+        }
+    }
+}
