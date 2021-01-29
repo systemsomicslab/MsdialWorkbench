6a983eec33524f9f336b201acca69838494136f6
Yuki Matsuzawa <yuki.matsuzawa@riken.jp>
2021-01-29 11:08:41 +0900

    Implement DimsMspAnnotator.
    Annotation class for dims method and msp database.

--- MsdialCore/Algorithm/Annotation/IAnnotator.cs
+++ MsdialCore/Algorithm/Annotation/IAnnotator.cs
@@ -10,6 +10,7 @@ namespace CompMs.MsdialCore.Algorithm.Annotation
     public interface IAnnotator
     {
         MsScanMatchResult Annotate(IMSScanProperty scan, IReadOnlyList<IsotopicPeak> isotopes, MsRefSearchParameterBase parameter = null);
+        List<MsScanMatchResult> FindCandidates(IMSScanProperty scan, IReadOnlyList<IsotopicPeak> isotopes, MsRefSearchParameterBase parameter = null);
         MsScanMatchResult CalculateScore(IMSScanProperty scan, IReadOnlyList<IsotopicPeak> scanIsotopes, MoleculeMsReference reference, IReadOnlyList<IsotopicPeak> referenceIsotopes, MsRefSearchParameterBase parameter = null);
         MoleculeMsReference Refer(MsScanMatchResult result);
         List<MoleculeMsReference> Search(IMSScanProperty scan, MsRefSearchParameterBase parameter = null);
