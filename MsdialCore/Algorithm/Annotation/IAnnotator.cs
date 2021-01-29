be4ffa7cf0d22a81ac03d9b1baa605684d3bd5e2
Yuki Matsuzawa <yuki.matsuzawa@riken.jp>
2021-01-29 11:08:00 +0900

    Add IAnnotator
    Interface for peak annotation.

---
+++ MsdialCore/Algorithm/Annotation/IAnnotator.cs
@@ -0,0 +1,18 @@
+﻿using CompMs.Common.Components;
+using CompMs.Common.DataObj.Property;
+using CompMs.Common.DataObj.Result;
+using CompMs.Common.Interfaces;
+using CompMs.Common.Parameter;
+using System.Collections.Generic;
+
+namespace CompMs.MsdialCore.Algorithm.Annotation
+{
+    public interface IAnnotator
+    {
+        MsScanMatchResult Annotate(IMSScanProperty scan, IReadOnlyList<IsotopicPeak> isotopes, MsRefSearchParameterBase parameter = null);
+        MsScanMatchResult CalculateScore(IMSScanProperty scan, IReadOnlyList<IsotopicPeak> scanIsotopes, MoleculeMsReference reference, IReadOnlyList<IsotopicPeak> referenceIsotopes, MsRefSearchParameterBase parameter = null);
+        MoleculeMsReference Refer(MsScanMatchResult result);
+        List<MoleculeMsReference> Search(IMSScanProperty scan, MsRefSearchParameterBase parameter = null);
+        void Validate(MsScanMatchResult result, IMSScanProperty scan, IReadOnlyList<IsotopicPeak> scanIsotopes, MoleculeMsReference reference, IReadOnlyList<IsotopicPeak> referenceIsotopes, MsRefSearchParameterBase parameter = null);
+    }
+}
