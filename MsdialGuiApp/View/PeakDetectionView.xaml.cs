9ecb2d32f1e9e17b0662c706c6ee0a412b53dd28
Yuki Matsuzawa <yuki.matsuzawa@riken.jp>
2021-02-10 16:43:16 +0900

    Move PeakDetectionView.xaml

--- MsdialGuiApp/View/PeakDetectionView.xaml.cs
+++ MsdialGuiApp/View/PeakDetectionView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CompMs.App.Msdial.LC
+namespace CompMs.App.Msdial.View
 {
     /// <summary>
     /// Interaction logic for PeakDetectionView.xaml
