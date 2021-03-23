e9edf700286ad248fe6ea1d63f9859f9733d209b
hiroshi.tsugawa <hiroshi.tsugawa@riken.jp>
2021-03-23 17:09:52 +0900

    finalized raw data parser for the upload of 4.60

--- MsDial/Properties/Resources.Designer.cs
+++ MsDial/Properties/Resources.Designer.cs
@@ -88,7 +88,7 @@ namespace Rfx.Riken.OsakaUniv.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to MS-DIAL ver. 4.60.
+        ///   Looks up a localized string similar to MS-DIAL ver. 4.60-dev.
         /// </summary>
         internal static string VERSION {
             get {
