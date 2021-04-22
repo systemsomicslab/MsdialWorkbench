09a04bc9623770ef8b64b8ba8af4450eec4d6a26
Yuki Matsuzawa <yuki.matsuzawa@riken.jp>
2021-04-22 14:36:58 +0900

    Add IndexOf extention method for IReadOnlyList<T>

---
+++ CommonStandard/Extension/ReadOnlyListExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompMs.Common.Extension
+{
+    public static class ReadOnlyListExtension
+    {
+        public static int IndexOf<T>(this IReadOnlyList<T> xs, T item) {
+            return IndexOfCore(xs, item, 0, xs.Count);
+        }
+
+        public static int IndexOf<T>(this IReadOnlyList<T> xs, T item, int index) {
+            return IndexOfCore(xs, item, index, xs.Count);
+        }
+
+        public static int IndexOf<T>(this IReadOnlyList<T> xs, T item, int index, int count) {
+            return IndexOfCore(xs, item, index, Math.Min(xs.Count, index + count));
+        }
+
+        private static int IndexOfCore<T>(IReadOnlyList<T> xs, T item, int start, int end) {
+            for (int i = start; i < end; ++i) {
+                if (Equals(xs[i], item)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ CommonStandardTests/Extension/ReadOnlyListExtensionTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CompMs.Common.Extension;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompMs.Common.Extension.Tests
+{
+    [TestClass()]
+    public class ReadOnlyListExtensionTests
+    {
+        [TestMethod()]
+        public void IndexOfTest() {
+            List<string> actuals = new List<string>();
+            IReadOnlyList<string> expected = actuals;
+
+            actuals.Add("Tyrannosaurus");
+            actuals.Add("Amargasaurus");
+            actuals.Add("Mamenchisaurus");
+            actuals.Add("Brachiosaurus");
+            actuals.Add("Deinonychus");
+            actuals.Add("Tyrannosaurus");
+            actuals.Add("Compsognathus");
+
+            Assert.AreEqual(actuals.IndexOf("Tyrannosaurus"), expected.IndexOf("Tyrannosaurus"));
+            Assert.AreEqual(actuals.IndexOf("Tyrannosaurus", 3), expected.IndexOf("Tyrannosaurus", 3));
+            Assert.AreEqual(actuals.IndexOf("Tyrannosaurus", 2, 2), expected.IndexOf("Tyrannosaurus", 2, 2));
+        }
+    }
+}
