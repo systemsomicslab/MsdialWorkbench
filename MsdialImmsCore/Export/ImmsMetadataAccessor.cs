638bb00db0e89c529af337a771924196935cb633
Yuki Matsuzawa <yuki.matsuzawa@riken.jp>
2021-06-03 14:57:36 +0900

    Add AlignmentExport class for imms.

---
+++ MsdialImmsCore/Export/ImmsMetadataAccessor.cs
@@ -0,0 +1,81 @@
+﻿using CompMs.Common.Components;
+using CompMs.Common.DataObj.Result;
+using CompMs.MsdialCore.Algorithm.Annotation;
+using CompMs.MsdialCore.DataObj;
+using CompMs.MsdialCore.Export;
+using CompMs.MsdialCore.MSDec;
+using CompMs.MsdialCore.Parameter;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompMs.MsdialImmsCore.Export
+{
+    public class ImmsMetadataAccessor : BaseMetadataAccessor
+    {
+        public ImmsMetadataAccessor(IMatchResultRefer refer, ParameterBase parameter) : base(refer, parameter) {
+
+        }
+
+        protected override string[] GetHeadersCore() {
+            return new string[] {
+                "Alignment ID" ,
+                "Average Mz",
+                "Average mobility",
+                "Average CCS",
+                "Metabolite name",
+                "Adduct type",
+                "Post curation result",
+                "Fill %",
+                "MS/MS assigned",
+                "Reference CCS",
+                "Reference m/z",
+                "Formula",
+                "Ontology",
+                "INCHIKEY",
+                "SMILES",
+                "Annotation tag (VS1.0)" ,
+                "CCS matched",
+                "m/z matched",
+                "MS/MS matched",
+                "Comment",
+                "Manually modified for quantification",
+                "Manually modified for annotation" ,
+                "Isotope tracking parent ID",
+                "Isotope tracking weight number",
+                "CCS similarity",
+                "m/z similarity",
+                "Simple dot product",
+                "Weighted dot product",
+                "Reverse dot product",
+                "Matched peaks count",
+                "Matched peaks percentage",
+                "Total score",
+                "S/N average",
+                "Spectrum reference file name",
+                "MS1 isotopic spectrum",
+                "MS/MS spectrum",
+            };
+        }
+
+        protected override Dictionary<string, string> GetContentCore(
+            AlignmentSpotProperty spot,
+            MSDecResult msdec,
+            MoleculeMsReference reference,
+            MsScanMatchResult matchResult) {
+
+            var content = base.GetContentCore(spot, msdec, reference, matchResult);
+            content.Add("Average Mz", spot.MassCenter.ToString("F5"));
+            content.Add("Average mobility", spot.TimesCenter.Drift.Value.ToString("F3"));
+            content.Add("Average CCS",  spot.CollisionCrossSection <= 0 ?
+                "null" : spot.CollisionCrossSection.ToString("F3"));
+            content.Add("Reference CCS", ValueOrNull(reference?.CollisionCrossSection.ToString("F3")));
+            content.Add("Reference m/z", ValueOrNull(reference?.PrecursorMz.ToString("F5")));
+            content.Add("CCS matched", (matchResult?.IsCcsMatch ?? false).ToString());
+            content.Add("m/z matched", (matchResult?.IsPrecursorMzMatch ?? false).ToString());
+            content.Add("MS/MS matched", (matchResult?.IsSpectrumMatch ?? false).ToString());
+            content.Add("CCS similarity", ValueOrNull(matchResult?.CcsSimilarity, "F2"));
+            return content;
+        }
+    }
+}
---
+++ MsdialImmsCoreTests/Export/ImmsAlignmentExporterTests.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CompMs.MsdialImmsCore.Export;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CompMs.Common.MessagePack;
+using CompMs.MsdialCore.Parser;
+using CompMs.MsdialCore.DataObj;
+using CompMs.MsdialCore.Export;
+using System.IO;
+using System.Linq;
+using CompMs.MsdialCore.MSDec;
+using CompMs.Common.Components;
+using CompMs.Common.Enum;
+using CompMs.MsdialCore.Parameter;
+using CompMs.MsdialCore.Algorithm.Annotation;
+
+namespace CompMs.MsdialImmsCore.Export.Tests
+{
+    [TestClass()]
+    public class ImmsAlignmentExporterTests
+    {
+        [TestMethod()]
+        public void ExportTest() {
+            // prepare();
+            var datafile = @"C:\Users\YUKI MATSUZAWA\works\msdialworkbench\MsdialImmsCoreTests\Resources\input_data1";
+            var expectedfile = @"C:\Users\YUKI MATSUZAWA\works\msdialworkbench\MsdialImmsCoreTests\Resources\output1.tsv";
+
+            var data = MessagePackHandler.LoadFromFile<DataStorageForTest>(datafile);
+            var msdecResults = MsdecResultsReader.ReadMSDecResults(data.MsdecResultFile, out var _, out var _);
+            var mapper = new DataBaseMapper();
+            mapper.Add("MspDB", new MockKey(data.MspDB));
+            mapper.Add("TextDB", new MockKey(data.TextDB));
+            mapper.Restore(null);
+
+            var exporter = new AlignmentCSVExporter();
+            var stream = new MemoryStream();
+            exporter.Export(
+                stream,
+                data.Spots,
+                msdecResults,
+                data.Files,
+                new ImmsMetadataAccessor(mapper, data.Parameter),
+                new LegacyQuantValueAccessor("Height", data.Parameter),
+                new List<StatsValue>(0));
+
+            var expected = File.ReadAllText(expectedfile);
+            var actual = Encoding.UTF8.GetString(stream.ToArray());
+            // Assert.AreEqual(expected, actual);
+            // CollectionAssert.AreEqual(
+            //     expected.Split(Environment.NewLine).Select(row => row.TrimEnd('\t')).ToArray(),
+            //     actual.Split(Environment.NewLine).ToArray());
+            foreach ((var ex, var ac) in expected.Split(Environment.NewLine).Select(row => row.TrimEnd('\t')).Zip(actual.Split(Environment.NewLine))) {
+                Assert.AreEqual(ex, ac);
+            }
+        }
+
+        private static void OriginalExportAlignmentResult(
+            string outfile, string exportType,
+            IReadOnlyList<AlignmentSpotProperty> spots, List<MSDecResult> msdecResults,
+            List<AnalysisFileBean> files,
+            List<MoleculeMsReference> mspDB, List<MoleculeMsReference> textDB,
+            ParameterBase param, MachineCategory category) {
+            using (var sw = new StreamWriter(outfile, false, Encoding.ASCII)) {
+                // Header
+                MsdialCore.Export.ResultExport.WriteAlignmentResultHeader(sw, category, files);
+
+                // From the second
+                foreach (var spot in spots) {
+                    var msdecID = spot.MasterAlignmentID;
+                    var msdec = msdecResults[msdecID];
+                    MsdialCore.Export.ResultExport.WriteAlignmentSpotFeature(sw, spot, msdec, param, mspDB, textDB, exportType);
+
+                    foreach (var driftSpot in spot.AlignmentDriftSpotFeatures ?? Enumerable.Empty<AlignmentSpotProperty>()) {
+                        msdecID = driftSpot.MasterAlignmentID;
+                        msdec = msdecResults[msdecID];
+                        MsdialCore.Export.ResultExport.WriteAlignmentSpotFeature(sw, driftSpot, msdec, param, mspDB, textDB, exportType);
+                    }
+                }
+            }
+        }
+
+        private static void prepare() {
+            var project = @"D:\infusion_project\Bruker_20210521_original\Bruker_20210521\infusion\timsON_neg\2021_06_03_10_57_44.mtd2";
+            var newmsdecfile = @"C:\Users\YUKI MATSUZAWA\works\msdialworkbench\MsdialImmsCoreTests\Resources\input_dec1";
+            var newdatafile = @"C:\Users\YUKI MATSUZAWA\works\msdialworkbench\MsdialImmsCoreTests\Resources\input_data1";
+            var expected = @"C:\Users\YUKI MATSUZAWA\works\msdialworkbench\MsdialImmsCoreTests\Resources\output1.tsv";
+
+            var storage = new Parser.MsdialImmsSerializer().LoadMsdialDataStorageBase(project);
+            var alignmentFile = storage.AlignmentFiles[0];
+            var container = MessagePackHandler.LoadFromFile<AlignmentResultContainer>(alignmentFile.FilePath);
+            var msdecResults = MsdecResultsReader.ReadMSDecResults(alignmentFile.SpectraFilePath, out _, out _);
+            var rand = new Random();
+
+            var idcs = Enumerable.Range(0, 20).Select(_ => rand.Next(container.AlignmentSpotProperties.Count)).ToList();
+            var newSpots = idcs.Select(idc => container.AlignmentSpotProperties[idc]).ToList();
+            var newDecs = idcs.Select(idc => msdecResults[idc]).ToList();
+
+            var counter = 0;
+            foreach (var spot in newSpots) {
+                spot.MasterAlignmentID = counter++;
+            }
+            var msp = storage.MspDB;
+            var text = storage.TextDB;
+            OriginalExportAlignmentResult(
+                expected, "Height",
+                newSpots, newDecs,
+                storage.AnalysisFiles,
+                msp, text,
+                storage.ParameterBase, MachineCategory.IMMS);
+
+            var newMsp = new List<MoleculeMsReference>();
+            var newText = new List<MoleculeMsReference>();
+            foreach (var spot in newSpots) {
+                if (spot.MspBasedMatchResult != null && spot.MspBasedMatchResult.LibraryIDWhenOrdered >= 0 && spot.MspBasedMatchResult.LibraryIDWhenOrdered < msp.Count) {
+                    newMsp.Add(msp[spot.MspBasedMatchResult.LibraryIDWhenOrdered]);
+                    spot.MspBasedMatchResult.LibraryIDWhenOrdered = newMsp.Count - 1;
+                }
+                if (spot.TextDbBasedMatchResult != null && spot.TextDbBasedMatchResult.LibraryIDWhenOrdered >= 0 && spot.TextDbBasedMatchResult.LibraryIDWhenOrdered < text.Count) {
+                    newText.Add(text[spot.TextDbBasedMatchResult.LibraryIDWhenOrdered]);
+                    spot.TextDbBasedMatchResult.LibraryIDWhenOrdered = newText.Count - 1;
+                }
+            }
+
+            MsdecResultsWriter.Write(newmsdecfile, newDecs);
+
+            var data = new DataStorageForTest
+            {
+                Spots = newSpots,
+                Parameter = storage.ParameterBase,
+                MsdecResultFile = newmsdecfile,
+                Files = storage.AnalysisFiles,
+                MspDB = msp,
+                TextDB = text,
+            };
+
+            MessagePackHandler.SaveToFile(data, newdatafile);
+        }
+    }
+
+    [MessagePack.MessagePackObject]
+    public class DataStorageForTest
+    {
+        [MessagePack.Key(0)]
+        public List<AlignmentSpotProperty> Spots { get; set; }
+        [MessagePack.Key(1)]
+        public ParameterBase Parameter { get; set; }
+        [MessagePack.Key(2)]
+        public string MsdecResultFile { get; set; }
+        [MessagePack.Key(3)]
+        public List<AnalysisFileBean> Files { get; set; }
+        [MessagePack.Key(4)]
+        public List<MoleculeMsReference> MspDB { get; set; }
+        [MessagePack.Key(5)]
+        public List<MoleculeMsReference> TextDB { get; set; }
+        [MessagePack.Key(6)]
+        public List<ChromatogramPeakFeature> Features { get; set; }
+    }
+
+    class MockKey : IReferRestorationKey
+    {
+        public MockKey(List<MoleculeMsReference> db) {
+            this.db = db;
+        }
+
+        private List<MoleculeMsReference> db;
+
+        public IMatchResultRefer Accept(IRestorationVisitor visitor) {
+            return new DataBaseRefer(db);
+        }
+    }
+}
