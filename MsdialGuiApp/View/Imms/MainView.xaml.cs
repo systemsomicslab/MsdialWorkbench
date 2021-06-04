48cb5959cee13926971d3db4a2d660e32d1d4738
Yuki Matsuzawa <yuki.matsuzawa@riken.jp>
2021-06-04 22:00:35 +0900

    Implement tab switch event handler

--- MsdialGuiApp/View/Imms/MainView.xaml.cs
+++ MsdialGuiApp/View/Imms/MainView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +24,85 @@ namespace CompMs.App.Msdial.View.Imms
     {
         public MainView() {
             InitializeComponent();
+
+            var tabChange = Observable.FromEvent<SelectionChangedEventHandler, SelectionChangedEventArgs>(
+                h => (s, e) => h(e),
+                h => TabControl_PairwisePlotViewer.SelectionChanged += h,
+                h => TabControl_PairwisePlotViewer.SelectionChanged -= h);
+
+            var peakPlotSelect = tabChange
+                .Where(e => e.AddedItems[0] == TabItem_RtMzPairwisePlotPeakView)
+                .Select(_ => Unit.Default);
+            Observable.FromEvent<DependencyPropertyChangedEventHandler, DependencyPropertyChangedEventArgs>(
+                    h => (s, e) => h(e),
+                    h => TabItem_RtMzPairwisePlotPeakView.DataContextChanged += h,
+                    h => TabItem_RtMzPairwisePlotPeakView.DataContextChanged -= h)
+                .Do(_ => TabItem_RtMzPairwisePlotPeakView.IsSelected = true)
+                .Select(_ => Unit.Default)
+                .Merge(peakPlotSelect)
+                .Subscribe(_ => OnAnalysisViewSelected());
+
+            var alignmentPlotSelect = tabChange
+                .Where(e => e.AddedItems[0] == TabItem_RtMzPairwisePlotAlignmentView)
+                .Select(_ => Unit.Default);
+            Observable.FromEvent<DependencyPropertyChangedEventHandler, DependencyPropertyChangedEventArgs>(
+                    h => (s, e) => h(e),
+                    h => TabItem_RtMzPairwisePlotAlignmentView.DataContextChanged += h,
+                    h => TabItem_RtMzPairwisePlotAlignmentView.DataContextChanged -= h)
+                .Do(_ => TabItem_RtMzPairwisePlotAlignmentView.IsSelected = true)
+                .Select(_ => Unit.Default)
+                .Merge(alignmentPlotSelect)
+                .Subscribe(_ => OnAlignmentViewSelected());
+
+            var mouseRDown = Observable.FromEvent<MouseButtonEventHandler, MouseButtonEventArgs>(
+                h => (s, e) => h(e),
+                h => MouseRightButtonDown += h,
+                h => MouseRightButtonDown -= h);
+            var mouseRUp = Observable.FromEvent<MouseButtonEventHandler, MouseButtonEventArgs>(
+                h => (s, e) => h(e),
+                h => MouseRightButtonUp += h,
+                h => MouseRightButtonUp -= h);
+            var mouseMove = Observable.FromEvent<MouseEventHandler, MouseEventArgs>(
+                h => (s, e) => h(e),
+                h => MouseMove += h,
+                h => MouseMove -= h);
+            var drag = mouseMove
+                .SkipUntil(mouseRDown)
+                .TakeUntil(mouseRUp)
+                .Repeat();
+
+            var mouseLeave = mouseRDown.Select(e => e.GetPosition(null))
+                .CombineLatest(drag.Select(e => e.GetPosition(null)), (p, q) => (p, q))
+                .Where(pq => Math.Abs(pq.p.X - pq.q.X) > SystemParameters.MinimumHorizontalDragDistance
+                            || Math.Abs(pq.p.Y - pq.q.Y) > SystemParameters.MinimumVerticalDragDistance);
+
+            var contextMenuOpening = Observable.FromEvent<ContextMenuEventHandler, ContextMenuEventArgs>(
+                h => (s, e) => h(e),
+                h => ContextMenuOpening += h,
+                h => ContextMenuOpening -= h);
+
+            contextMenuOpening
+                .TakeUntil(mouseLeave)
+                .Repeat()
+                .Subscribe(e => e.Handled = true);
+        }
+
+        private void OnAnalysisViewSelected() {
+            if (TabItem_EicViewer != null) {
+                TabItem_EicViewer.IsSelected = true;
+            }
+            if (TabItem_MeasurementVsReference != null) {
+                TabItem_MeasurementVsReference.IsSelected = true;
+            }
+        }
+
+        private void OnAlignmentViewSelected() {
+            if (TabItem_BarChartViewer != null) {
+                TabItem_BarChartViewer.IsSelected = true;
+            }
+            if (TabItem_RepresentativeVsReference != null) {
+                TabItem_RepresentativeVsReference.IsSelected = true;
+            }
         }
     }
 }
