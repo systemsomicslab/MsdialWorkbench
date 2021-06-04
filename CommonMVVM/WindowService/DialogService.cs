3b68d27a91636a0a8633dc2baf996bfe2f7b2e8f
Yuki Matsuzawa <yuki.matsuzawa@riken.jp>
2021-06-04 18:20:18 +0900

    Add DialogService class

---
+++ CommonMVVM/WindowService/DialogService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+
+namespace CompMs.Common.WindowService
+{
+    public class DialogService<TView, TViewModel> : IWindowService<TViewModel> where TView : Window, new()
+    {
+        public DialogService() {
+
+        }
+
+        public DialogService(Window owner) {
+            Owner = owner;
+        }
+
+        public Window Owner { get; }
+
+        public void Show(TViewModel viewmodel) {
+            var dialog = new TView()
+            {
+                Owner = Owner,
+                DataContext = viewmodel,
+            };
+
+            dialog.Show();
+        }
+
+        public bool? ShowDialog(TViewModel viewmodel) {
+            var dialog = new TView()
+            {
+                Owner = Owner,
+                DataContext = viewmodel,
+            };
+
+            return dialog.ShowDialog();
+        }
+    }
+}
---
+++ CommonMVVM/WindowService/IWindowService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CompMs.Common.WindowService
+{
+    public interface IWindowService<TViewModel>
+    {
+        bool? ShowDialog(TViewModel viewmodel);
+        void Show(TViewModel viewmodel);
+    }
+}
