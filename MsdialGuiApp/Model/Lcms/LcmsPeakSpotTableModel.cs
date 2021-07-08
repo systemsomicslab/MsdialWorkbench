c10fa967f9156dbc6f05947ffe5fd52e290e4d35
Yuki Matsuzawa <yuki.matsuzawa@riken.jp>
2021-07-08 11:44:53 +0900

    Add IonTable for lcms.

---
+++ MsdialGuiApp/Model/Lcms/LcmsPeakSpotTableModel.cs
@@ -0,0 +1,79 @@
+﻿using CompMs.App.Msdial.Model.DataObj;
+using CompMs.App.Msdial.Model.Table;
+using Reactive.Bindings;
+using System.Collections.ObjectModel;
+
+namespace CompMs.App.Msdial.Model.Lcms
+{
+    interface ILcmsPeakSpotTableModel : IPeakSpotTableModelBase
+    {
+        double MassMin { get; }
+        double MassMax { get; }
+        double RtMin { get; }
+        double RtMax { get; }
+    }
+
+    abstract class LcmsPeakSpotTableModel<T> : PeakSpotTableModelBase<T>, ILcmsPeakSpotTableModel where T : class
+    {
+        public LcmsPeakSpotTableModel(
+            ObservableCollection<T> peakSpots,
+            IReactiveProperty<T> target,
+            double massMin,
+            double massMax,
+            double rtMin,
+            double rtMax)
+            : base(peakSpots, target) {
+
+            MassMin = massMin;
+            MassMax = massMax;
+            RtMin = rtMin;
+            RtMax = rtMax;
+        }
+
+        public double MassMin { get; }
+
+        public double MassMax { get; }
+
+        public double RtMin { get; }
+
+        public double RtMax { get; }
+    }
+
+    sealed class LcmsAlignmentSpotTableModel : LcmsPeakSpotTableModel<AlignmentSpotPropertyModel>
+    {
+        public LcmsAlignmentSpotTableModel(
+            ObservableCollection<AlignmentSpotPropertyModel> peakSpots,
+            IReactiveProperty<AlignmentSpotPropertyModel> target,
+            double massMin,
+            double massMax,
+            double rtMin,
+            double rtMax)
+            : base(
+                  peakSpots,
+                  target,
+                  massMin,
+                  massMax,
+                  rtMin,
+                  rtMax) {
+        }
+    }
+
+    sealed class LcmsAnalysisPeakTableModel : LcmsPeakSpotTableModel<ChromatogramPeakFeatureModel>
+    {
+        public LcmsAnalysisPeakTableModel(
+            ObservableCollection<ChromatogramPeakFeatureModel> peakSpots,
+            IReactiveProperty<ChromatogramPeakFeatureModel> target,
+            double massMin,
+            double massMax,
+            double rtMin,
+            double rtMax)
+            : base(
+                  peakSpots,
+                  target,
+                  massMin,
+                  massMax,
+                  rtMin,
+                  rtMax) {
+        }
+    }
+}
---
+++ MsdialGuiApp/ViewModel/Lcms/LcmsPeakSpotTableViewModel.cs
@@ -0,0 +1,115 @@
+﻿using CompMs.App.Msdial.Model;
+using CompMs.App.Msdial.Model.Lcms;
+using CompMs.App.Msdial.Model.Loader;
+using CompMs.App.Msdial.ViewModel.Table;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+
+namespace CompMs.App.Msdial.ViewModel.Lcms
+{
+    abstract class LcmsPeakSpotTableViewModel : PeakSpotTableViewModelBase
+    {
+        protected LcmsPeakSpotTableViewModel(
+            ILcmsPeakSpotTableModel model,
+            IReactiveProperty<double> massLower,
+            IReactiveProperty<double> massUpper,
+            IReactiveProperty<double> rtLower,
+            IReactiveProperty<double> rtUpper,
+            IReactiveProperty<string> metaboliteFilterKeyword,
+            IReactiveProperty<string> commentFilterKeyword)
+            : base(model, metaboliteFilterKeyword, commentFilterKeyword) {
+            if (massLower is null) {
+                throw new ArgumentNullException(nameof(massLower));
+            }
+
+            if (massUpper is null) {
+                throw new ArgumentNullException(nameof(massUpper));
+            }
+
+            if (rtLower is null) {
+                throw new ArgumentNullException(nameof(rtLower));
+            }
+
+            if (rtUpper is null) {
+                throw new ArgumentNullException(nameof(rtUpper));
+            }
+
+            MassMin = model.MassMin;
+            MassMax = model.MassMax;
+            MassLower = massLower;
+            MassUpper = massUpper;
+
+            RtMin = model.RtMin;
+            RtMax = model.RtMax;
+            RtLower = rtLower;
+            RtUpper = rtUpper;
+        }
+
+        public double MassMin { get; }
+        public double MassMax { get; }
+        public IReactiveProperty<double> MassLower { get; }
+        public IReactiveProperty<double> MassUpper { get; }
+
+        public double RtMin { get; }
+        public double RtMax { get; }
+        public IReactiveProperty<double> RtLower { get; }
+        public IReactiveProperty<double> RtUpper { get; }
+    }
+
+    sealed class LcmsAnalysisPeakTableViewModel : LcmsPeakSpotTableViewModel
+    {
+        public LcmsAnalysisPeakTableViewModel(
+            ILcmsPeakSpotTableModel model,
+            IObservable<EicLoader> eicLoader,
+            IReactiveProperty<double> massLower,
+            IReactiveProperty<double> massUpper,
+            IReactiveProperty<double> rtLower,
+            IReactiveProperty<double> rtUpper,
+            IReactiveProperty<string> metaboliteFilterKeyword,
+            IReactiveProperty<string> commentFilterKeyword)
+            : base(
+                  model,
+                  massLower,
+                  massUpper,
+                  rtLower,
+                  rtUpper,
+                  metaboliteFilterKeyword,
+                  commentFilterKeyword) {
+            if (eicLoader is null) {
+                throw new ArgumentNullException(nameof(eicLoader));
+            }
+            EicLoader = eicLoader.ToReadOnlyReactivePropertySlim().AddTo(Disposables);
+        }
+
+        public ReadOnlyReactivePropertySlim<EicLoader> EicLoader { get; }
+    }
+
+    sealed class LcmsAlignmentSpotTableViewModel : LcmsPeakSpotTableViewModel
+    {
+        public LcmsAlignmentSpotTableViewModel(
+            ILcmsPeakSpotTableModel model,
+            IObservable<IBarItemsLoader> barItemsLoader,
+            IReactiveProperty<double> massLower,
+            IReactiveProperty<double> massUpper,
+            IReactiveProperty<double> rtLower,
+            IReactiveProperty<double> rtUpper,
+            IReactiveProperty<string> metaboliteFilterKeyword,
+            IReactiveProperty<string> commentFilterKeyword)
+            : base(
+                  model,
+                  massLower,
+                  massUpper,
+                  rtLower,
+                  rtUpper,
+                  metaboliteFilterKeyword,
+                  commentFilterKeyword) {
+            if (barItemsLoader is null) {
+                throw new ArgumentNullException(nameof(barItemsLoader));
+            }
+            BarItemsLoader = barItemsLoader.ToReadOnlyReactivePropertySlim().AddTo(Disposables);
+        }
+
+        public ReadOnlyReactivePropertySlim<IBarItemsLoader> BarItemsLoader { get; }
+    }
+}
