d5d5493a690ec643bb4a72348657b5d4726e959a
Yuki Matsuzawa <yuki.matsuzawa@riken.jp>
2021-08-12 12:10:06 +0900

    Add test for LcimmsAnalyissParameterSetModel

--- MsdialGuiApp/Model/Lcimms/LcimmsAnalysisParameterSetModel.cs
+++ MsdialGuiApp/Model/Lcimms/LcimmsAnalysisParameterSetModel.cs
@@ -1,12 +1,16 @@
 ﻿using CompMs.App.Msdial.Model.Core;
+using CompMs.App.Msdial.ViewModel;
+using CompMs.Common.DataObj;
+using CompMs.Common.Parameter;
 using CompMs.MsdialCore.DataObj;
+using CompMs.MsdialCore.Utility;
 using CompMs.MsdialLcImMsApi.Parameter;
 using System;
 using System.Collections.Generic;
 
 namespace CompMs.App.Msdial.Model.Lcimms
 {
-    class LcimmsAnalysisParameterSetModel : AnalysisParameterSetModelBase
+    public class LcimmsAnalysisParameterSetModel : AnalysisParameterSetModelBase
     {
         public LcimmsAnalysisParameterSetModel(MsdialLcImMsParameter parameter, IEnumerable<AnalysisFileBean> files)
             : base(parameter, files) {
@@ -15,8 +19,21 @@ namespace CompMs.App.Msdial.Model.Lcimms
             }
 
             Parameter = parameter;
+
+            if (parameter.FileID2CcsCoefficients is null) {
+                parameter.FileID2CcsCoefficients = new Dictionary<int, CoefficientsForCcsCalculation>();
+            }
+            FileID2CcsCoefficients = parameter.FileID2CcsCoefficients;
+            foreach (var file in files) {
+                if (!FileID2CcsCoefficients.ContainsKey(file.AnalysisFileId)) {
+                    var calinfo = DataAccess.ReadIonMobilityCalibrationInfo(file.AnalysisFilePath) ?? new RawCalibrationInfo();
+                    FileID2CcsCoefficients[file.AnalysisFileId] = new CoefficientsForCcsCalculation(calinfo);
+                }
+            }
         }
 
         public MsdialLcImMsParameter Parameter { get; }
+
+        public Dictionary<int, CoefficientsForCcsCalculation> FileID2CcsCoefficients { get; } 
     }
 }
