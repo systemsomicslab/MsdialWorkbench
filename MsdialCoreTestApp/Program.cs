0ce3ed53e8038e5ef7112ef3a7ddc75b2968a303
Yuki Matsuzawa <yuki.matsuzawa@riken.jp>
2021-09-08 15:53:04 +0900

    Change reading baf file.

--- MsdialCoreTestApp/Program.cs
+++ MsdialCoreTestApp/Program.cs
@@ -81,12 +81,14 @@ namespace CompMs.App.MsdialConsole {
             var dimsfile = @"D:\msdial_test\Msdial\out\MSMSALL_Positive\20200717_Posi_MSMSALL_Liver1.abf";
             var immsfile = @"D:\msdial_test\Msdial\out\infusion_neg_timsON_pasef\kidney1_3times_timsON_pasef_neg000001.d";
             var lcimmsfile = @"D:\BugReport\20201216_MS2missing\PS78_Plasma1_4_1_4029.d";
+            var samplefile = @"D:\infusion_project\Bruker_20210521_original\Bruker_20210521\infusion\timsOFF_pos\kidney1_1-47_1_14919.d";
+            DumpSpectrum(samplefile, 1206, 800, 100);
 
             //new FileParser().FastaParserTest(@"E:\6_Projects\PROJECT_Proteomics\jPOST_files_JPST000200.0\human_proteins_ref_wrong.fasta");
             //new EnzymesXmlRefParser().Read();
             //new ModificationsXmlRefParser().Read();
 
-            new TestProteomicsProcess().PDFTest();
+            // new TestProteomicsProcess().PDFTest();
 
             //FormulaStringParcer.Convert2FormulaObjV2("C6H12O6");
             //FormulaStringParcer.Convert2FormulaObjV2("CH3COONa");
@@ -124,8 +126,18 @@ namespace CompMs.App.MsdialConsole {
             Console.WriteLine($"Number of scan {allspectra.Where(spec => spec.MsLevel == 1).Select(spec => spec.ScanNumber).Distinct().Count()}");
             for(int i = 0; i < n; i++) {
                 var spec = allspectra[i];
-                Console.WriteLine("Original index={0} ID={1}, Time={2}, Drift ID={3}, Drift time={4}, Polarity={5}, MS level={6}, Precursor mz={7}, CollisionEnergy={8}, SpecCount={9}",
-                    spec.OriginalIndex, spec.ScanNumber, spec.ScanStartTime, spec.DriftScanNumber, spec.DriftTime, spec.ScanPolarity, spec.MsLevel, spec.Precursor?.SelectedIonMz ?? -1, spec.CollisionEnergy, spec.Spectrum.Length);
+                Console.WriteLine("Original index={0} ID={1}, Time={2}, Drift ID={3}, Drift time={4}, Polarity={5}, MS level={6}, Precursor mz={7}, CollisionEnergy={8}, SpecCount={9}", spec.OriginalIndex, spec.ScanNumber, spec.ScanStartTime, spec.DriftScanNumber, spec.DriftTime, spec.ScanPolarity, spec.MsLevel, spec.Precursor?.SelectedIonMz ?? -1, spec.CollisionEnergy, spec.Spectrum.Length);
+            }
+        }
+
+        private static void DumpSpectrum(string file, int scanNumber, double mz, double mztol) {
+            var allspectra = DataAccess.GetAllSpectra(file);
+            var spectra = allspectra.FirstOrDefault(spec => spec.ScanNumber == scanNumber);
+            Console.WriteLine(
+                "Original index={0} ID={1}, Time={2}, Drift ID={3}, Drift time={4}, Polarity={5}, MS level={6}, Precursor mz={7}, CollisionEnergy={8}, SpecCount={9}",
+                spectra.OriginalIndex, spectra.ScanNumber, spectra.ScanStartTime, spectra.DriftScanNumber, spectra.DriftTime, spectra.ScanPolarity, spectra.MsLevel, spectra.Precursor?.SelectedIonMz ?? -1, spectra.CollisionEnergy, spectra.Spectrum.Length);
+            foreach (var spec in spectra.Spectrum.SkipWhile(spec => spec.Mz < mz - mztol).TakeWhile(spec => spec.Mz < mz + mztol)) {
+                Console.WriteLine($"Mz: {spec.Mz}\tIntensity: {spec.Intensity}");
             }
         }
     }
