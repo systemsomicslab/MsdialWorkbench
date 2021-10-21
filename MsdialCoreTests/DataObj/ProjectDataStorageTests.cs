d6bbbddffe51634969eb3158a6acc874d3b9e82a
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2021-10-21 16:16:29 +0900

    Add Storage for multi project.

---
+++ MsdialCoreTests/DataObj/ProjectDataStorageTests.cs
@@ -0,0 +1,80 @@
+﻿using CompMs.Common.Components;
+using CompMs.Common.DataObj.Database;
+using CompMs.MsdialCore.Parameter;
+using CompMs.MsdialCore.Parser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompMs.MsdialCore.DataObj.Tests
+{
+    [TestClass()]
+    public class ProjectDataStorageTests
+    {
+        [TestMethod()]
+        public async Task LoadTest() {
+            var projectParameter = new ParameterBase { ProjectFilePath = "TestProject" };
+            var proj = new ProjectDataStorage(projectParameter, new List<IMsdialDataStorage<ParameterBase>>());
+            var storage1 = new MockStorage { Parameter = new ParameterBase { ProjectFilePath = "Test1", } };
+            var storage2 = new MockStorage { Parameter = new ParameterBase { ProjectFilePath = "Test2", } };
+            var storage3 = new MockStorage { Parameter = new ParameterBase { ProjectFilePath = "Test3", } };
+            proj.AddStorage(storage1);
+            proj.AddStorage(storage2);
+            proj.AddStorage(storage3);
+
+            var serializer = new MockSerializer();
+            var stream = new MemoryStream();
+            using (var streamManager = ZipStreamManager.OpenCreate(stream)) {
+                await proj.Save(streamManager, serializer);
+            }
+
+            using (var streamManager = ZipStreamManager.OpenGet(stream)) {
+                var actual = await ProjectDataStorage.Load(streamManager, serializer);
+
+                Assert.AreEqual(proj.ProjectParameter.ProjectFilePath, actual.ProjectParameter.ProjectFilePath);
+                CollectionAssert.AreEqual(proj.ProjectPaths, actual.ProjectPaths);
+                Assert.AreEqual(proj.Storages.Count, actual.Storages.Count);
+                foreach ((var exp, var act) in proj.Storages.Zip(actual.Storages)) {
+                    Assert.AreEqual(exp.Parameter.ProjectFilePath, act.Parameter.ProjectFilePath);
+                }
+            }
+        }
+    }
+
+    class MockStorage : IMsdialDataStorage<ParameterBase> {
+        public List<AnalysisFileBean> AnalysisFiles { get; set; }
+        public List<AlignmentFileBean> AlignmentFiles { get; set; }
+        public List<MoleculeMsReference> MspDB { get; set; }
+        public List<MoleculeMsReference> TextDB { get; set; }
+        public List<MoleculeMsReference> IsotopeTextDB { get; set; }
+        public IupacDatabase IupacDatabase { get; set; }
+
+        public ParameterBase Parameter { get; set; }
+
+        public DataBaseMapper DataBaseMapper { get; set; }
+        public DataBaseStorage DataBases { get; set; }
+
+        public Task Save(IStreamManager streamManager, string projectTitle, string prefix) {
+            throw new NotImplementedException();
+        }
+    }
+
+    class MockSerializer : IMsdialSerializer
+    {
+        // private List<IMsdialDataStorage<ParameterBase>> storages = new List<IMsdialDataStorage<ParameterBase>>();
+        private Dictionary<string, IMsdialDataStorage<ParameterBase>> storages = new Dictionary<string, IMsdialDataStorage<ParameterBase>>();
+        public Task<IMsdialDataStorage<ParameterBase>> LoadAsync(IStreamManager streamManager, string projectTitle, string prefix) {
+            var storage = storages[$"{prefix}/{projectTitle}"];
+            storages.Remove(projectTitle);
+            return Task.FromResult(storage);
+        }
+
+        public Task SaveAsync(IMsdialDataStorage<ParameterBase> dataStorage, IStreamManager streamManager, string projectTitle, string prefix) {
+            storages.Add($"{prefix}/{projectTitle}", dataStorage);
+            return Task.CompletedTask;
+        }
+    }
+}
