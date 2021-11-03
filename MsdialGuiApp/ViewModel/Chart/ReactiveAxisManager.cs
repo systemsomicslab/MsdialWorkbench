5622521cd9c611d5c741e9f7ebc757d2ca8b53ff
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2021-11-03 14:31:36 +0900

    Add test app to check spectrum

--- MsdialGuiApp/ViewModel/Chart/ReactiveAxisManager.cs
+++ MsdialGuiApp/ViewModel/Chart/ReactiveAxisManager.cs
@@ -2,48 +2,26 @@
 using CompMs.Graphics.Core.Base;
 using System;
 using System.Reactive.Linq;
-using System.Threading;
 
 namespace CompMs.App.Msdial.ViewModel.Chart
 {
-    public class ReactiveAxisManager<T> : ContinuousAxisManager<T>, IDisposable where T : IConvertible
+    public class ReactiveAxisManager<T> : ContinuousAxisManager<T> where T : IConvertible
     {
         public ReactiveAxisManager(
             IObservable<Range> rangeSource,
             Range bounds = null) : base(new Range(0, 1), bounds) {
-            rangeUnSubscriber = rangeSource.ObserveOnDispatcher().Subscribe(UpdateRange);
+            Disposables.Add(rangeSource.ObserveOnDispatcher().Subscribe(UpdateRange));
         }
 
         public ReactiveAxisManager(
             IObservable<Range> rangeSource,
             IChartMargin margin,
             Range bounds = null) : base(new Range(0, 1), margin, bounds) {
-            rangeUnSubscriber = rangeSource.ObserveOnDispatcher().Subscribe(UpdateRange);
+            Disposables.Add(rangeSource.ObserveOnDispatcher().Subscribe(UpdateRange));
         }
 
         private void UpdateRange(Range initial) {
             UpdateInitialRange(initial);
-        }
-
-        private bool disposedValue;
-        private IDisposable rangeUnSubscriber;
-
-        protected virtual void Dispose(bool disposing) {
-            if (!disposedValue) {
-                if (disposing) {
-                    // TODO: dispose managed state (managed objects)
-                    rangeUnSubscriber.Dispose();
-                    rangeUnSubscriber = null;
-                }
-
-                disposedValue = true;
-            }
-        }
-
-        public void Dispose() {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
         }
     }
 
