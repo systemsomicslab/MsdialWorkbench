e74f93902d28ce9308de546618dfc7319f8c45e7
Hiroshi_Tsugawa <hiroshi.tsugawa@riken.jp>
2021-12-02 16:21:55 +0900

    To Todoroki-kun

--- StructureFinderConsoleApp/Program.cs
+++ StructureFinderConsoleApp/Program.cs
@@ -19,6 +19,8 @@ namespace StructureFinderConsoleApp
         [STAThread]
         static void Main(string[] args)
         {
+
+            Code4NPR2020.CalculatePrecursorMzVS2("", "");
             //Derivatization.Test();
             /// code for Natural Product Reports 2020
             //Code4NPR2020.GenerateStructureTableFromMSPs();
@@ -37,7 +39,7 @@ namespace StructureFinderConsoleApp
             //Code4NPR2020.Check144Existence(@"D:\Paper of Natural Product Reports\Statistics\node-2105.txt", @"D:\Paper of Natural Product Reports\Statistics\node-2105-144.txt");
 
             //Code4NPR2020.ExtractSubstructureContainingStructureQueries();
-            Code4NPR2020.CalculatePrecursorMz(@"E:\6_Projects\PROJECT_ImagingMS\Lipid reference library\temp.txt", @"E:\6_Projects\PROJECT_ImagingMS\Lipid reference library\temp_premz.txt");
+            //Code4NPR2020.CalculatePrecursorMz(@"E:\6_Projects\PROJECT_ImagingMS\Lipid reference library\temp.txt", @"E:\6_Projects\PROJECT_ImagingMS\Lipid reference library\temp_premz.txt");
             //Code4NPR2020.CheckPrecursorMzExistence();
 
             //BondPathStatistics.StatisticsOfMatchedFragmentIons(@"D:\9_Spectral library curations\Fragment curation\20200910\Pos\msp\", "-Pos");
