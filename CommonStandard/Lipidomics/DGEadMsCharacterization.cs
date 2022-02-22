c5a4a6e7708db89cb506d54131c354a90cfe5203
Yuto KURIZAKI <s197846v@st.go.tuat.ac.jp>
2022-02-22 10:28:52 +0900

    Add DGEadMsCharacterization

--- CommonStandard/Lipidomics/DGEadMsCharacterization.cs
+++ CommonStandard/Lipidomics/DGEadMsCharacterization.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using CompMs.Common.Algorithm.Scoring;
+using CompMs.Common.Components;
+using CompMs.Common.Enum;
+using CompMs.Common.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CompMs.Common.Lipidomics
 {
-    class Class1
+    public static class DGEadMsCharacterization
     {
+        public static (ILipid, double[]) Characterize(
+            IMSScanProperty scan, ILipid molecule, MoleculeMsReference reference,
+            float tolerance, float mzBegin, float mzEnd)
+        {
+
+            var defaultResult = EadMsCharacterizationUtility.GetDefaultScoreForGlycerophospholipid(
+                    scan, reference, tolerance, mzBegin, mzEnd, 0, 2, 1, 0.5);
+            return EadMsCharacterizationUtility.GetDefaultCharacterizationResultForGlycerophospholipid(molecule, defaultResult);
+        }
     }
 }
