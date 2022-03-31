bc964d551ac6b535940fb162915c0ead27d7d2e5
Kozo Nishida <kozo.nishida@gmail.com>
2022-03-31 10:35:08 +0000

    Add Blank ratio filter

--- MsdialLcMsApi/Algorithm/PostCuration/PostCurator.cs
+++ MsdialLcMsApi/Algorithm/PostCuration/PostCurator.cs
@@ -1,8 +1,9 @@
-﻿using CompMs.MsdialCore.DataObj;
+using CompMs.MsdialCore.DataObj;
 using CompMs.MsdialCore.MSDec;
 using CompMs.MsdialCore.Parameter;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CompMs.MsdialLcMsApi.Algorithm.PostCuration {
@@ -18,30 +19,46 @@ namespace CompMs.MsdialLcMsApi.Algorithm.PostCuration {
 
                 var frag = false;
 
+                double avgBlank = 0;
+                List<AlignmentChromPeakFeature> blankProps = spot.AlignedPeakProperties.Where(x => param.FileID_ClassName[x.FileID] == "Blank").ToList();
+                //Console.Write(blankProps.Average(x => x.PeakHeightTop));
+                avgBlank = blankProps.Average(x => x.PeakHeightTop);
+
+                double avgQC = 0;
+                List<AlignmentChromPeakFeature> qcProps = spot.AlignedPeakProperties.Where(x => param.FileID_ClassName[x.FileID] == "QC").ToList();
+                //Console.Write(qcProps.Average(x => x.PeakHeightTop));
+                avgQC = qcProps.Average(x => x.PeakHeightTop);
+
                 // filtering process
                 #region
-                var mz = spot.MassCenter;
-                var rt = spot.TimesCenter.RT.Value;
-                var alignedPeaks = spot.AlignedPeakProperties;
+                // var mz = spot.MassCenter;
+                // var rt = spot.TimesCenter.RT.Value;
+                // var alignedPeaks = spot.AlignedPeakProperties;
 
-                foreach (var peak in alignedPeaks) {
-                    var intensity = peak.PeakHeightTop;
-                    var area = peak.PeakAreaAboveZero;
-                    var mzOfPeak = peak.Mass;
-                    var fileID = peak.FileID;
+                // foreach (var peak in alignedPeaks) {
+                //     var intensity = peak.PeakHeightTop;
+                //     var area = peak.PeakAreaAboveZero;
+                //     var mzOfPeak = peak.Mass;
+                //     var fileID = peak.FileID;
 
-                    var fileObj = files[fileID];
-                    var fileType = fileObj.AnalysisFileType;
-                    if (fileType == Common.Enum.AnalysisFileType.Blank) {
+                //     var fileObj = files[fileID];
+                //     var fileType = fileObj.AnalysisFileType;
+                //     if (fileType == Common.Enum.AnalysisFileType.Blank) {
 
-                    }
+                //     }
 
-                }
+                // }
                 #endregion
 
-                if (frag == true) {
+                double filterBlankThreshold = 0.8;
+                double ratioBlank = avgBlank / avgQC;
+                if (ratioBlank >= filterBlankThreshold) {
                     spot.IsFilteredByPostCurator = true;
                 }
+
+                // if (frag == true) {
+                //     spot.IsFilteredByPostCurator = true;
+                // }
 
             }
 
