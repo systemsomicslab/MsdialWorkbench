a3ed0d96436610cc3aa5598e312780f677ff8407
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2022-05-18 09:57:36 +0900

    Add RawSpectra class for first class collection for RawSpectrum.

---
+++ MsdialCore/DataObj/RawSpectra.cs
@@ -0,0 +1,84 @@
+﻿using CompMs.Common.Components;
+using CompMs.Common.DataObj;
+using CompMs.Common.Enum;
+using CompMs.Common.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompMs.MsdialCore.DataObj
+{
+    public class RawSpectra
+    {
+        private readonly IReadOnlyList<RawSpectrum> _spectra;
+        private readonly ChromXType _type;
+        private readonly ChromXUnit _unit;
+        private readonly ScanPolarity _polarity;
+        private readonly Dictionary<int, ChromX> _idToChromX;
+
+        public RawSpectra(IReadOnlyList<RawSpectrum> spectra, ChromXType type, ChromXUnit unit, IonMode ionMode) {
+            _type = type;
+            _unit = unit;
+
+            switch (ionMode) {
+                case IonMode.Positive:
+                    _polarity = ScanPolarity.Positive;
+                    break;
+                case IonMode.Negative:
+                    _polarity = ScanPolarity.Negative;
+                    break;
+                default:
+                    throw new ArgumentException($"IonMode {ionMode} is not supported.");
+            }
+
+            _idToChromX = new Dictionary<int, ChromX>();
+            switch (type) {
+                case ChromXType.RT:
+                    _spectra = spectra?.OrderBy(spectrum => spectrum.ScanStartTime).ToList() ?? throw new ArgumentNullException(nameof(spectra));
+                    for (int i = 0; i < _spectra.Count; i++) {
+                        _idToChromX[i] = new RetentionTime(_spectra[i].ScanStartTime, unit);
+                    }
+                    break;
+                case ChromXType.Drift:
+                    _spectra = spectra?.OrderBy(spectrum => spectrum.DriftTime).ToList() ?? throw new ArgumentNullException(nameof(spectra));
+                    for (int i = 0; i < _spectra.Count; i++) {
+                        _idToChromX[i] = new DriftTime(_spectra[i].DriftTime, unit);
+                    }
+                    break;
+                default:
+                    throw new ArgumentException($"ChromXType {type} is not supported.");
+            }
+        }
+
+        public List<ChromatogramPeak> GetMs1Chromatogram(double mz, double tolerance, double start, double end) {
+            int startIndex, endIndex;
+            var results = new List<ChromatogramPeak>();
+            switch (_type) {
+                case ChromXType.RT:
+                    startIndex = _spectra.LowerBound(start, (spectrum, target) => spectrum.ScanStartTime.CompareTo(target));
+                    endIndex = _spectra.UpperBound(end, startIndex, _spectra.Count, (spectrum, target) => spectrum.ScanStartTime.CompareTo(target));
+                    for (int i = startIndex; i < endIndex; i++) {
+                        if (_spectra[i].ScanPolarity != _polarity) {
+                            continue;
+                        }
+                        var (basePeakMz, _, summedIntensity) = new Spectrum(_spectra[i].Spectrum).RetrieveBin(mz, tolerance);
+                        results.Add(new ChromatogramPeak(i, basePeakMz, summedIntensity, _idToChromX[i]));
+                    }
+                    return results;
+                case ChromXType.Drift:
+                    startIndex = _spectra.LowerBound(start, (spectrum, target) => spectrum.DriftTime.CompareTo(target));
+                    endIndex = _spectra.UpperBound(end, startIndex, _spectra.Count, (spectrum, target) => spectrum.DriftTime.CompareTo(target));
+                    for (int i = startIndex; i < endIndex; i++) {
+                        if (_spectra[i].ScanPolarity != _polarity) {
+                            continue;
+                        }
+                        var (basePeakMz, _, summedIntensity) = new Spectrum(_spectra[i].Spectrum).RetrieveBin(mz, tolerance);
+                        results.Add(new ChromatogramPeak(i, basePeakMz, summedIntensity, _idToChromX[i]));
+                    }
+                    return results;
+                default:
+                    throw new NotSupportedException($"{nameof(_type)} {_type} is not supported");
+            }
+        }
+    }
+}
---
+++ MsdialCoreTests/DataObj/RawSpectraTests.cs
@@ -0,0 +1,71 @@
+﻿using CompMs.Common.Components;
+using CompMs.Common.DataObj;
+using CompMs.Common.Enum;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace CompMs.MsdialCore.DataObj.Tests
+{
+    [TestClass]
+    public class RawSpectraTests
+    {
+        [TestMethod]
+        public void GetMs1ChromatogramRtTest() {
+            var spectra = new RawSpectra(new[]
+            {
+                new RawSpectrum { ScanStartTime = 1d, ScanPolarity = ScanPolarity.Positive, Spectrum = new[] { new RawPeakElement{ Mz = 100d, Intensity = 1000d, } } },
+                new RawSpectrum { ScanStartTime = 2d, ScanPolarity = ScanPolarity.Positive, Spectrum = new[] { new RawPeakElement{ Mz = 101d, Intensity = 1001d, } } },
+                new RawSpectrum { ScanStartTime = 3d, ScanPolarity = ScanPolarity.Positive, Spectrum = new[] { new RawPeakElement{ Mz = 102d, Intensity = 1002d, } } },
+                new RawSpectrum { ScanStartTime = 4d, ScanPolarity = ScanPolarity.Positive, Spectrum = new[] { new RawPeakElement{ Mz = 103d, Intensity = 1003d, } } },
+                new RawSpectrum { ScanStartTime = 5d, ScanPolarity = ScanPolarity.Positive, Spectrum = new[] { new RawPeakElement{ Mz = 104d, Intensity = 1004d, } } },
+            },
+            ChromXType.RT,
+            ChromXUnit.Min,
+            IonMode.Positive);
+            var chromatogram = spectra.GetMs1Chromatogram(102, 2d, 2d, 4d);
+            Assert.AreEqual(3, chromatogram.Count);
+            Assert.AreEqual(1, chromatogram[0].ID);
+            Assert.AreEqual(101d, chromatogram[0].Mass);
+            Assert.AreEqual(1001d, chromatogram[0].Intensity);
+            Assert.AreEqual(2d, chromatogram[0].ChromXs.RT.Value);
+            Assert.AreEqual(2, chromatogram[1].ID);
+            Assert.AreEqual(102d, chromatogram[1].Mass);
+            Assert.AreEqual(1002d, chromatogram[1].Intensity);
+            Assert.AreEqual(3d, chromatogram[1].ChromXs.RT.Value);
+            Assert.AreEqual(3, chromatogram[2].ID);
+            Assert.AreEqual(103d, chromatogram[2].Mass);
+            Assert.AreEqual(1003d, chromatogram[2].Intensity);
+            Assert.AreEqual(4d, chromatogram[2].ChromXs.RT.Value);
+        }
+
+        [TestMethod]
+        public void GetMs1ChromatogramDtTest() {
+            var spectra = new RawSpectra(new[]
+            {
+                new RawSpectrum { DriftTime = 1d, ScanPolarity = ScanPolarity.Negative, Spectrum = new[] { new RawPeakElement{ Mz = 100d, Intensity = 1000d, } } },
+                new RawSpectrum { DriftTime = 2d, ScanPolarity = ScanPolarity.Negative, Spectrum = new[] { new RawPeakElement{ Mz = 101d, Intensity = 1001d, } } },
+                new RawSpectrum { DriftTime = 3d, ScanPolarity = ScanPolarity.Negative, Spectrum = new[] { new RawPeakElement{ Mz = 102d, Intensity = 1002d, } } },
+                new RawSpectrum { DriftTime = 4d, ScanPolarity = ScanPolarity.Negative, Spectrum = new[] { new RawPeakElement{ Mz = 103d, Intensity = 1003d, } } },
+                new RawSpectrum { DriftTime = 5d, ScanPolarity = ScanPolarity.Negative, Spectrum = new[] { new RawPeakElement{ Mz = 104d, Intensity = 1004d, } } },
+            },
+            ChromXType.Drift,
+            ChromXUnit.Msec,
+            IonMode.Negative);
+            var chromatogram = spectra.GetMs1Chromatogram(102, 2d, 2d, 4d);
+            Assert.AreEqual(3, chromatogram.Count);
+            Assert.AreEqual(1, chromatogram[0].ID);
+            Assert.AreEqual(101d, chromatogram[0].Mass);
+            Assert.AreEqual(1001d, chromatogram[0].Intensity);
+            Assert.AreEqual(2d, chromatogram[0].ChromXs.Drift.Value);
+            Assert.AreEqual(2, chromatogram[1].ID);
+            Assert.AreEqual(102d, chromatogram[1].Mass);
+            Assert.AreEqual(1002d, chromatogram[1].Intensity);
+            Assert.AreEqual(3d, chromatogram[1].ChromXs.Drift.Value);
+            Assert.AreEqual(3, chromatogram[2].ID);
+            Assert.AreEqual(103d, chromatogram[2].Mass);
+            Assert.AreEqual(1003d, chromatogram[2].Intensity);
+            Assert.AreEqual(4d, chromatogram[2].ChromXs.Drift.Value);
+        }
+    }
+}
