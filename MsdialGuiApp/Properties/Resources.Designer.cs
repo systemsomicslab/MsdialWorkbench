20f9f2a56cbe5a2f4eb8c321e9a088a0909442cf
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2022-05-31 16:02:59 +0900

    Update msdial version properties.

--- MsdialGuiApp/Properties/Resources.Designer.cs
+++ MsdialGuiApp/Properties/Resources.Designer.cs
@@ -79,7 +79,7 @@ namespace CompMs.App.Msdial.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 5.0.1-alpha.
+        ///   Looks up a localized string similar to 5.0.3.
         /// </summary>
         public static string VERSION {
             get {
