8732fabb3cb318cda6aa2d95066e257966daf608
mikiko.takahashi <mikiko.takahashi@riken.jp>
2022-06-01 18:06:21 +0900

    modify SpectrumPeakGenerator(Hgain Hloss intensity related double bond position) and fix CLLipidParser

--- CommonStandard/Lipidomics/CLLipidParser.cs
+++ CommonStandard/Lipidomics/CLLipidParser.cs
@@ -56,15 +56,15 @@ namespace CompMs.Common.Lipidomics
                         var chains = chainsParser.Parse(groupSub["sn1sn2"].Value + "_" + groupSub["sn3sn4"].Value);
                         return new Lipid(LbmClass.CL, Skelton + chains.Mass, chains);
                     }
-                    else
-                    {
-                        var carbon = int.Parse(groupSub["carbon"].Captures[0].Value) + int.Parse(groupSub["carbon"].Captures[1].Value);
-                        var db = int.Parse(groupSub["db"].Captures[0].Value) + int.Parse(groupSub["db"].Captures[1].Value);
-                        var ox = !groupSub["ox"].Success ? 0 : !groupSub["oxnum"].Success ? 1 : int.Parse(groupSub["oxnum"].Value);
-                        var chains = chainsParser.Parse("CL "+ carbon + ":" + db + ";" + ox);
-                        return new Lipid(LbmClass.CL, Skelton + chains.Mass, chains);
+                    //else
+                    //{
+                    //    var carbon = int.Parse(groupSub["carbon"].Captures[0].Value) + int.Parse(groupSub["carbon"].Captures[1].Value);
+                    //    var db = int.Parse(groupSub["db"].Captures[0].Value) + int.Parse(groupSub["db"].Captures[1].Value);
+                    //    var ox = !groupSub["ox"].Success ? 0 : !groupSub["oxnum"].Success ? 1 : int.Parse(groupSub["oxnum"].Value);
+                    //    var chains = chainsParser.Parse("CL " + carbon + ":" + db + ";" + ox);
+                    //    return new Lipid(LbmClass.CL, Skelton + chains.Mass, chains);
 
-                    }
+                    //}
                 }
             }
             return null;
