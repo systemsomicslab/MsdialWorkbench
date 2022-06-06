cbcb2ba8691b234fc4cb2c663d16757d0a94d811
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2022-06-06 15:21:16 +0900

    Add ribbon tab class for bar chart property setting.

---
+++ MsdialGuiApp/View/ChartSetting/BarChartOfAlignedSpotSettingRibbonTab.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls.Ribbon;
+
+namespace CompMs.App.Msdial.View.ChartSetting
+{
+    /// <summary>
+    /// Interaction logic for BarChartOfAlignedSpotSettingRibbonTab.xaml
+    /// </summary>
+    public partial class BarChartOfAlignedSpotSettingRibbonTab : RibbonTab
+    {
+        public BarChartOfAlignedSpotSettingRibbonTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
