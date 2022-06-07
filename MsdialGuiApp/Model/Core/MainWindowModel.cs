574bf6021147c573f279f4ebed3270ea0407dd55
Yoshimasa TODOROKI <s196482u@st.me.tuat.ac.jp>
2022-06-07 17:10:28 +0900

    Merge "origin/yuki.matsuzawa" into todoY.

--- MsdialGuiApp/Model/Core/MainWindowModel.cs
+++ MsdialGuiApp/Model/Core/MainWindowModel.cs
@@ -13,11 +13,15 @@ namespace CompMs.App.Msdial.Model.Core
             ProjectSetting = new ProjectSettingModel(SetNewProject, broker);
             nowSaving = new BusyNotifier();
             _broker = broker;
+            nowLoading = new BusyNotifier();
         }
 
         public IObservable<bool> NowSaving => nowSaving;
         private readonly BusyNotifier nowSaving;
         private readonly IMessageBroker _broker;
+
+        public IObservable<bool> NowLoading => nowLoading;
+        private readonly BusyNotifier nowLoading;
 
         public IProjectModel CurrentProject {
             get => currentProject;
@@ -56,17 +60,19 @@ namespace CompMs.App.Msdial.Model.Core
         }
 
         public async Task LoadAsync() {
-            try {
-                var loadedProject = await ProjectModel.LoadAsync(_broker).ConfigureAwait(true);
-                if (!(loadedProject is null)) {
-                    CurrentProject = loadedProject;
+            using (nowLoading.ProcessStart()) {
+                try {
+                    var loadedProject = await ProjectModel.LoadAsync(_broker).ConfigureAwait(true);
+                    if (!(loadedProject is null)) {
+                        CurrentProject = loadedProject;
+                    }
                 }
-            }
-            catch {
-                await Application.Current.Dispatcher.InvokeAsync(() => {
-                    MessageBox.Show("Failed to load project.\nPlease check your project.");
-                    return Task.CompletedTask;
-                });
+                catch {
+                    await Application.Current.Dispatcher.InvokeAsync(() => {
+                        MessageBox.Show("Failed to load project.\nPlease check your project.");
+                        return Task.CompletedTask;
+                    });
+                }
             }
         }
     }
