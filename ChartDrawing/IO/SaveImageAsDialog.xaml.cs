fb559b1f88c31dc816eb871780831f0110259cf7
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2022-06-13 17:08:46 +0900

    Add saving image dialog.

---
+++ ChartDrawing/IO/SaveImageAsDialog.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+
+namespace CompMs.Graphics.IO
+{
+    /// <summary>
+    /// Interaction logic for SaveImageAsDialog.xaml
+    /// </summary>
+    public partial class SaveImageAsDialog : System.Windows.Window
+    {
+        public SaveImageAsDialog() {
+            InitializeComponent();
+
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, (s, e) => Close()));
+        }
+    }
+}
---
+++ ChartDrawing/IO/SaveImageAsDialogViewModel.cs
@@ -0,0 +1,52 @@
+﻿using CompMs.CommonMVVM;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CompMs.Graphics.IO
+{
+    internal class SaveImageAsDialogViewModel : ViewModelBase
+    {
+        private readonly SaveImageAsDialogModel _model;
+
+        public SaveImageAsDialogViewModel(SaveImageAsDialogModel model) {
+            _model = model ?? throw new ArgumentNullException(nameof(model));
+            Path = model.Path;
+            DpiX = model.DpiX.ToString();
+            DpiY = model.DpiY.ToString();
+        }
+
+        [Required(ErrorMessage = "Path is empty.")]
+        public string Path {
+            get => _path;
+            set {
+                if (SetProperty(ref _path, value)) {
+                    _model.Path = value;
+                }
+            }
+        }
+        private string _path = string.Empty;
+
+        [Required(ErrorMessage = "DpiX is empty.")]
+        [RegularExpression(@"\d*\.?\d+", ErrorMessage = "Invalid format")]
+        public string DpiX {
+            get => _dpiX;
+            set {
+                if (SetProperty(ref _dpiX, value)) {
+                    _model.DpiX = double.Parse(value);
+                }
+            }
+        }
+        private string _dpiX;
+
+        [Required(ErrorMessage = "DpiY is empty.")]
+        [RegularExpression(@"\d*\.?\d+", ErrorMessage = "Invalid format")]
+        public string DpiY {
+            get => _dpiY;
+            set => SetProperty(ref _dpiY, value);
+        }
+        private string _dpiY;
+
+        public DelegateCommand SaveCommand => _saveCommand ?? (_saveCommand = new DelegateCommand(() => { _ = _model.ExportAsync(); }));
+        private DelegateCommand _saveCommand;
+    }
+}
