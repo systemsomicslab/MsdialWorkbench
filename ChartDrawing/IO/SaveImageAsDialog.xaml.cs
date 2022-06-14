d7742d98cf1b0913ba06a81bc45e56a88e2b3cc7
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2022-06-14 18:03:29 +0900

    Implement selecting save file dialog to SaveImageAsDialog.

--- ChartDrawing/IO/SaveImageAsDialog.xaml.cs
+++ ChartDrawing/IO/SaveImageAsDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using Microsoft.Win32;
+using System.Windows.Input;
 
 namespace CompMs.Graphics.IO
 {
@@ -11,10 +12,27 @@ namespace CompMs.Graphics.IO
             InitializeComponent();
 
             CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, Click_close));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.SaveAs, Select_export_file));
         }
 
         private void Click_close(object sender, System.Windows.RoutedEventArgs e) {
             Close();
+        }
+
+        private void Select_export_file(object sender, System.Windows.RoutedEventArgs e) {
+            var sfd = new SaveFileDialog
+            {
+                RestoreDirectory = true,
+                DefaultExt = ".emf",
+                Filter = string.Join("|", new[]
+                {
+                    "Extended Metafile Format(.emf)|*.emf",
+                    "PNG image(.png)|*.png",
+                }),
+            };
+            if (sfd.ShowDialog() == true) {
+                ExportPath_TextBox.Text = sfd.FileName;
+            }
         }
     }
 }
