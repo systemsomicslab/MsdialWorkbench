df0f92545905f5db671d96e61b5fbbe306ee1ca5
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2022-06-27 10:58:17 +0900

    Implement selecting class for ms spectrum.

---
+++ MsdialGuiApp/Model/Loader/MsSelectionItem.cs
@@ -0,0 +1,13 @@
+﻿namespace CompMs.App.Msdial.Model.Loader
+{
+    internal sealed class MsSelectionItem
+    {
+        public MsSelectionItem(int id, double collisionEnergy) {
+            Id = id;
+            CollisionEnergy = collisionEnergy;
+        }
+
+        public int Id { get; }
+        public double CollisionEnergy { get; }
+    }
+}
