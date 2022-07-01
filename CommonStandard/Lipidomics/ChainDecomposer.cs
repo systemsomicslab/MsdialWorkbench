7a968e86b803d736e92a78da0ebf11c2de39e242
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2022-07-01 18:50:10 +0900

    Fix EadMsCharacterizationUtility bug.

--- CommonStandard/Lipidomics/ChainDecomposer.cs
+++ CommonStandard/Lipidomics/ChainDecomposer.cs
@@ -2,29 +2,36 @@
 
 namespace CompMs.Common.Lipidomics
 {
-    internal class ChainDecomposer<TResult> : IDecomposer<TResult, IChain, (IChain, IDoubleBond, IOxidized)>, IDecomposer<IDoubleBond, IChain, IDoubleBond>, IDecomposer<IOxidized, IChain, IOxidized>
+    internal class ChainDecomposer<TResult> : IDecomposer<TResult, IChain, (IChain, IDoubleBond, IOxidized)>
     {
         public TResult Decompose(IAcyclicVisitor visitor, IChain element) {
             if (visitor is IVisitor<TResult, (IChain, IDoubleBond, IOxidized)> chainVisitor) {
-                var db = element.Accept(visitor, (IDecomposer<IDoubleBond, IChain, IDoubleBond>)this) ?? element.DoubleBond;
-                var ox = element.Accept(visitor, (IDecomposer<IOxidized, IChain, IOxidized>)this) ?? element.Oxidized;
+                var db = element.Accept(visitor, DB_DECOMPOSER) ?? element.DoubleBond;
+                var ox = element.Accept(visitor, OX_DECOMPOSER) ?? element.Oxidized;
                 return chainVisitor.Visit((element, db, ox));
             }
             return default;
         }
 
-        IDoubleBond IDecomposer<IDoubleBond, IChain, IDoubleBond>.Decompose(IAcyclicVisitor visitor, IChain element) {
-            if (visitor is IVisitor<IDoubleBond, IDoubleBond> dbVisitor) {
-                return dbVisitor.Visit(element.DoubleBond);
+        private static readonly DoubleBondDecomposer DB_DECOMPOSER = new DoubleBondDecomposer();
+        private static readonly OxidizedDecomposer OX_DECOMPOSER = new OxidizedDecomposer();
+
+        sealed class DoubleBondDecomposer : IDecomposer<IDoubleBond, IChain, IDoubleBond> {
+            IDoubleBond IDecomposer<IDoubleBond, IChain, IDoubleBond>.Decompose(IAcyclicVisitor visitor, IChain element) {
+                if (visitor is IVisitor<IDoubleBond, IDoubleBond> dbVisitor) {
+                    return dbVisitor.Visit(element.DoubleBond);
+                }
+                return default;
             }
-            return default;
         }
 
-        IOxidized IDecomposer<IOxidized, IChain, IOxidized>.Decompose(IAcyclicVisitor visitor, IChain element) {
-            if (visitor is IVisitor<IOxidized, IOxidized> dbVisitor) {
-                return dbVisitor.Visit(element.Oxidized);
+        sealed class OxidizedDecomposer : IDecomposer<IOxidized, IChain, IOxidized> {
+            IOxidized IDecomposer<IOxidized, IChain, IOxidized>.Decompose(IAcyclicVisitor visitor, IChain element) {
+                if (visitor is IVisitor<IOxidized, IOxidized> dbVisitor) {
+                    return dbVisitor.Visit(element.Oxidized);
+                }
+                return default;
             }
-            return default;
         }
     }
 }
