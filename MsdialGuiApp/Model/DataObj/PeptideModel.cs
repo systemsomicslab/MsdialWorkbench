4420132bfc90e1e32ce9b4af55b237c3cbc9e0fa
Kozo Nishida <kozo.nishida@gmail.com>
2022-07-11 20:32:35 +0900

    Add Protein/Peptide* model skeltons

---
+++ MsdialGuiApp/Model/DataObj/PeptideModel.cs
@@ -0,0 +1,25 @@
+﻿using CompMs.MsdialCore.DataObj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompMs.App.Msdial.Model.DataObj
+{
+    internal sealed class PeptideModel {
+        public object AnnotatedSpot { get; }
+
+        public PeptideModel(PeptideMsResult peptideMsResult, IReadOnlyList<ChromatogramPeakFeatureModel> spots)
+        {
+            AnnotatedSpot = spots.FirstOrDefault(spot => spot.InnerModel == peptideMsResult.ChromatogramPeakFeature);
+            results = PeptideMsResult;
+            Console.WriteLine(results);
+        }
+
+        public PeptideModel(PeptideMsResult peptideMsResult, IReadOnlyList<AlignmentSpotPropertyModel> spots)
+        {
+            AnnotatedSpot = spots.FirstOrDefault(spot => spot.innerModel == peptideMsResult.AlignmentSpotProperty);
+            results = PeptideMsResult;
+            Console.WriteLine(results);
+        }
+    }
+}
---
+++ MsdialGuiApp/Model/DataObj/ProteinResultContainerModel.cs
@@ -0,0 +1,27 @@
+﻿using CompMs.CommonMVVM;
+using CompMs.MsdialCore.DataObj;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CompMs.App.Msdial.Model.DataObj
+{
+    internal sealed class ProteinResultContainerModel : BindableBase
+    {
+        private readonly ProteinResultContainer _resultContainer;
+        private readonly ObservableCollection<ProteinGroupModel> _proteinGroups;
+
+        public ProteinResultContainerModel(ProteinResultContainer resultContainer, IReadOnlyList<ChromatogramPeakFeatureModel> spots)
+        {
+            _resultContainer = resultContainer;
+            _proteinGroups = new ObservableCollection<ProteinGroupModel>(resultContainer.ProteinGroups.Select(group => new ProteinGroupModel(group, spots)));
+
+        }
+
+        public ProteinResultContainerModel(ProteinResultContainer resultContainer, IReadOnlyList<AlignmentSpotPropertyModel> spots)
+        {
+            _resultContainer = resultContainer;
+            _proteinGroups = new ObservableCollection<ProteinGroupModel>(resultContainer.ProteinGroups.Select(group => new ProteinGroupModel(group, spots)));
+        }
+    }
+}
