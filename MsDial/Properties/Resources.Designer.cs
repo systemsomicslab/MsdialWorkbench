7309d4819c3258146914562060ade5f75971fc19
Hiroshi_Tsugawa <hiroshi.tsugawa@riken.jp>
2022-07-16 09:45:24 +0900

    fixed waters raw data parser

--- MsDial/Properties/Resources.Designer.cs
+++ MsDial/Properties/Resources.Designer.cs
@@ -88,7 +88,7 @@ namespace Rfx.Riken.OsakaUniv.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to MS-DIAL ver. 4.90.
+        ///   Looks up a localized string similar to MS-DIAL ver. 4.90-dev.
         /// </summary>
         internal static string VERSION {
             get {
