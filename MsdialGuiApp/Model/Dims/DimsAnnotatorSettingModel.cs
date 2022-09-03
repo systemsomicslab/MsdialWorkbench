59d4f0fd5bacb33120ae28147fd1f39f9ce78caf
Hiroshi_Tsugawa <hiroshi.tsugawa@riken.jp>
2022-09-03 22:39:57 +0900

    modified default parameters of di-ms

--- MsdialGuiApp/Model/Dims/DimsAnnotatorSettingModel.cs
+++ MsdialGuiApp/Model/Dims/DimsAnnotatorSettingModel.cs
@@ -16,7 +16,24 @@ namespace CompMs.App.Msdial.Model.Dims
         public DimsMetabolomicsUseMs2AnnotatorSettingModel(DataBaseSettingModel dataBaseSettingModel, string annotatorID, MsRefSearchParameterBase searchParameter) {
             DataBaseSettingModel = dataBaseSettingModel;
             AnnotatorID = annotatorID;
-            SearchParameter = searchParameter ?? new MsRefSearchParameterBase();
+            if (dataBaseSettingModel.DBSource == DataBaseSource.Msp) {
+                SearchParameter = searchParameter ?? new MsRefSearchParameterBase {
+                    SimpleDotProductCutOff = 0.5F,
+                    WeightedDotProductCutOff = 0.5F,
+                    ReverseDotProductCutOff = 0.7F,
+                    MatchedPeaksPercentageCutOff = 0.3F,
+                    MinimumSpectrumMatch = 3,
+                };
+            }
+            else { // meaning lbm
+                SearchParameter = searchParameter ?? new MsRefSearchParameterBase {
+                    SimpleDotProductCutOff = 0.1F,
+                    WeightedDotProductCutOff = 0.1F,
+                    ReverseDotProductCutOff = 0.1F,
+                    MatchedPeaksPercentageCutOff = 0.0F,
+                    MinimumSpectrumMatch = 1
+                };
+            }
         }
 
         public MsRefSearchParameterBase SearchParameter { get; }
