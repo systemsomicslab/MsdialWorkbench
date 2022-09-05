5ab92d9d5cba7baf5152b224649b4a2b45cf57fe
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2022-09-05 18:28:05 +0900

    Add main view for Imaging ms.

---
+++ MsdialGuiApp/View/Imaging/ImagingMainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace CompMs.App.Msdial.View.Imaging
+{
+    /// <summary>
+    /// Interaction logic for ImagingMainView.xaml
+    /// </summary>
+    public partial class ImagingMainView : UserControl
+    {
+        public ImagingMainView() {
+            InitializeComponent();
+        }
+    }
+}
