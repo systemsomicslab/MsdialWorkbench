7767baac6142a381c3951b43cc40d3da6b95f3d6
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2022-10-05 15:11:58 +0900

    Improve ColorPicker.

--- ChartDrawing/Template/ComboBoxItemTemplateSelector.cs
+++ ChartDrawing/Template/ComboBoxItemTemplateSelector.cs
@@ -4,20 +4,14 @@ using System.Windows.Media;
 
 namespace CompMs.Graphics.Template
 {
-    // from https://meleak.wordpress.com/2012/05/13/different-combobox-itemtemplate-for-dropdown/
+    // from https://wpf.2000things.com/2014/02/19/1012-using-a-different-data-template-for-the-face-of-a-combobox/
     internal sealed class ComboBoxItemTemplateSelector : DataTemplateSelector
     {
         public DataTemplate SelectedTemplate { get; set; }
         public DataTemplate DropDownTemplate { get; set; }
 
         public override DataTemplate SelectTemplate(object item, DependencyObject container) {
-            var ancestor = container;
-            while (!(ancestor is null) && !(ancestor is ComboBox) && !(ancestor is ComboBoxItem)) {
-                ancestor = VisualTreeHelper.GetParent(ancestor);
-            }
-            return ancestor is ComboBoxItem
-                ? DropDownTemplate
-                : SelectedTemplate;
+            return container is FrameworkElement fe && fe.TemplatedParent is ComboBox ? SelectedTemplate : DropDownTemplate;
         }
     }
 }
