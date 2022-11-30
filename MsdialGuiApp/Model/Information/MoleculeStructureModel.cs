dfce2d99bb7228658eec1ee1793c6a9e253f3702
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2022-11-30 16:57:24 +0900

    Add structure view.

---
+++ MsdialGuiApp/Model/Information/MoleculeStructureModel.cs
@@ -0,0 +1,87 @@
+﻿using CompMs.Common.DataStructure;
+using CompMs.Common.Interfaces;
+using CompMs.CommonMVVM;
+using NCDK.Depict;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace CompMs.App.Msdial.Model.Information
+{
+    internal sealed class MoleculeStructureModel : DisposableModelBase
+    {
+        private CacheProxy<IMoleculeProperty, MoleculeImage> _proxy;
+
+        public MoleculeStructureModel() {
+            _proxy = new CacheProxy<IMoleculeProperty, MoleculeImage>(20, MoleculeImage.Create);
+        }
+
+        public MoleculeImage Current {
+            get => _current;
+            private set => SetProperty(ref _current, value);
+        }
+        private MoleculeImage _current;
+
+        public void UpdateMolecule(IMoleculeProperty molecule) {
+            if (molecule is null) {
+                Current = MoleculeImage.FailedMoleculeImage;
+                return;
+            }
+            Current = _proxy?.GetOrAdd(molecule) ?? MoleculeImage.FailedMoleculeImage;
+        }
+
+        protected override void Dispose(bool disposing) {
+            base.Dispose(disposing);
+            _proxy = null;
+            Current = null;
+        }
+    }
+
+    internal sealed class MoleculeImage : BindableBase {
+        public static readonly MoleculeImage FailedMoleculeImage = new MoleculeImage();
+
+        private MoleculeImage() {
+            Image = Task.FromResult((BitmapSource)null);
+            IsFailed = true;
+        }
+
+        public MoleculeImage(IMoleculeProperty molecule) {
+            Image = Task.Run(() =>
+            {
+                IsLoading = true;
+
+                try {
+                    var atomContainer = molecule.ToAtomContainer();
+                    var depiction = new DepictionGenerator().Depict(atomContainer);
+                    BitmapSource bitmapSource = depiction.ToBitmap();
+                    bitmapSource.Freeze();
+                    return bitmapSource;
+                }
+                catch {
+                    IsFailed = true;
+                    return null;
+                }
+                finally {
+                    IsLoading = false;
+                }
+            });
+        }
+
+        public Task<BitmapSource> Image { get; }
+
+        public bool IsLoading {
+            get => _isLoading;
+            private set => SetProperty(ref _isLoading, value);
+        }
+        private bool _isLoading = false;
+
+        public bool IsFailed {
+            get => _isFailed;
+            private set => SetProperty(ref _isFailed, value);
+        }
+        private bool _isFailed = false;
+
+        public static MoleculeImage Create(IMoleculeProperty molecule) {
+            return new MoleculeImage(molecule);
+        }
+    }
+}
---
+++ MsdialGuiApp/ViewModel/Information/MoleculeStructureViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CompMs.App.Msdial.Model.Information;
+using CompMs.CommonMVVM;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Windows.Media.Imaging;
+
+namespace CompMs.App.Msdial.ViewModel.Information
+{
+    internal sealed class MoleculeStructureViewModel : ViewModelBase
+    {
+        private readonly MoleculeStructureModel _model;
+
+        public MoleculeStructureViewModel(MoleculeStructureModel model) {
+            _model = model ?? throw new ArgumentNullException(nameof(model));
+            var current = model.ObserveProperty(m => m.Current).ToReactiveProperty().AddTo(Disposables);
+            Image = current.Select(c => Observable.FromAsync(() => c.Image).StartWith((BitmapSource)null)).Switch()
+                .ToReadOnlyReactivePropertySlim()
+                .AddTo(Disposables);
+            IsLoading = current.Select(c => c.ObserveProperty(m => m.IsLoading)).Switch()
+                .ToReadOnlyReactivePropertySlim()
+                .AddTo(Disposables);
+            IsFailed = current.Select(c => c.ObserveProperty(m => m.IsFailed)).Switch()
+                .ToReactiveProperty()
+                .AddTo(Disposables);
+        }
+
+        public ReadOnlyReactivePropertySlim<BitmapSource> Image { get; }
+        public ReadOnlyReactivePropertySlim<bool> IsLoading { get; }
+        public IReadOnlyReactiveProperty<bool> IsFailed { get; }
+    }
+}
