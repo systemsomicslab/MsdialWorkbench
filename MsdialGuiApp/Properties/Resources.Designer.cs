20ab8d4708ccd6a589b6c37b4326817cca07cd8e
Hiroshi_Tsugawa <hiroshi.tsugawa@riken.jp>
2022-12-26 09:56:23 +0900

    modified spectrum generator

--- MsdialGuiApp/Properties/Resources.Designer.cs
+++ MsdialGuiApp/Properties/Resources.Designer.cs
@@ -88,7 +88,7 @@ namespace CompMs.App.Msdial.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 5.1.221224.
+        ///   Looks up a localized string similar to 5.1.221224-dev.
         /// </summary>
         public static string VERSION {
             get {
