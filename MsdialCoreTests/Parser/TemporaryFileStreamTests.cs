74e2da40728f8f2ec0928184a6f55842d4d1eebd
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-01-13 10:53:40 +0900

    Add TemporaryFileStream class for temporary save and move file after writring contents.

---
+++ MsdialCoreTests/Parser/TemporaryFileStreamTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace CompMs.MsdialCore.Parser.Tests
+{
+    [TestClass()]
+    public class TemporaryFileStreamTests
+    {
+        [TestMethod()]
+        public void MoveTest() {
+            var filePath = Path.GetTempFileName();
+
+            using (var stream = new TemporaryFileStream(filePath)) {
+                using (var writer = new StreamWriter(stream, leaveOpen: true)) {
+                    writer.WriteLine("ABC");
+                    writer.WriteLine("XYZ");
+                }
+                stream.Move();
+            }
+            Assert.IsTrue(File.Exists(filePath));
+
+            using (var stream = File.Open(filePath, FileMode.Open))
+            using (var reader = new StreamReader(stream)) {
+                Assert.AreEqual("ABC", reader.ReadLine());
+                Assert.AreEqual("XYZ", reader.ReadLine());
+            }
+        }
+    }
+}
