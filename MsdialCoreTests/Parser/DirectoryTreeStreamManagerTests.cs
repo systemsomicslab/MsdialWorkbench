98017d2839b2418d2464d9c8076b335c40d0e53f
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-01-13 14:04:11 +0900

    Change DirectoryTreeStreamManager class.
    Save the file as a temporary file and move these files when disposing.

--- MsdialCoreTests/Parser/DirectoryTreeStreamManagerTests.cs
+++ MsdialCoreTests/Parser/DirectoryTreeStreamManagerTests.cs
@@ -11,56 +11,60 @@ namespace CompMs.MsdialCore.Parser.Tests
         [TestMethod()]
         public async Task DirectoryTreeStreamManagerTest() {
             var folder = Path.GetTempPath();
-            IStreamManager manager = new DirectoryTreeStreamManager(folder);
 
-            var s = await manager.Create("ABC");
-            var t = manager.Create("XYZ");
-            Assert.IsTrue(t.IsCompleted);
+            using (IStreamManager manager = new DirectoryTreeStreamManager(folder)) {
+                var s = await manager.Create("ABC");
+                var t = manager.Create("XYZ");
+                Assert.IsTrue(t.IsCompleted);
 
-            s.Write(Encoding.UTF8.GetBytes("abc"));
-            s.Close();
+                s.Write(Encoding.UTF8.GetBytes("abc"));
+                s.Close();
 
-            s = await t;
-            s.Write(Encoding.UTF8.GetBytes("xyz"));
-            manager.Release(s);
+                s = await t;
+                s.Write(Encoding.UTF8.GetBytes("xyz"));
+                manager.Release(s);
 
-            var subManager = manager.Join("DEF");
-            var u =  await subManager.Create("GHI");
-            u.Write(Encoding.UTF8.GetBytes("ghi"));
-            u.Close();
+                var subManager = manager.Join("DEF");
+                var u =  await subManager.Create("GHI");
+                u.Write(Encoding.UTF8.GetBytes("ghi"));
+                u.Close();
 
-            var v =  await manager.Create("JKL");
-            v.Write(Encoding.UTF8.GetBytes("jkl"));
-            v.Close();
+                var v =  await manager.Create("JKL");
+                v.Write(Encoding.UTF8.GetBytes("jkl"));
+                v.Close();
+            }
 
             Assert.IsTrue(File.Exists(Path.Combine(folder, "ABC")));
             Assert.IsTrue(File.Exists(Path.Combine(folder, "XYZ")));
             Assert.IsTrue(File.Exists(Path.Combine(folder, "DEF", "GHI")));
             Assert.IsTrue(File.Exists(Path.Combine(folder, "JKL")));
 
-            s = await manager.Get("ABC");
-            t = manager.Get("XYZ");
-            Assert.IsTrue(t.IsCompleted);
+            using (IStreamManager manager = new DirectoryTreeStreamManager(folder)) {
+                var s = await manager.Get("ABC");
+                var t = manager.Get("XYZ");
+                Assert.IsTrue(t.IsCompleted);
 
-            var buffer = new byte[3];
-            s.Read(buffer, 0, buffer.Length);
-            Assert.AreEqual("abc", Encoding.UTF8.GetString(buffer));
-            s.Dispose();
+                var buffer = new byte[3];
+                s.Read(buffer, 0, buffer.Length);
+                Assert.AreEqual("abc", Encoding.UTF8.GetString(buffer));
+                s.Dispose();
 
-            s = await t;
-            s.Read(buffer, 0, buffer.Length);
-            Assert.AreEqual("xyz", Encoding.UTF8.GetString(buffer));
-            s.Close();
+                s = await t;
+                s.Read(buffer, 0, buffer.Length);
+                Assert.AreEqual("xyz", Encoding.UTF8.GetString(buffer));
+                s.Close();
 
-            u = await subManager.Get("GHI");
-            u.Read(buffer, 0, buffer.Length);
-            Assert.AreEqual("ghi", Encoding.UTF8.GetString(buffer));
-            u.Close();
+                var subManager = manager.Join("DEF");
+                var u = await subManager.Get("GHI");
+                u.Read(buffer, 0, buffer.Length);
+                Assert.AreEqual("ghi", Encoding.UTF8.GetString(buffer));
+                u.Close();
 
-            v = await manager.Get("JKL");
-            v.Read(buffer, 0, buffer.Length);
-            Assert.AreEqual("jkl", Encoding.UTF8.GetString(buffer));
-            v.Close();
+                var v = await manager.Get("JKL");
+                v.Read(buffer, 0, buffer.Length);
+                Assert.AreEqual("jkl", Encoding.UTF8.GetString(buffer));
+                v.Close();
+            }
 
             File.Delete(Path.Combine(folder, "ABC"));
             File.Delete(Path.Combine(folder, "XYZ"));
