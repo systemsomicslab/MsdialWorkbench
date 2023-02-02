d0561dd767aeb4c885c5816d9e78007aba68c074
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-02 16:22:48 +0900

    Add GcmsAnalysisModel class.

---
+++ src/MSDIAL5/MsdialGuiApp/Model/Gcms/GcmsAnalysisModel.cs
@@ -0,0 +1,45 @@
+﻿using CompMs.App.Msdial.Model.Core;
+using CompMs.App.Msdial.Model.DataObj;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CompMs.App.Msdial.Model.Gcms
+{
+    internal sealed class GcmsAnalysisModel : IAnalysisModel
+    {
+        ObservableCollection<ChromatogramPeakFeatureModel> IAnalysisModel.Ms1Peaks => throw new NotImplementedException();
+
+        IReactiveProperty<ChromatogramPeakFeatureModel> IAnalysisModel.Target => throw new NotImplementedException();
+
+        string IAnalysisModel.DisplayLabel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged {
+            add {
+                throw new NotImplementedException();
+            }
+
+            remove {
+                throw new NotImplementedException();
+            }
+        }
+
+        void IResultModel.InvokeMsfinder() {
+            throw new NotImplementedException();
+        }
+
+        Task IAnalysisModel.SaveAsync(CancellationToken token) {
+            throw new NotImplementedException();
+        }
+
+        void IResultModel.SearchFragment() {
+            throw new NotImplementedException();
+        }
+    }
+}
