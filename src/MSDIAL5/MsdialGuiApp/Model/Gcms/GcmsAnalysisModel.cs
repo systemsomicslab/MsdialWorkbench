f418ee50d63d4d1dcd94a77f991e84f86de47cb3
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-03 18:53:47 +0900

    Change GcmsAnalysisModel

--- src/MSDIAL5/MsdialGuiApp/Model/Gcms/GcmsAnalysisModel.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/Gcms/GcmsAnalysisModel.cs
@@ -1,28 +1,20 @@
 ﻿using CompMs.App.Msdial.Model.Core;
+using CompMs.CommonMVVM;
 using System;
-using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace CompMs.App.Msdial.Model.Gcms
 {
-    internal sealed class GcmsAnalysisModel : IAnalysisModel
+    internal sealed class GcmsAnalysisModel : BindableBase, IAnalysisModel
     {
-        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged {
-            add {
-                throw new NotImplementedException();
-            }
-
-            remove {
-                throw new NotImplementedException();
-            }
-        }
-
-        void IResultModel.InvokeMsfinder() {
+        // IAnalysisModel interface
+        Task IAnalysisModel.SaveAsync(CancellationToken token) {
             throw new NotImplementedException();
         }
 
-        Task IAnalysisModel.SaveAsync(CancellationToken token) {
+        // IResultModel interface
+        void IResultModel.InvokeMsfinder() {
             throw new NotImplementedException();
         }
 
