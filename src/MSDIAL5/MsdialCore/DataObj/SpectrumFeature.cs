21805b6b70b802d2330b76f126c711148418f6bf
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-09 17:39:17 +0900

    Change SpectrumFeature.

--- src/MSDIAL5/MsdialCore/DataObj/SpectrumFeature.cs
+++ src/MSDIAL5/MsdialCore/DataObj/SpectrumFeature.cs
@@ -1,12 +1,28 @@
-﻿using CompMs.Common.Interfaces;
+﻿using CompMs.Common.Components;
+using CompMs.Common.Interfaces;
 
 namespace CompMs.MsdialCore.DataObj
 {
     public sealed class SpectrumFeature
     {
-        public IMSScanProperty Scan { get; }
+        private static readonly IMoleculeProperty UNKNOWN_MOLECULE = new MoleculeProperty();
+
+        private readonly IMoleculeProperty _molecule;
+
+        public SpectrumFeature(double quantMass, IMSScanProperty scan, IMoleculeProperty molecule) {
+            QuantMass = quantMass;
+            Scan = scan;
+            _molecule = molecule;
+            MatchResults = new MsScanMatchResultContainer();
+        }
+
+        public SpectrumFeature(double quantMass, IMSScanProperty scan) : this(quantMass, scan, null) {
+
+        }
+
         public double QuantMass { get; }
-        public IMoleculeProperty Molecule { get; }
+        public IMSScanProperty Scan { get; }
+        public IMoleculeProperty Molecule => _molecule ?? UNKNOWN_MOLECULE;
         public MsScanMatchResultContainer MatchResults { get; }
     }
 }
