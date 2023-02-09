d32cf1ef3e85f2d590f66f8cf4782088e0c31357
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-09 16:44:49 +0900

    Add a SpectrumFeature class equivalent to ChromatogramPeakFeature.

---
+++ src/MSDIAL5/MsdialCore/DataObj/SpectrumFeature.cs
@@ -0,0 +1,12 @@
+﻿using CompMs.Common.Interfaces;
+
+namespace CompMs.MsdialCore.DataObj
+{
+    public sealed class SpectrumFeature
+    {
+        public IMSScanProperty Scan { get; }
+        public double QuantMass { get; }
+        public IMoleculeProperty Molecule { get; }
+        public MsScanMatchResultContainer MatchResults { get; }
+    }
+}
