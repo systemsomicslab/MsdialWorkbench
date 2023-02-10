0c230315502abcb6dafa1cc33758ebb3df5830ed
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-10 10:41:33 +0900

    Add serialization test for BaseChroamtogramPeakFeature.

---
+++ tests/Common/CommonStandardTests/Components/BaseChromatogramPeakFeatureTests.cs
@@ -0,0 +1,30 @@
+﻿using CompMs.Common.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace CompMs.Common.Components.Tests
+{
+    [TestClass()]
+    public class BaseChromatogramPeakFeatureTests
+    {
+        [TestMethod()]
+        public void SerializationAndDeserializationTest() {
+            var memory = new MemoryStream();
+            BaseChromatogramPeakFeature peak = new BaseChromatogramPeakFeature();
+            MessagePack.MessagePackDefaultHandler.SaveToStream(peak, memory);
+            memory.Seek(0, SeekOrigin.Begin);
+            var actual = MessagePack.MessagePackDefaultHandler.LoadFromStream<BaseChromatogramPeakFeature>(memory);
+            Assert.That.AreEqual(peak, actual);
+        }
+
+        [TestMethod()]
+        public void SerializationAndDeserializationAsInterfaceTest() {
+            var memory = new MemoryStream();
+            IChromatogramPeakFeature peak = new BaseChromatogramPeakFeature();
+            MessagePack.MessagePackDefaultHandler.SaveToStream(peak, memory);
+            memory.Seek(0, SeekOrigin.Begin);
+            var actual = MessagePack.MessagePackDefaultHandler.LoadFromStream<IChromatogramPeakFeature>(memory);
+            Assert.That.AreEqual(peak, actual);
+        }
+    }
+}
