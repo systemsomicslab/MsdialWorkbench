cae80255fa4efc4d49ab95a42255d8d2687e839c
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-22 09:59:23 +0900

    Change GcmsMethodModel.

--- src/MSDIAL5/MsdialGuiApp/Model/Gcms/GcmsMethodModel.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/Gcms/GcmsMethodModel.cs
@@ -1,9 +1,14 @@
 ﻿using CompMs.App.Msdial.Model.Core;
 using CompMs.App.Msdial.Model.DataObj;
 using CompMs.Common.Enum;
+using CompMs.Graphics.UI.ProgressBar;
+using CompMs.MsdialCore.Algorithm;
 using CompMs.MsdialCore.DataObj;
+using CompMs.MsdialGcMsApi.Parameter;
+using CompMs.MsdialGcMsApi.Process;
+using Reactive.Bindings.Notifiers;
 using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,8 +16,12 @@ namespace CompMs.App.Msdial.Model.Gcms
 {
     internal sealed class GcmsMethodModel : MethodModelBase
     {
-        public GcmsMethodModel(AnalysisFileBeanModelCollection analysisFileBeanModelCollection, IEnumerable<AlignmentFileBean> alignmentFiles, ProjectBaseParameterModel projectBaseParameter) : base(analysisFileBeanModelCollection, alignmentFiles, projectBaseParameter) {
+        private readonly IMsdialDataStorage<MsdialGcmsParameter> _storage;
+        private readonly IMessageBroker _broker;
 
+        public GcmsMethodModel(AnalysisFileBeanModelCollection analysisFileBeanModelCollection, IMsdialDataStorage<MsdialGcmsParameter> storage, ProjectBaseParameterModel projectBaseParameter, IMessageBroker broker) : base(analysisFileBeanModelCollection, storage.AlignmentFiles, projectBaseParameter) {
+            _storage = storage ?? throw new ArgumentNullException(nameof(storage));
+            _broker = broker;
         }
 
         public override Task RunAsync(ProcessOption option, CancellationToken token) {
@@ -26,19 +35,46 @@ namespace CompMs.App.Msdial.Model.Gcms
             if (option.HasFlag(ProcessOption.Alignment)) {
                 RunAlignment();
             }
-
             return Task.CompletedTask;
         }
 
-        private Task RunFromPeakSpotting() {
-            throw new NotImplementedException();
+        private bool RunFromPeakSpotting() {
+            var request = new ProgressBarMultiContainerRequest(
+                vm_ =>
+                {
+                    var processor = new FileProcess(new StandardDataProviderFactory(retry: 5, isGuiProcess: true), _storage);
+                    var runner = new ProcessRunner(processor);
+                    return runner.RunAllAsync(
+                        _storage.AnalysisFiles,
+                        Enumerable.Repeat((Action<int>)null, _storage.AnalysisFiles.Count),
+                        Math.Max(1, _storage.Parameter.ProcessBaseParam.UsableNumThreads / 2),
+                        null,
+                        default);
+                },
+                _storage.AnalysisFiles.Select(file => file.AnalysisFileName).ToArray());
+            _broker.Publish(request);
+            return request.Result ?? false;
         }
 
-        private Task RunFromIdentification() {
-            throw new NotImplementedException();
+        private bool RunFromIdentification() {
+            var request = new ProgressBarMultiContainerRequest(
+                vm_ =>
+                {
+                    var processor = new FileProcess(new StandardDataProviderFactory(retry: 5, isGuiProcess: true), _storage);
+                    var runner = new ProcessRunner(processor);
+                    return runner.AnnotateAllAsync(
+                        _storage.AnalysisFiles,
+                        Enumerable.Repeat((Action<int>)null, _storage.AnalysisFiles.Count),
+                        Math.Max(1, _storage.Parameter.ProcessBaseParam.UsableNumThreads / 2),
+                        null,
+                        default);
+                },
+                _storage.AnalysisFiles.Select(file => file.AnalysisFileName).ToArray());
+            _broker.Publish(request);
+            return request.Result ?? false;
         }
 
-        private Task RunAlignment() {
+        private bool RunAlignment() {
             throw new NotImplementedException();
         }
 
