a548b90e7b64c936a5b4f3771a11a13d98d35bbe
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-22 15:18:51 +0900

    Change build.ps1

--- build.ps1
+++ build.ps1
@@ -1,7 +1,10 @@
+#! /usr/bin/env pwsh
+
 Param(
-	[string]$OutputPath=''
+	[string]$OutputPath='',
+	[ValidateSet('Release', 'Debug', 'Release vendor unsupported', 'Debug vendor unsupported')]
+	[string]$Configuration='Release'
 )
 
-dotnet restore
-dotnet build .\src\MSDIAL5\MsdialGuiApp\MsdialGuiApp.csproj --no-restore --configuration Release -p:OutputPath=$OutputPath -p:DebugType=none
-dotnet build .\src\RawDataApp\RawDataViewer\RawDataViewer.csproj --no-restore --configuration Release -p:OutputPath=$OutputPath -p:DebugType=none
+dotnet build .\src\MSDIAL5\MsdialGuiApp\MsdialGuiApp.csproj --configuration $Configuration -p:OutputPath=$OutputPath -p:DebugType=none
+dotnet build .\src\RawDataApp\RawDataViewer\RawDataViewer.csproj --configuration $Configuration -p:OutputPath=$OutputPath -p:DebugType=none
