59e5f212b9a779ee432a9e72405a892d22597f70
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-22 14:10:42 +0900

    Add RawDataViewer building process to build.ps1.

--- build.ps1
+++ build.ps1
@@ -4,3 +4,4 @@ Param(
 
 dotnet restore
 dotnet build .\src\MSDIAL5\MsdialGuiApp\MsdialGuiApp.csproj --no-restore --configuration Release -p:OutputPath=$OutputPath -p:DebugType=none
+dotnet build .\src\RawDataApp\RawDataViewer\RawDataViewer.csproj --no-restore --configuration Release -p:OutputPath=$OutputPath -p:DebugType=none
