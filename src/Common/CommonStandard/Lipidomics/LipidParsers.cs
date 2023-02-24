3e05985613281fb2091c769f5efd10de60b0a4ba
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-24 12:29:11 +0900

    Add LPC to lipid model.

--- src/Common/CommonStandard/Lipidomics/LipidParsers.cs
+++ src/Common/CommonStandard/Lipidomics/LipidParsers.cs
@@ -187,4 +187,31 @@ namespace CompMs.Common.Lipidomics
             return null;
         }
 	}
+
+    public class LPCLipidParser : ILipidParser {
+        public string Target { get; } = "LPC";
+
+        private static readonly TotalChainParser chainsParser = TotalChainParser.BuildSpeciesLevelParser(1, 2);
+        public static readonly string Pattern = $"^LPC\\s*(?<sn>{chainsParser.Pattern})$";
+        private static readonly Regex pattern = new Regex(Pattern, RegexOptions.Compiled);
+
+        private static readonly double Skelton = new[]
+        {
+            MassDiffDictionary.CarbonMass * 8,
+            MassDiffDictionary.HydrogenMass * 18,
+            MassDiffDictionary.OxygenMass * 6,
+            MassDiffDictionary.NitrogenMass,
+            MassDiffDictionary.PhosphorusMass,
+        }.Sum();
+
+        public ILipid Parse(string lipidStr) {
+            var match = pattern.Match(lipidStr);
+            if (match.Success) {
+                var group = match.Groups;
+                var chains = chainsParser.Parse(group["sn"].Value);
+                return new Lipid(LbmClass.LPC, Skelton + chains.Mass, chains);
+            }
+            return null;
+        }
+	}
 }
