86994bb5080e5efe417752c9c3e69ebc974bf524
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-24 12:15:53 +0900

    Add PI to lipid model.

--- src/Common/CommonStandard/Lipidomics/LipidParsers.cs
+++ src/Common/CommonStandard/Lipidomics/LipidParsers.cs
@@ -156,4 +156,35 @@ namespace CompMs.Common.Lipidomics
             return null;
         }
 	}
+
+    public class PSLipidParser : ILipidParser {
+        public string Target { get; } = "PS";
+
+        private static readonly TotalChainParser chainsParser = TotalChainParser.BuildParser(2);
+        public static readonly string Pattern = $"^PS\\s*(?<sn>{chainsParser.Pattern})$";
+        private static readonly Regex pattern = new Regex(Pattern, RegexOptions.Compiled);
+
+        private static readonly double Skelton = new[]
+        {
+            MassDiffDictionary.CarbonMass * 6,
+            MassDiffDictionary.HydrogenMass * 12,
+            MassDiffDictionary.OxygenMass * 8,
+            MassDiffDictionary.NitrogenMass,
+            MassDiffDictionary.PhosphorusMass,
+        }.Sum();
+
+        public ILipid Parse(string lipidStr) {
+            var match = pattern.Match(lipidStr);
+            if (match.Success) {
+                var group = match.Groups;
+                var chains = chainsParser.Parse(group["sn"].Value);
+                if (chains.OxidizedCount > 0)
+                {
+                    return new Lipid(LbmClass.OxPS, Skelton + chains.Mass, chains);
+                }
+                return new Lipid(LbmClass.PS, Skelton + chains.Mass, chains);
+            }
+            return null;
+        }
+	}
 }
