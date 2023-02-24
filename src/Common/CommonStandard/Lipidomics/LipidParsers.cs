793b677fc54ed62f8754eb8922f3cf0c66b0905e
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-24 12:05:06 +0900

    Add PA to lipid model.

--- src/Common/CommonStandard/Lipidomics/LipidParsers.cs
+++ src/Common/CommonStandard/Lipidomics/LipidParsers.cs
@@ -39,6 +39,7 @@ namespace CompMs.Common.Lipidomics
             return null;
         }
 	}
+
     public class PELipidParser : ILipidParser {
         public string Target { get; } = "PE";
 
@@ -65,6 +66,32 @@ namespace CompMs.Common.Lipidomics
                     return new Lipid(LbmClass.OxPE, Skelton + chains.Mass, chains);
                 }
                 return new Lipid(LbmClass.PE, Skelton + chains.Mass, chains);
+            }
+            return null;
+        }
+	}
+
+    public class PALipidParser : ILipidParser {
+        public string Target { get; } = "PA";
+
+        private static readonly TotalChainParser chainsParser = TotalChainParser.BuildParser(2);
+        public static readonly string Pattern = $"^PA\\s*(?<sn>{chainsParser.Pattern})$";
+        private static readonly Regex pattern = new Regex(Pattern, RegexOptions.Compiled);
+
+        private static readonly double Skelton = new[]
+        {
+            MassDiffDictionary.CarbonMass * 3,
+            MassDiffDictionary.HydrogenMass * 7,
+            MassDiffDictionary.OxygenMass * 6,
+            MassDiffDictionary.PhosphorusMass,
+        }.Sum();
+
+        public ILipid Parse(string lipidStr) {
+            var match = pattern.Match(lipidStr);
+            if (match.Success) {
+                var group = match.Groups;
+                var chains = chainsParser.Parse(group["sn"].Value);
+                return new Lipid(LbmClass.PA, Skelton + chains.Mass, chains);
             }
             return null;
         }
