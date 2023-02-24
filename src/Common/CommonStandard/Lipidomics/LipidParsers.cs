585fa6bba3ab1b2ae1e10f09cfd721dbbaf640aa
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-24 20:08:00 +0900

    Add TG to lipid model.

--- src/Common/CommonStandard/Lipidomics/LipidParsers.cs
+++ src/Common/CommonStandard/Lipidomics/LipidParsers.cs
@@ -340,13 +340,13 @@ namespace CompMs.Common.Lipidomics
             if (match.Success) {
                 var group = match.Groups;
                 var chains = chainsParser.Parse(group["sn"].Value);
-                if (chains.OxidizedCount>0)
+                if (chains.OxidizedCount > 0)
                 {
                     return new Lipid(LbmClass.OxTG, Skelton + chains.Mass, chains);
                 }
-                else return new Lipid(LbmClass.TG, Skelton + chains.Mass, chains);
+                return new Lipid(LbmClass.TG, Skelton + chains.Mass, chains);
             }
             return null;
         }
-    }
+	}
 }
