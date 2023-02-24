44512cfa82846330dab460167824012e4e6dc69a
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-24 20:03:33 +0900

    Remove TGLipidParser file.

--- src/Common/CommonStandard/Lipidomics/LipidParsers.cs
+++ src/Common/CommonStandard/Lipidomics/LipidParsers.cs
@@ -320,4 +320,33 @@ namespace CompMs.Common.Lipidomics
             return null;
         }
 	}
+
+    public class TGLipidParser : ILipidParser {
+        public string Target { get; } = "TG";
+
+        private static readonly TotalChainParser chainsParser = TotalChainParser.BuildParser(3);
+        public static readonly string Pattern = $"^TG\\s*(?<sn>{chainsParser.Pattern})$";
+        private static readonly Regex pattern = new Regex(Pattern, RegexOptions.Compiled);
+
+        private static readonly double Skelton = new[]
+        {
+            MassDiffDictionary.CarbonMass * 3,
+            MassDiffDictionary.HydrogenMass * 5,
+            MassDiffDictionary.OxygenMass * 3,
+        }.Sum();
+
+        public ILipid Parse(string lipidStr) {
+            var match = pattern.Match(lipidStr);
+            if (match.Success) {
+                var group = match.Groups;
+                var chains = chainsParser.Parse(group["sn"].Value);
+                if (chains.OxidizedCount>0)
+                {
+                    return new Lipid(LbmClass.OxTG, Skelton + chains.Mass, chains);
+                }
+                else return new Lipid(LbmClass.TG, Skelton + chains.Mass, chains);
+            }
+            return null;
+        }
+    }
 }
