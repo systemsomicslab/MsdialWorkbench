d02e288d5f4bb7fc25501c57dda88a66b1aac3c1
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-02-24 12:09:33 +0900

    Add PG to lipid model.

--- src/Common/CommonStandard/Lipidomics/LipidParsers.cs
+++ src/Common/CommonStandard/Lipidomics/LipidParsers.cs
@@ -96,4 +96,34 @@ namespace CompMs.Common.Lipidomics
             return null;
         }
 	}
+
+    public class PGLipidParser : ILipidParser {
+        public string Target { get; } = "PG";
+
+        private static readonly TotalChainParser chainsParser = TotalChainParser.BuildParser(2);
+        public static readonly string Pattern = $"^PG\\s*(?<sn>{chainsParser.Pattern})$";
+        private static readonly Regex pattern = new Regex(Pattern, RegexOptions.Compiled);
+
+        private static readonly double Skelton = new[]
+        {
+            MassDiffDictionary.CarbonMass * 6,
+            MassDiffDictionary.HydrogenMass * 13,
+            MassDiffDictionary.OxygenMass * 8,
+            MassDiffDictionary.PhosphorusMass,
+        }.Sum();
+
+        public ILipid Parse(string lipidStr) {
+            var match = pattern.Match(lipidStr);
+            if (match.Success) {
+                var group = match.Groups;
+                var chains = chainsParser.Parse(group["sn"].Value);
+                if (chains.OxidizedCount > 0)
+                {
+                    return new Lipid(LbmClass.OxPG, Skelton + chains.Mass, chains);
+                }
+                return new Lipid(LbmClass.PG, Skelton + chains.Mass, chains);
+            }
+            return null;
+        }
+	}
 }
