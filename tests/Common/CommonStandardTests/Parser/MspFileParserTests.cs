04395888436c28641e3391e7a113ad27ef325ba0
YukiMatsuzawa <122433968+YukiMatsuzawa@users.noreply.github.com>
2023-03-01 14:49:44 +0900

    Add msp parser tests. (#41)
    * Add msp parser tests.
    
    * Add msp parser test.
    
    * Add msp parser test.

--- tests/Common/CommonStandardTests/Parser/MspFileParserTests.cs
+++ tests/Common/CommonStandardTests/Parser/MspFileParserTests.cs
@@ -118,10 +118,14 @@ namespace CompMs.Common.Parser.Tests
         }
 
         [TestMethod]
-        [DeploymentItem(@"Resources\Parser\msp_from_mzvault.msp", @"Resources\Parser\")]
-        [DeploymentItem(@"Resources\Parser\msp_from_sciex.msp", @"Resources\Parser\")]
-        [DataRow(@"Resources\Parser\msp_from_mzvault.msp", 1)]
-        [DataRow(@"Resources\Parser\msp_from_sciex.msp", 1)]
+        [DeploymentItem(@"Resources\Parser\msp_test_data.msp", @"Resources\Parser\")]
+        [DataRow(@"Resources\Parser\msp_test_data.msp", 2)]
+        [DeploymentItem(@"Resources\Parser\msp_name_check.msp", @"Resources\Parser\")]
+        [DataRow(@"Resources\Parser\msp_name_check.msp", 12)]
+        [DeploymentItem(@"Resources\Parser\msp_same_name.msp", @"Resources\Parser\")]
+        [DataRow(@"Resources\Parser\msp_same_name.msp", 2)]
+        [DeploymentItem(@"Resources\Parser\msp_spectra_check.msp", @"Resources\Parser\")]
+        [DataRow(@"Resources\Parser\msp_spectra_check.msp", 2)]
         public void ReadMspFileTest(string testFile, int size) {
             var datas = MspFileParser.MspFileReader(testFile);
             Assert.AreEqual(size, datas.Count);
