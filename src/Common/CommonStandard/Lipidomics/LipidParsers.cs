7aac9fd10287ec199f32b203d807a527c83fd379
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-03-02 22:06:58 +0900

    Remove BMPLipidParser file.

--- src/Common/CommonStandard/Lipidomics/LipidParsers.cs
+++ src/Common/CommonStandard/Lipidomics/LipidParsers.cs
@@ -639,4 +639,31 @@ namespace CompMs.Common.Lipidomics
             return null;
         }
 	}
+
+    public class BMPLipidParser : ILipidParser {
+        public string Target { get; } = "BMP";
+
+        private static readonly TotalChainParser chainsParser = TotalChainParser.BuildParser(2);
+        public static readonly string Pattern = $"^BMP\\s*(?<sn>{chainsParser.Pattern})$";
+        private static readonly Regex pattern = new Regex(Pattern, RegexOptions.Compiled);
+
+        private static readonly double Skelton = new[]
+        {
+            MassDiffDictionary.CarbonMass * 6,
+            MassDiffDictionary.HydrogenMass * 13,
+            MassDiffDictionary.OxygenMass * 8,
+            //MassDiffDictionary.NitrogenMass,
+            MassDiffDictionary.PhosphorusMass,
+        }.Sum();
+
+        public ILipid Parse(string lipidStr) {
+            var match = pattern.Match(lipidStr);
+            if (match.Success) {
+                var group = match.Groups;
+                var chains = chainsParser.Parse(group["sn"].Value);
+                return new Lipid(LbmClass.BMP, Skelton + chains.Mass, chains);
+            }
+            return null;
+        }
+    }
 }
