16e3dab3e2f059b2551f197db9251bf0edbccfd8
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-03-02 22:20:46 +0900

    Remove SMLipidParser file.

--- src/Common/CommonStandard/Lipidomics/LipidParsers.cs
+++ src/Common/CommonStandard/Lipidomics/LipidParsers.cs
@@ -717,4 +717,34 @@ namespace CompMs.Common.Lipidomics
             return null;
         }
 	}
+
+    public class SMLipidParser : ILipidParser
+    {
+        public string Target { get; } = "SM";
+
+        private static readonly TotalChainParser chainsParser = TotalChainParser.BuildCeramideParser(2);
+        public static readonly string Pattern = $"^SM\\s*(?<sn>{chainsParser.Pattern})$";
+        private static readonly Regex pattern = new Regex(Pattern, RegexOptions.Compiled);
+
+        private static readonly double Skelton = new[]
+        {
+            MassDiffDictionary.CarbonMass * 5,
+            MassDiffDictionary.HydrogenMass * 12,
+            MassDiffDictionary.OxygenMass * 3,
+            MassDiffDictionary.NitrogenMass * 1,
+            MassDiffDictionary.PhosphorusMass,
+        }.Sum();
+
+        public ILipid Parse(string lipidStr)
+        {
+            var match = pattern.Match(lipidStr);
+            if (match.Success)
+            {
+                var group = match.Groups;
+                var chains = chainsParser.Parse(group["sn"].Value);
+                return new Lipid(LbmClass.SM, Skelton + chains.Mass, chains);
+            }
+            return null;
+        }
+    }
 }
