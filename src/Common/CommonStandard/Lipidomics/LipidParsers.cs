603e45ba9d7cc6713bdd16504952f99709fa2ad9
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-03-02 21:25:02 +0900

    Remove CARLipidParser file.

--- src/Common/CommonStandard/Lipidomics/LipidParsers.cs
+++ src/Common/CommonStandard/Lipidomics/LipidParsers.cs
@@ -401,4 +401,30 @@ namespace CompMs.Common.Lipidomics
             return null;
         }
 	}
+
+    public class CARLipidParser : ILipidParser {
+        public string Target { get; } = "CAR";
+
+        private static readonly TotalChainParser chainsParser = TotalChainParser.BuildParser(1);
+        public static readonly string Pattern = $"^CAR\\s*(?<sn>{chainsParser.Pattern})$";
+        private static readonly Regex pattern = new Regex(Pattern, RegexOptions.Compiled);
+
+        private static readonly double Skelton = new[]
+        {
+            MassDiffDictionary.CarbonMass * 7,
+            MassDiffDictionary.HydrogenMass * 14,
+            MassDiffDictionary.OxygenMass * 3,
+            MassDiffDictionary.NitrogenMass * 1,
+        }.Sum();
+
+        public ILipid Parse(string lipidStr) {
+            var match = pattern.Match(lipidStr);
+            if (match.Success) {
+                var group = match.Groups;
+                var chains = chainsParser.Parse(group["sn"].Value);
+                return new Lipid(LbmClass.CAR, Skelton + chains.Mass, chains);
+            }
+            return null;
+        }
+    }
 }
