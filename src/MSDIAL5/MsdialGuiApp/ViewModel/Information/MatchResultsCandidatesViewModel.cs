9555f4de33cee62e4181755aad126a4e9dae16c8
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-03-08 16:38:22 +0900

    Add MatchResultsCandidatesViewModel.

---
+++ src/MSDIAL5/MsdialGuiApp/ViewModel/Information/MatchResultsCandidatesViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CompMs.App.Msdial.Model.Information;
+using CompMs.Common.DataObj.Result;
+using CompMs.CommonMVVM;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+
+namespace CompMs.App.Msdial.ViewModel.Information
+{
+    public sealed class MatchResultsCandidatesViewModel : ViewModelBase
+    {
+        private readonly MatchResultCandidatesModel _model;
+
+        public MatchResultsCandidatesViewModel(MatchResultCandidatesModel model) {
+            _model = model ?? throw new ArgumentNullException(nameof(model));
+            SelectedCandidate = model.SelectedCandidate;
+            Candidates = model.Candidates;
+        }
+
+        public ReactiveProperty<MsScanMatchResult> SelectedCandidate { get; }
+        public ReadOnlyReactivePropertySlim<IList<MsScanMatchResult>> Candidates { get; }
+    }
+}
