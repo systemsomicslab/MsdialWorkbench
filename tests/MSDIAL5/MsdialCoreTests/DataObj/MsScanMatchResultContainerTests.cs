0ef99cbce1b54ff3559538fc407d9102d07ece8f
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-03-08 21:43:55 +0900

    Fix MsScanMatchResultContainer merge bug.

--- tests/MSDIAL5/MsdialCoreTests/DataObj/MsScanMatchResultContainerTests.cs
+++ tests/MSDIAL5/MsdialCoreTests/DataObj/MsScanMatchResultContainerTests.cs
@@ -252,6 +252,32 @@ namespace CompMs.MsdialCore.DataObj.Tests
         }
 
         [TestMethod()]
+        public void MergeShrinkTest() {
+            var container1 = new MsScanMatchResultContainer();
+            var commonResult = new MsScanMatchResult { Source = SourceType.TextDB, TotalScore = 0.7f, Priority = 2, };
+            var results1 = new[]
+            {
+                commonResult,
+                new MsScanMatchResult { Source = SourceType.MspDB, TotalScore = 0.8f, Priority = 1, },
+            };
+            container1.AddResults(results1);
+            Assert.AreEqual(3, container1.MatchResults.Count);
+
+            var container2 = new MsScanMatchResultContainer();
+            var results2 = new[]
+            {
+                commonResult,
+                new MsScanMatchResult { Source = SourceType.MspDB | SourceType.Manual, TotalScore = 0.3f, Priority = 1, },
+                new MsScanMatchResult { Source = SourceType.MspDB, TotalScore = 0.6f, Priority = 1, },
+            };
+            container2.AddResults(results2);
+            Assert.AreEqual(4, container2.MatchResults.Count);
+
+            container1.MergeContainers(container2);
+            Assert.AreEqual(5, container1.MatchResults.Count);
+        }
+
+        [TestMethod()]
         public void SaveAndLoadTest() {
             var container = new MsScanMatchResultContainer();
             var results = new[]{
