4891ab58bd2cd6f26a16436bb1dcb12ded6c419e
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-03-09 13:45:48 +0900

    Add UndoManager class.

---
+++ src/MSDIAL5/MsdialGuiApp/Model/Service/UndoManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CompMs.App.Msdial.Model.Service
+{
+    public sealed class UndoManager {
+        public void Add(IUndoCommand command) {
+            throw new NotImplementedException();
+        }
+    
+        public void Undo() {
+            throw new NotImplementedException();
+        }
+
+        public void Redo() {
+            throw new NotImplementedException();
+        }
+    }
+}
