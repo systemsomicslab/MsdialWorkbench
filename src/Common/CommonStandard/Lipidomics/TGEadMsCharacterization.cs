2589bebf4cec860bc2d781a5a6a9af954fb26b09
htsugawa <htsugawa@go.tuat.ac.jp>
2023-04-02 23:47:41 +0900

    Fixed DG, PI, PS, TG eieio characterizations

--- src/Common/CommonStandard/Lipidomics/TGEadMsCharacterization.cs
+++ src/Common/CommonStandard/Lipidomics/TGEadMsCharacterization.cs
@@ -9,9 +9,47 @@ namespace CompMs.Common.Lipidomics
             IMSScanProperty scan, ILipid molecule, MoleculeMsReference reference,
             float tolerance, float mzBegin, float mzEnd)
         {
+            var class_cutoff = 0;
+            var chain_cutoff = 2;
+            var position_cutoff = 1;
+            var double_cutoff = 0.5;
+
+            if (molecule.Chains.ChainCount > 1) {
+                var deepChains = (SeparatedChains)molecule.Chains;
+                if (deepChains.Chains[0].CarbonCount == deepChains.Chains[1].CarbonCount &&
+                    deepChains.Chains[1].CarbonCount == deepChains.Chains[2].CarbonCount &&
+                    deepChains.Chains[0].DoubleBond == deepChains.Chains[1].DoubleBond &&
+                    deepChains.Chains[1].DoubleBond == deepChains.Chains[2].DoubleBond) {
+                    chain_cutoff = 1;
+                } 
+                else if (deepChains.Chains[0].CarbonCount == deepChains.Chains[1].CarbonCount &&
+                    deepChains.Chains[1].CarbonCount != deepChains.Chains[2].CarbonCount &&
+                    deepChains.Chains[0].DoubleBond == deepChains.Chains[1].DoubleBond &&
+                    deepChains.Chains[1].DoubleBond != deepChains.Chains[2].DoubleBond) {
+                    chain_cutoff = 2;
+                }
+                else if (deepChains.Chains[0].CarbonCount != deepChains.Chains[1].CarbonCount &&
+                    deepChains.Chains[1].CarbonCount == deepChains.Chains[2].CarbonCount &&
+                    deepChains.Chains[0].DoubleBond != deepChains.Chains[1].DoubleBond &&
+                    deepChains.Chains[1].DoubleBond == deepChains.Chains[2].DoubleBond) {
+                    chain_cutoff = 2;
+                }
+                else if (deepChains.Chains[0].CarbonCount == deepChains.Chains[2].CarbonCount &&
+                    deepChains.Chains[1].CarbonCount != deepChains.Chains[2].CarbonCount &&
+                    deepChains.Chains[0].DoubleBond == deepChains.Chains[2].DoubleBond &&
+                    deepChains.Chains[1].DoubleBond != deepChains.Chains[2].DoubleBond) {
+                    chain_cutoff = 2;
+                }
+                else {
+                    chain_cutoff = 3;
+                }
+            }
+            if (reference.AdductType.AdductIonName == "[M+NH4]+") {
+                position_cutoff = 0;
+            }
 
             var defaultResult = EieioMsCharacterizationUtility.GetDefaultScore(
-                    scan, reference, tolerance, mzBegin, mzEnd, 0, 2, 1, 0.5);
+                    scan, reference, tolerance, mzBegin, mzEnd, class_cutoff, chain_cutoff, position_cutoff, double_cutoff);
             return StandardMsCharacterizationUtility.GetDefaultCharacterizationResultForTriacylGlycerols(molecule, defaultResult);
         }
     }
