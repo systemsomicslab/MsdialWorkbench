04ae0cdcf56b659bb683e782e2d5eefb1eddfd4a
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-04-05 19:40:34 +0900

    Add MsdialExportTab control.

---
+++ src/MSDIAL5/MsdialGuiApp/View/RibbonControl/MsdialExportTab.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls.Ribbon;
+
+namespace CompMs.App.Msdial.View.RibbonControl
+{
+    /// <summary>
+    /// Interaction logic for MsdialExportTab.xaml
+    /// </summary>
+    public partial class MsdialExportTab : RibbonTab
+    {
+        public MsdialExportTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
