b4bc4a5566f5ab7a72e9949ac9fade29c3e206b8
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-04-05 10:57:56 +0900

    Add AlignmnetProcess to GcmsMethodModel.

--- src/MSDIAL5/MsdialGuiApp/Model/Gcms/GcmsMethodModel.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/Gcms/GcmsMethodModel.cs
@@ -1,9 +1,13 @@
 ﻿using CompMs.App.Msdial.Model.Core;
 using CompMs.App.Msdial.Model.DataObj;
+using CompMs.Common.Components;
 using CompMs.Common.Enum;
 using CompMs.Graphics.UI.ProgressBar;
 using CompMs.MsdialCore.Algorithm;
+using CompMs.MsdialCore.Algorithm.Annotation;
 using CompMs.MsdialCore.DataObj;
+using CompMs.MsdialCore.Parser;
+using CompMs.MsdialGcMsApi.Algorithm.Alignment;
 using CompMs.MsdialGcMsApi.Parameter;
 using CompMs.MsdialGcMsApi.Process;
 using Reactive.Bindings.Notifiers;
@@ -17,10 +21,20 @@ namespace CompMs.App.Msdial.Model.Gcms
     internal sealed class GcmsMethodModel : MethodModelBase
     {
         private readonly IMsdialDataStorage<MsdialGcmsParameter> _storage;
+        private readonly FacadeMatchResultEvaluator _evaluator;
         private readonly IMessageBroker _broker;
+        private readonly StandardDataProviderFactory _providerFactory;
+        private static readonly ChromatogramSerializer<ChromatogramSpotInfo> CHROMATOGRAM_SPOT_SERIALIZER;
+
+        static GcmsMethodModel() {
+            CHROMATOGRAM_SPOT_SERIALIZER = ChromatogramSerializerFactory.CreateSpotSerializer("CSS1", ChromXType.RT);
+            //CHROMATOGRAM_SPOT_SERIALIZER = ChromatogramSerializerFactory.CreateSpotSerializer("CSS1", ChromXType.RI);
+        }
         public GcmsMethodModel(AnalysisFileBeanModelCollection analysisFileBeanModelCollection, AlignmentFileBeanModelCollection alignmentFiles, IMsdialDataStorage<MsdialGcmsParameter> storage, ProjectBaseParameterModel projectBaseParameter, IMessageBroker broker) : base(analysisFileBeanModelCollection, alignmentFiles, projectBaseParameter) {
             _storage = storage ?? throw new ArgumentNullException(nameof(storage));
+            _evaluator = FacadeMatchResultEvaluator.FromDataBases(storage.DataBases);
             _broker = broker;
+            _providerFactory = new StandardDataProviderFactory(retry: 5, isGuiProcess: true);
         }
 
         public GcmsAnalysisModel SelectedAnalysisModel {
@@ -53,7 +67,7 @@ namespace CompMs.App.Msdial.Model.Gcms
             var request = new ProgressBarMultiContainerRequest(
                 vm_ =>
                 {
-                    var processor = new FileProcess(new StandardDataProviderFactory(retry: 5, isGuiProcess: true), _storage);
+                    var processor = new FileProcess(_providerFactory, _storage);
                     var runner = new ProcessRunner(processor);
                     return runner.RunAllAsync(
                         _storage.AnalysisFiles,
@@ -71,7 +85,7 @@ namespace CompMs.App.Msdial.Model.Gcms
             var request = new ProgressBarMultiContainerRequest(
                 vm_ =>
                 {
-                    var processor = new FileProcess(new StandardDataProviderFactory(retry: 5, isGuiProcess: true), _storage);
+                    var processor = new FileProcess(_providerFactory, _storage);
                     var runner = new ProcessRunner(processor);
                     return runner.AnnotateAllAsync(
                         _storage.AnalysisFiles,
@@ -86,7 +100,25 @@ namespace CompMs.App.Msdial.Model.Gcms
         }
 
         private bool RunAlignment() {
-            throw new NotImplementedException();
+            var request = new ProgressBarRequest("Process alignment..", isIndeterminate: false,
+                async vm =>
+                {
+                    var factory = new GcmsAlignmentProcessFactory(_storage.AnalysisFiles, _storage, _evaluator);
+                    var aligner = factory.CreatePeakAligner();
+                    aligner.ProviderFactory = _providerFactory; // TODO: I'll remove this later.
+
+                    var alignmentFileModel = AlignmentFiles.Files.Last();
+                    var result = await Task.Run(() => alignmentFileModel.RunAlignment(aligner, CHROMATOGRAM_SPOT_SERIALIZER)).ConfigureAwait(false);
+
+                    var tasks = new[]
+                    {
+                        alignmentFileModel.SaveAlignmentResultAsync(result),
+                        alignmentFileModel.SaveMSDecResultsAsync(alignmentFileModel.LoadMSDecResultsFromEachFiles(result.AlignmentSpotProperties)),
+                    };
+                    await Task.WhenAll(tasks).ConfigureAwait(false);
+                });
+            _broker.Publish(request);
+            return request.Result ?? false;
         }
 
         protected override IAlignmentModel LoadAlignmentFileCore(AlignmentFileBeanModel alignmentFileModel) {
