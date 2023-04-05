0aa54bd1da64ce5f2b11bea62a2255824ae49ff0
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-04-05 14:43:49 +0900

    Add MsdialDataVisualizationTab control.

---
+++ src/MSDIAL5/MsdialGuiApp/View/RibbonControl/MsdialDataVisualizationTab.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls.Ribbon;
+
+namespace CompMs.App.Msdial.View.RibbonControl
+{
+    /// <summary>
+    /// Interaction logic for MsdialDataVisualizationTab.xaml
+    /// </summary>
+    public partial class MsdialDataVisualizationTab : RibbonTab
+    {
+        public MsdialDataVisualizationTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
