8b8629fd1f4a2d2e7d6f88159efb9ba21be48a1e
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-04-05 19:59:05 +0900

    Add MsdialHelpTab control.

---
+++ src/MSDIAL5/MsdialGuiApp/View/RibbonControl/MsdialHelpTab.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls.Ribbon;
+
+namespace CompMs.App.Msdial.View.RibbonControl
+{
+    /// <summary>
+    /// Interaction logic for MsdialHelpTab.xaml
+    /// </summary>
+    public partial class MsdialHelpTab : RibbonTab
+    {
+        public MsdialHelpTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
