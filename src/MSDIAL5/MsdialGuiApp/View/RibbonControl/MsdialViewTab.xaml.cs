550de998c3ee633e592d908eb1de4715f2df0171
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-04-05 19:14:11 +0900

    Add MsdialViewTab control.

---
+++ src/MSDIAL5/MsdialGuiApp/View/RibbonControl/MsdialViewTab.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
+using System.Windows.Input;
+
+namespace CompMs.App.Msdial.View.RibbonControl
+{
+    /// <summary>
+    /// Interaction logic for MsdialViewTab.xaml
+    /// </summary>
+    public partial class MsdialViewTab : RibbonTab
+    {
+        public MsdialViewTab()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickHandled(object sender, RoutedEventArgs e) {
+            //e.Handled = true;
+            //((sender as FrameworkElement)?.Tag as ICommand)?.Execute((sender as MenuItem).CommandParameter);
+        }
+    }
+}
