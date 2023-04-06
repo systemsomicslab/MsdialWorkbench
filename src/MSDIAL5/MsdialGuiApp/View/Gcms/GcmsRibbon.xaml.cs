e5b6be3e9c25c9bab401f44e82b4542cab4403da
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-04-06 11:24:25 +0900

    Add MsdialGraphUtilityTab control.

--- src/MSDIAL5/MsdialGuiApp/View/Gcms/GcmsRibbon.xaml.cs
+++ src/MSDIAL5/MsdialGuiApp/View/Gcms/GcmsRibbon.xaml.cs
@@ -1,11 +1,11 @@
-﻿using System.Windows.Controls.Ribbon;
+﻿using System.Windows.Controls;
 
 namespace CompMs.App.Msdial.View.Gcms
 {
     /// <summary>
     /// Interaction logic for GcmsRibbon.xaml
     /// </summary>
-    public partial class GcmsRibbon : Ribbon
+    public partial class GcmsRibbon : UserControl
     {
         public GcmsRibbon()
         {
