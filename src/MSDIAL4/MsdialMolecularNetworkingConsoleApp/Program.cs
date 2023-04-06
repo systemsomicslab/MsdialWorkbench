42c11c0bed63b09a93a70e1fe7369bbbc93d1970
htsugawa <htsugawa@go.tuat.ac.jp>
2023-04-06 17:56:18 +0900

    made moleculer networking console app

--- src/MSDIAL4/MsdialMolecularNetworkingConsoleApp/Program.cs
+++ src/MSDIAL4/MsdialMolecularNetworkingConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CompMs.App.MsdialConsole.MolecularNetwork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,31 +13,28 @@ namespace Riken.Metabolomics.MolecularNetworking {
             args = new string[] {
                 "msms"
                 , "-i"
-                , @"E:\6_Projects\PROJECT_SCIEXEAD\MSDIAL_1000samples_submit\RIKEN\POS_MSP\Curated_10KE\MergedMSP_10KE.msp"
-                , "-t"
-                , @"0.05"
+                , @"E:\6_Projects\PAPERWORK_MSDIAL5\02_1000standards\RIKEN\POS_MSP\Curated_40CID_15CES\MergedMSP_40CID_15CES.msp"
+                , "-p"
+                , @"E:\6_Projects\PAPERWORK_MSDIAL5\02_1000standards\RIKEN\POS_MSP\Curated_40CID_15CES\param.txt"
                 , "-o"
-                , @"E:\6_Projects\PROJECT_SCIEXEAD\MSDIAL_1000samples_submit\RIKEN\POS_MSP\Curated_10KE\edge_10KE.txt"
+                , @"E:\6_Projects\PAPERWORK_MSDIAL5\02_1000standards\RIKEN\POS_MSP\Curated_40CID_15CES"
             };
             #endregion
             if (args.Length == 0) return argsError();
             if (args.Length < 7) return argsError();
 
             var mspfilepath = string.Empty;
-            var outputfilepath = string.Empty;
-            var tolerance = 0.02F;
+            var paramfilepath = string.Empty;
+            var outputdir = string.Empty;
             for (int i = 1; i < args.Length; i++) {
                 if (args[i] == "-i" && i + 1 < args.Length) mspfilepath = args[i + 1];
-                else if (args[i] == "-t" && i + 1 < args.Length) float.TryParse(args[i + 1], out tolerance);
-                else if (args[i] == "-o" && i + 1 < args.Length) outputfilepath = args[i + 1];
+                else if (args[i] == "-p" && i + 1 < args.Length) paramfilepath = args[i + 1];
+                else if (args[i] == "-o" && i + 1 < args.Length) outputdir = args[i + 1];
             }
 
-            if (mspfilepath == string.Empty || outputfilepath == string.Empty) return argsError();
+            if (mspfilepath == string.Empty || outputdir == string.Empty) return argsError();
 
-            Msdial.Lcms.Dataprocess.Algorithm.Clustering.EgdeGenerator.GenerateEdgesFromMsp(
-                mspfilepath,
-                tolerance,
-                outputfilepath);
+            MoleculerSpectrumNetworkingTest.Run(mspfilepath, paramfilepath, outputdir);
             return 1;
         }
 
@@ -46,11 +44,11 @@ namespace Riken.Metabolomics.MolecularNetworking {
 		/// <returns>error code -1</returns>
         private static int argsError() {
             var error = @"Msdial molecular netwokring console app requires the following args:
-						MsdialMolecularNetworkingConsoleApp.exe <analysisType> -i <input msp file path> -o <output file path> -t <mass tolerance (Da)
+						MsdialMolecularNetworkingConsoleApp.exe <analysisType> -i <input msp file path> -o <output directory> -p <parameter file path>
 						Where: <analysisType>	is now 'msms' only	(required)
 							   <input msp file path>	is the MSP format file for edge clustering	(required)
-							   <output file path>	is the file path to save results	(required)
-							   <mass tolerance>	is the mass accuracy for MS/MS spectra	(required)";
+							   <output directory>	is the folder path to save results	(required)
+							   <parameter file>	contains parameter sets	(required)";
 
             Console.Error.WriteLine(error);
 
