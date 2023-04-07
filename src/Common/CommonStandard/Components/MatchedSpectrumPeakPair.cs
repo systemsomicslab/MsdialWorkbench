d421857bf3acbde0fc4379d7d8f0efa5bb12d373
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-04-07 13:26:41 +0900

    Add MatchedSpectrumPeakPair class.

---
+++ src/Common/CommonStandard/Components/MatchedSpectrumPeakPair.cs
@@ -0,0 +1,15 @@
+﻿using CompMs.Common.Interfaces;
+
+namespace CompMs.Common.Components
+{
+    internal sealed class MatchedSpectrumPeakPair
+    {
+        public MatchedSpectrumPeakPair(ISpectrumPeak experiment, SpectrumPeak reference) {
+            Experiment = experiment;
+            Reference = reference;
+        }
+
+        public ISpectrumPeak Experiment { get; }
+        public SpectrumPeak Reference { get; }
+    }
+}
