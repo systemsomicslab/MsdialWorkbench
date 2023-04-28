6e9fa5c9397d3fa6d774fd4dce3917ce486b8140
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-04-28 16:21:09 +0900

    Add FindTargetCompoundsSpotViewModel.

---
+++ src/MSDIAL5/MsdialGuiApp/ViewModel/Table/FindTargetCompoundsSpotViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CompMs.App.Msdial.Model.DataObj;
+using CompMs.App.Msdial.Model.Table;
+using CompMs.CommonMVVM;
+using CompMs.MsdialCore.DataObj;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using Reactive.Bindings.Notifiers;
+using System;
+using System.Collections.ObjectModel;
+
+namespace CompMs.App.Msdial.ViewModel.Table
+{
+    internal sealed class FindTargetCompoundsSpotViewModel : ViewModelBase
+    {
+        private readonly FindTargetCompoundsSpotModel _model;
+        private readonly IMessageBroker _broker;
+
+        public FindTargetCompoundsSpotViewModel(FindTargetCompoundsSpotModel model, IMessageBroker broker) {
+            _model = model ?? throw new ArgumentNullException(nameof(model));
+            _broker = broker ?? throw new ArgumentNullException(nameof(broker));
+            SetLibraryCommand = new ReactiveCommand().WithSubscribe(OpenSetLibraryDialog).AddTo(Disposables);
+            FindCommand = new ReactiveCommand().WithSubscribe(model.Find).AddTo(Disposables);
+        }
+
+        public ReadOnlyCollection<MatchedSpotCandidate<AlignmentSpotPropertyModel>> Candidates => _model.Candidates;
+
+        public ReactiveCommand FindCommand { get; }
+
+        public ReactiveCommand SetLibraryCommand { get; }
+
+        private void OpenSetLibraryDialog() {
+            using (var vm = new TargetCompoundLibrarySettingViewModel(_model.LibrarySettingModel, _broker)) {
+                _broker.Publish(vm);
+            }
+        }
+    }
+}
