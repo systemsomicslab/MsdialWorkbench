a55ef6a7ae2284e95d274b7e4f4df142a4c0a168
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-05-10 20:26:01 +0900

    Add exporter for MatchedSpotCandidate.

--- src/MSDIAL5/MsdialCore/DataObj/MatchedSpotCandidate.cs
+++ src/MSDIAL5/MsdialCore/DataObj/MatchedSpotCandidate.cs
@@ -1,6 +1,5 @@
 ﻿using CompMs.Common.Components;
 using CompMs.Common.Interfaces;
-using CompMs.Common.Lipidomics;
 using System;
 using System.Linq;
 
@@ -28,12 +27,15 @@ namespace CompMs.MsdialCore.DataObj
             }
         }
 
-        public bool IsSimilarWithMzAndTime {
+        public bool IsSimilarWithMz {
             get {
                 var mzTolerance = Math.Max(Reference.MassTolerance, _mzTolerance);
-                if (!(Math.Abs(Spot.Mass - Reference.PrecursorMz) < mzTolerance)) {
-                    return false;
-                }
+                return Math.Abs(Spot.Mass - Reference.PrecursorMz) < mzTolerance;
+            }
+        }
+
+        public bool IsSimilarWithTime {
+            get {
                 var type = Spot.ChromXs.MainType;
                 var t = Spot.ChromXs.GetChromByType(type);
                 var s = Reference.ChromXs.GetChromByType(type);
@@ -57,7 +59,7 @@ namespace CompMs.MsdialCore.DataObj
     public static class MatchedSpotCandidate {
         public static MatchedSpotCandidate<T> IsMatchedWith<T>(this T spot, MoleculeMsReference reference, double mzTolerance, double mainChromXTolerance) where T: IAnnotatedObject, IChromatogramPeak {
             var candidate = new MatchedSpotCandidate<T>(spot, reference, mzTolerance, mainChromXTolerance);
-            if (candidate.IsAnnotated || candidate.IsSimilarWithMzAndTime) {
+            if (candidate.IsAnnotated || candidate.IsSimilarWithMz) {
                 return candidate;
             }
             else {
---
+++ src/MSDIAL5/MsdialGuiApp/Model/Export/AlignmentMatchedSpotCandidateExporter.cs
@@ -0,0 +1,103 @@
+﻿using CompMs.App.Msdial.Model.DataObj;
+using CompMs.Common.Components;
+using CompMs.Common.Interfaces;
+using CompMs.MsdialCore.DataObj;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CompMs.App.Msdial.Model.Export
+{
+    internal sealed class AlignmentMatchedSpotCandidateExporter
+    {
+        public async Task ExportAsync(Stream stream, IReadOnlyList<MatchedSpotCandidate<AlignmentSpotPropertyModel>> candidates) {
+            var doc = new XElement("MatchedSpots");
+            var tasks = candidates.Select(ToXmlElement).ToArray();
+            var elements = await Task.WhenAll(tasks).ConfigureAwait(false);
+            foreach (var element in elements) {
+                doc.Add(element);
+            }
+            doc.Save(stream);
+        }
+
+        private async Task<XElement> ToXmlElement(MatchedSpotCandidate<AlignmentSpotPropertyModel> candidate) {
+            var spot = await ToXmlElement(candidate.Spot).ConfigureAwait(false);
+            var reference = ToXmlElement(candidate.Reference);
+            var annotated = new XElement("Annotated", candidate.IsAnnotated);
+            var similarWithMz = new XElement("SimilarWithMz", candidate.IsSimilarWithMz);
+            var similarWithChrom = new XElement("SimilarWithTime", candidate.IsSimilarWithTime);
+            return new XElement("MatchedSpot", reference, spot, annotated, similarWithMz, similarWithChrom);
+        }
+
+        private XElement ToXmlElement(MoleculeMsReference reference) {
+            var referenceElement = new XElement("Reference", new XElement("Name", reference.Name));
+            if (reference.PrecursorMz > 0) {
+                referenceElement.Add(new XElement("Mz", reference.PrecursorMz));
+            }
+            if (reference.ChromXs != null) {
+                referenceElement.Add(ToXmlElement(reference.ChromXs));
+            }
+            return referenceElement;
+        }
+
+        private async Task<XElement> ToXmlElement(AlignmentSpotPropertyModel spot) {
+            var spotElement = new XElement("Aligned spot",
+                new XElement("SpotId", spot.MasterAlignmentID),
+                new XElement("Name", spot.Name),
+                new XElement("Mz", spot.Mass),
+                ToXmlElement(((IChromatogramPeak)spot).ChromXs));
+            var peaks = await spot.AlignedPeakPropertiesModelAsObservable;
+            foreach (var peak in peaks) {
+                spotElement.Add(ToXml(peak));
+            }
+            return spotElement;
+        }
+
+        private XElement ToXml(AlignmentChromPeakFeatureModel peak) {
+            var peakElement = new XElement("Peak");
+            peakElement.Add(new XElement("File", peak.FileName));
+            peakElement.Add(new XElement("Name", peak.Name));
+            peakElement.Add(new XElement("Mz", peak.Mass));
+            peakElement.Add(ToXmlElement(peak.ChromXsTop));
+            peakElement.Add(new XElement("PeakHeight", peak.PeakHeightTop));
+            if (peak.NormalizedPeakHeight > 0) {
+                peakElement.Add(new XElement("NormalizedPeakHeight", peak.NormalizedPeakHeight));
+            }
+            return peakElement;
+        }
+
+        private XElement ToXmlElement(ChromXs chrom) {
+            var time = new XElement("Time");
+            switch (chrom.MainType) {
+                case ChromXType.RT:
+                    time.Add("Type", "RetentionTime");
+                    break;
+                case ChromXType.RI:
+                    time.Add("Type", "RetentionIndex");
+                    break;
+                case ChromXType.Drift:
+                    time.Add("Type", "DriftTime");
+                    break;
+                case ChromXType.Mz:
+                    time.Add("Type", "Mz");
+                    break;
+            }
+            if (chrom.RT.Value > 0) {
+                time.Add(new XElement("RetentionTime", chrom.RT.Value));
+            }
+            if (chrom.RI.Value > 0) {
+                time.Add(new XElement("RetentionIndex", chrom.RI.Value));
+            }
+            if (chrom.Drift.Value > 0) {
+                time.Add(new XElement("DriftTime", chrom.Drift.Value));
+            }
+            if (chrom.Mz.Value > 0) {
+                time.Add(new XElement("Mz", chrom.Mz.Value));
+            }
+            return time;
+        }
+    }
+}
