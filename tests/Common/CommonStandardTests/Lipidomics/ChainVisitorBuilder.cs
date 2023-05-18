156b99c3823f89949f3c0854fe3516b5bbb4d75a
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-05-18 20:40:30 +0900

    Add ChainVisitorBuilder.

---
+++ tests/Common/CommonStandardTests/Lipidomics/ChainVisitorBuilder.cs
@@ -0,0 +1,25 @@
+﻿using CompMs.Common.DataStructure;
+using CompMs.Common.Lipidomics;
+
+namespace CommonStandardTests.Lipidomics
+{
+    internal sealed class ChainVisitorBuilder : ILipidomicsVisitorBuilder
+    {
+        private IVisitor<IDoubleBond, IDoubleBond> _acylDoubleBondVisitor, _alkylDoubleBondVisitor, _sphingoDoubleBondVisitor;
+        private IVisitor<IOxidized, IOxidized> _acylOxidizedVisitor, _alkylOxidizedVisitor, _sphingoOxidizedVisitor;
+
+        public IVisitor<IChain, IChain> Create() {
+            var acylVisitor = new AcylChainVisitor(_acylDoubleBondVisitor, _acylOxidizedVisitor);
+            var alkylVisitor = new AlkylChainVisitor(_alkylDoubleBondVisitor, _alkylOxidizedVisitor);
+            var sphingoVisitor = new SphingosineChainVisitor(_sphingoDoubleBondVisitor, _sphingoOxidizedVisitor);
+            return new ChainVisitor(acylVisitor, alkylVisitor, sphingoVisitor);
+        }
+
+        void ILipidomicsVisitorBuilder.SetAcylDoubleBondVisitor(IVisitor<IDoubleBond, IDoubleBond> doubleBondVisitor) => _acylDoubleBondVisitor = doubleBondVisitor;
+        void ILipidomicsVisitorBuilder.SetAcylOxidizedVisitor(IVisitor<IOxidized, IOxidized> oxidizedVisitor) => _acylOxidizedVisitor = oxidizedVisitor;
+        void ILipidomicsVisitorBuilder.SetAlkylDoubleBondVisitor(IVisitor<IDoubleBond, IDoubleBond> doubleBondVisitor) => _alkylDoubleBondVisitor = doubleBondVisitor;
+        void ILipidomicsVisitorBuilder.SetAlkylOxidizedVisitor(IVisitor<IOxidized, IOxidized> oxidizedVisitor) => _alkylOxidizedVisitor = oxidizedVisitor;
+        void ILipidomicsVisitorBuilder.SetSphingoDoubleBondVisitor(IVisitor<IDoubleBond, IDoubleBond> doubleBondVisitor) => _sphingoDoubleBondVisitor = doubleBondVisitor;
+        void ILipidomicsVisitorBuilder.SetSphingoOxidizedVisitor(IVisitor<IOxidized, IOxidized> oxidizedVisitor) => _sphingoOxidizedVisitor = oxidizedVisitor;
+    }
+}
