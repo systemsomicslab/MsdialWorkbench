9b9c09241c6ed07c0b3ba194a167d209c09ad750
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-06-07 15:58:51 +0900

    Add classes for explanation of annotation changes.

---
+++ src/MSDIAL5/MsdialCore/DataObj/AnnotationQualityLabel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CompMs.MsdialCore.DataObj
+{
+    public sealed class AnnotationQualityLabel
+    {
+        private readonly List<AnnotationQualityType> _selected;
+
+        public AnnotationQualityLabel()
+        {
+            _selected = new List<AnnotationQualityType>();
+            Selected = _selected.AsReadOnly();
+        }
+
+        public ReadOnlyCollection<AnnotationQualityType> Selected { get; }
+
+        public bool IsSelected(AnnotationQualityType type) {
+            return _selected.Contains(type);
+        }
+
+        public void Select(AnnotationQualityType type) {
+            if (!_selected.Contains(type)) {
+                _selected.Add(type);
+            }
+        }
+
+        public void Deselect(AnnotationQualityType type) {
+            if (_selected.Contains(type)) {
+                _selected.Remove(type);
+            }
+        }
+    }
+}
---
+++ src/MSDIAL5/MsdialCore/DataObj/AnnotationQualityType.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace CompMs.MsdialCore.DataObj
+{
+    public sealed class AnnotationQualityType
+    {
+        /*
+         * When adding a new type, please be careful not to duplicate the ID.
+         * When deleting a type, make sure that the ID is not used in the future.
+         */
+        public static AnnotationQualityType LOW_QUALITY_SPECTRUM = new AnnotationQualityType("Low quality spectrum", 1);
+        public static AnnotationQualityType MISANNOTATION = new AnnotationQualityType("Misannotation", 2);
+        public static AnnotationQualityType COELUTION = new AnnotationQualityType("Coelution (mixed spectra)", 3);
+        public static AnnotationQualityType OVERANNOTATION = new AnnotationQualityType("Overannotation", 4);
+
+        private static readonly Dictionary<int, AnnotationQualityType> ID_TO_TYPES = new Dictionary<int, AnnotationQualityType>();
+
+        private AnnotationQualityType(string label, int id) {
+            Label = label;
+            Id = id;
+            ID_TO_TYPES.Add(id, this);
+        }
+
+        public string Label { get; }
+        public int Id { get; }
+
+        internal static AnnotationQualityType GetById(int id) {
+            return ID_TO_TYPES.TryGetValue(id, out var type) ? type : null;
+        }
+
+        public static IEnumerable<AnnotationQualityType> AllTypes() {
+            return ID_TO_TYPES.Values;
+        }
+    }
+}
