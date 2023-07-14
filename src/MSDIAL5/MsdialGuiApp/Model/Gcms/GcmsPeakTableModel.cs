8ec433add0d20dd77acd319b6a2021454fa2311a
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-07-15 01:09:43 +0900

    Add GcmsAnalysisPeakTableModel

---
+++ src/MSDIAL5/MsdialGuiApp/Model/Gcms/GcmsPeakTableModel.cs
@@ -0,0 +1,35 @@
+﻿using CompMs.App.Msdial.Model.DataObj;
+using CompMs.App.Msdial.Model.Search;
+using CompMs.App.Msdial.Model.Table;
+using Reactive.Bindings;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompMs.App.Msdial.Model.Gcms
+{
+    internal sealed class GcmsAnalysisPeakTableModel : PeakSpotTableModelBase<Ms1BasedSpectrumFeature>
+    {
+        public GcmsAnalysisPeakTableModel(IReadOnlyList<Ms1BasedSpectrumFeature> spectrumFeature, IReactiveProperty<Ms1BasedSpectrumFeature> target, PeakSpotNavigatorModel peakSpotNavigatorModel)
+            : base(spectrumFeature, target, peakSpotNavigatorModel) {
+            MassMin = spectrumFeature.Select(s => s.QuantifiedChromatogramPeak.PeakFeature.Mass).DefaultIfEmpty().Min();
+            MassMax = spectrumFeature.Select(s => s.QuantifiedChromatogramPeak.PeakFeature.Mass).DefaultIfEmpty().Max();
+            RtMin = spectrumFeature.Select(s => s.QuantifiedChromatogramPeak.PeakFeature.ChromXsTop.RT.Value).DefaultIfEmpty().Min();
+            RtMax = spectrumFeature.Select(s => s.QuantifiedChromatogramPeak.PeakFeature.ChromXsTop.RT.Value).DefaultIfEmpty().Max();
+            RiMin = spectrumFeature.Select(s => s.QuantifiedChromatogramPeak.PeakFeature.ChromXsTop.RI.Value).DefaultIfEmpty().Min();
+            RiMax = spectrumFeature.Select(s => s.QuantifiedChromatogramPeak.PeakFeature.ChromXsTop.RI.Value).DefaultIfEmpty().Max();
+        }
+
+        public double MassMin { get; }
+        public double MassMax { get; }
+        public double RtMin { get; }
+        public double RtMax { get; }
+        public double RiMin { get; }
+        public double RiMax { get; }
+
+        public void MarkAllAsConfirmed() {
+            foreach (var peak in PeakSpots) {
+                peak.Confirmed = true;
+            }
+        }
+    }
+}
