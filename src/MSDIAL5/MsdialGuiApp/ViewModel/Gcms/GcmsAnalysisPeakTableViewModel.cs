bf57856a180396b593ad4cabc8c58d6e459e0f86
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-07-15 01:42:07 +0900

    Add GcmsAnalysisPeakTableViewModel.

---
+++ src/MSDIAL5/MsdialGuiApp/ViewModel/Gcms/GcmsAnalysisPeakTableViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CompMs.App.Msdial.Model.Gcms;
+using CompMs.App.Msdial.Model.Loader;
+using CompMs.App.Msdial.ViewModel.Search;
+using CompMs.App.Msdial.ViewModel.Service;
+using CompMs.App.Msdial.ViewModel.Table;
+using System;
+using System.Windows.Input;
+
+namespace CompMs.App.Msdial.ViewModel.Gcms
+{
+    internal sealed class GcmsAnalysisPeakTableViewModel : AnalysisPeakTableViewModelBase
+    {
+        public GcmsAnalysisPeakTableViewModel(GcmsAnalysisPeakTableModel model, IObservable<EicLoader> eicLoader, PeakSpotNavigatorViewModel peakSpotNavigatorViewModel, ICommand setUnknownCommand, UndoManagerViewModel undoManagerViewModel)
+            : base(model, peakSpotNavigatorViewModel, setUnknownCommand, undoManagerViewModel, eicLoader) {
+            MassMin = model.MassMin;
+            MassMax = model.MassMax;
+            RtMin = model.RtMin;
+            RtMax = model.RtMax;
+            RiMin = model.RiMin;
+            RiMax = model.RiMax;
+        }
+
+        public double MassMin { get; }
+        public double MassMax { get; }
+        public double RtMin { get; }
+        public double RtMax { get; }
+        public double RiMin { get; }
+        public double RiMax { get; }
+    }
+}
