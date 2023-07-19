a9b317c6aa1ffb7c056675c93a4a0593259c2cd3
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-07-19 19:33:09 +0900

    Add ILipidCondition interface.

---
+++ src/Common/CommonStandard/Lipidomics/Characterization/ILipidCondition.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompMs.Common.Lipidomics.Characterization
+{
+    internal interface ILipidCondition
+    {
+        bool Filter(LipidMolecule molecule);
+    }
+}
