1b0a0ad66a59cd6061279e332f8d43b307e3a2bd
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-07-20 11:41:37 +0900

    Add test for PC Na adduct

--- tests/Common/CommonStandardTests/Lipidomics/Characterization/CharacterizationSampleTests.cs
+++ tests/Common/CommonStandardTests/Lipidomics/Characterization/CharacterizationSampleTests.cs
@@ -1,5 +1,4 @@
-﻿using Accord.Math.Distances;
-using CompMs.Common.Components;
+﻿using CompMs.Common.Components;
 using CompMs.Common.DataObj.Property;
 using CompMs.Common.Enum;
 using CompMs.Common.FormulaGenerator.DataObj;
@@ -146,6 +145,129 @@ namespace CompMs.Common.Lipidomics.Characterization.Tests
                             new SpectrumPeak { Mass = 478.32920, Intensity = 50d, },
                             new SpectrumPeak { Mass = 496.33977, Intensity = 50d, },
                             new SpectrumPeak { Mass = 701.51158, Intensity = 20d, }
+                        }
+                    },
+                };
+            }
+        }
+
+        [DataTestMethod()]
+        [DynamicData(nameof(PCCharacterizationTest2Data))]
+        public void PCCharacterizationTest2(LipidMolecule lipid, IMSScanProperty scan) {
+            var pcType = new PCLipidType();
+            var preConditions = new MergePreCondition(
+                new IonModeCondition(IonMode.Positive),
+                new AdductCondition("[M+Na]+"));
+            var conditions = new MergeCondition<PCCandidate>(
+                new LipidChainCondition<PCCandidate>(c => c.Sn1DoubleBond <= 6),
+                new FragmentsExistAnyCondition<PCCandidate>(c => {
+                    //// seek 184.07332 (C5H15NO4P)
+                    //var diagnosticMz = 184.07332;
+                    // seek [M+Na -C5H14NO4P]+
+                    var diagnosticMz2 = c.Lipid.Mz - 183.06604;
+                    // seek [M+Na -C3H9N]+
+                    var diagnosticMz3 = c.Lipid.Mz - 59.0735;
+                    return new[] {
+                        (diagnosticMz2, 3d),
+                        (diagnosticMz3, 3d),
+                    };
+                }, .02d));
+            var scorer = new LipidScoring<PCCandidate>(c => {
+                var theoreticalMz = c.Lipid.Mz;
+                var sn1CarbonNum = c.Sn1Carbon;
+                var sn1DoubleNum = c.Sn1DoubleBond;
+                var sn2CarbonNum = c.Sn2Carbon;
+                var sn2DoubleNum = c.Sn2DoubleBond;
+                var H2O = MassDiffDictionary.HydrogenMass * 2 + MassDiffDictionary.OxygenMass;
+
+                var nl_SN1 = theoreticalMz - LipidMsmsCharacterizationUtility.acylCainMass(sn1CarbonNum, sn1DoubleNum);
+                var nl_SN1_H2O = nl_SN1 - H2O;
+                var nl_SN2 = theoreticalMz - LipidMsmsCharacterizationUtility.acylCainMass(sn2CarbonNum, sn2DoubleNum);
+                var nl_SN2_H2O = nl_SN2 - H2O;
+
+                var query = new List<SpectrumPeak>
+                {
+                };
+                return new[]
+                {
+                    (nl_SN1, 0.01),
+                    (nl_SN1_H2O, 0.01),
+                    (nl_SN2, 0.01),
+                    (nl_SN2_H2O, 0.01)
+                };
+            }, .02d);
+            var search = new PCSearchSpace(6, 7, conditions, scorer);
+            var expected = LipidEieioMsmsCharacterization.JudgeIfPhosphatidylcholine(scan, .02d, lipid.Mz, lipid.TotalCarbonCount, lipid.TotalDoubleBondCount, lipid.Sn1CarbonCount, lipid.TotalCarbonCount - lipid.Sn1CarbonCount, lipid.Sn1DoubleBondCount, lipid.TotalDoubleBondCount - lipid.Sn1DoubleBondCount, lipid.Adduct);
+            if (!preConditions.Satisfy(lipid)) {
+                Assert.IsNull(expected);
+                return;
+            }
+            var candidates = search.RetrieveAll(lipid, scan);
+            if (candidates is null) {
+                Assert.IsNull(expected);
+                return;
+            }
+            var actual = pcType.Convert(lipid, candidates);
+            if (expected is null) {
+                Assert.IsNull(actual);
+                return;
+            }
+            Assert.AreEqual(expected.LipidName, actual?.LipidName);
+        }
+
+        public static IEnumerable<object[]> PCCharacterizationTest2Data {
+            get {
+                var lipid = new LipidMolecule
+                {
+                    TotalCarbonCount = 34,
+                    TotalDoubleBondCount = 1,
+                    Sn1CarbonCount = 16,
+                    Sn2CarbonCount = 18,
+                    Sn1DoubleBondCount = 0,
+                    Sn2DoubleBondCount = 1,
+                    Adduct = AdductIon.GetAdductIon("[M+Na]+"),
+                    Mz = 782.5670f,
+                    IonMode = IonMode.Positive,
+                };
+                yield return new object[] {
+                    lipid,
+                    new MSScanProperty
+                    {
+                        Spectrum = new List<SpectrumPeak>
+                        {
+                            new SpectrumPeak { Mass = 500.31115, Intensity = 50d, },
+                            new SpectrumPeak { Mass = 518.32171, Intensity = 50d, },
+                            new SpectrumPeak { Mass = 526.32680, Intensity = 50d, },
+                            new SpectrumPeak { Mass = 544.33736, Intensity = 50d, },
+                            new SpectrumPeak { Mass = 599.50098, Intensity = 300d, },
+                            new SpectrumPeak { Mass = 723.49353, Intensity = 200d, },
+                            new SpectrumPeak { Mass = 782.56703, Intensity = 999d, },
+                        }
+                    },
+                };
+                yield return new object[] {
+                    lipid,
+                    new MSScanProperty
+                    {
+                        Spectrum = new List<SpectrumPeak>
+                        {
+                            new SpectrumPeak { Mass = 500.31115, Intensity = 50d, },
+                            new SpectrumPeak { Mass = 518.32171, Intensity = 50d, },
+                            new SpectrumPeak { Mass = 526.32680, Intensity = 50d, },
+                            new SpectrumPeak { Mass = 544.33736, Intensity = 50d, },
+                        }
+                    },
+                };
+                yield return new object[] {
+                    lipid,
+                    new MSScanProperty
+                    {
+                        Spectrum = new List<SpectrumPeak>
+                        {
+                            new SpectrumPeak { Mass = 500.31115, Intensity = 50d, },
+                            new SpectrumPeak { Mass = 518.32171, Intensity = 50d, },
+                            new SpectrumPeak { Mass = 526.32680, Intensity = 50d, },
+                            new SpectrumPeak { Mass = 544.33736, Intensity = 50d, },
                         }
                     },
                 };
