90222460fc406c1b00fcae418cc88ac2cdc8d165
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-07-24 18:26:18 +0900

    Add finding compounds process.

---
+++ src/CompMs.App/CompareAnnotationResult/CompoundTargetFinder.cs
@@ -0,0 +1,29 @@
+﻿using CompMs.Common.Components;
+using CompMs.MsdialCore.DataObj;
+
+namespace CompareAnnotationResult
+{
+    internal sealed class CompoundTargetFinder
+    {
+        private readonly MatchedSpotCandidateCalculator _calculator;
+        private readonly List<MoleculeMsReference> _references;
+
+        public CompoundTargetFinder(CommandLineData data) {
+            _calculator = new MatchedSpotCandidateCalculator(data.MzTolerance, data.RtTolerance, data.AmplitudeThreshold);
+            _references = data.GetLibrary();
+        }
+
+        public List<MatchedSpotCandidate<AlignmentSpotProperty>> Find(IEnumerable<AlignmentSpotProperty> spots) {
+            var candidates = new List<MatchedSpotCandidate<AlignmentSpotProperty>>();
+            foreach (var spot in spots) {
+                foreach (var reference in _references) {
+                    var candidate = _calculator.Match(spot, reference);
+                    if (candidate != null) {
+                        candidates.Add(candidate);
+                    }
+                }
+            }
+            return candidates;
+        }
+    }
+}
