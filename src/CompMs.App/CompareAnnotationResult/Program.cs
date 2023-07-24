eccd0dede7468dc4ec11753829ccd399d7c8bb88
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-07-24 18:26:41 +0900

    call the finding compound process.

--- src/CompMs.App/CompareAnnotationResult/Program.cs
+++ src/CompMs.App/CompareAnnotationResult/Program.cs
@@ -6,6 +6,10 @@ namespace CompareAnnotationResult
     {
         static void Main(string[] args) {
             var data = CommandLineParser.Parse<CommandLineData>(args);
+
+            var finder = new CompoundTargetFinder(data);
+            var candidate = finder.Find(data.LoadSpots().AlignmentSpotProperties);
+            
         }
     }
 }
