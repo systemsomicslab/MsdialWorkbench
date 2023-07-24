b1ab9d89b4abc363fe0d796f6b71a9f256f88019
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-07-24 18:23:48 +0900

    Add class for command line arguments.

---
+++ src/CompMs.App/CompareAnnotationResult/CommandLineData.cs
@@ -0,0 +1,29 @@
+﻿using CompMs.Common.Components;
+using CompMs.Common.Parser;
+using CompMs.MsdialCore.DataObj;
+
+namespace CompareAnnotationResult
+{
+    internal sealed class CommandLineData {
+        [LongStyleArgument("--input")]
+        public string? AlignmentResultPath { get; set; }
+        [LongStyleArgument("--output")]
+        public string? OutputPath { get; set; }
+        [LongStyleArgument("--library")]
+        public string? LibraryPath { get; set; }
+        [LongStyleArgument("--mz-tolerance")]
+        public double MzTolerance { get; set; }
+        [LongStyleArgument("--rt-tolerance")]
+        public double RtTolerance { get; set; }
+        [LongStyleArgument("--amplitude-threshold")]
+        public double AmplitudeThreshold { get; set; }
+
+        public List<MoleculeMsReference> GetLibrary() {
+            throw new NotImplementedException();
+        }
+
+        public AlignmentResultContainer LoadSpots() {
+            throw new NotImplementedException();
+        }
+    }
+}
