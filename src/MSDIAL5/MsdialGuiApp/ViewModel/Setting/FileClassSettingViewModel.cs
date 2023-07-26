79c40316d5f4f5193fb9367134a9fab0215ae3c2
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-07-26 10:18:05 +0900

    Update FileClassSettingViewModel

--- src/MSDIAL5/MsdialGuiApp/ViewModel/Setting/FileClassSettingViewModel.cs
+++ src/MSDIAL5/MsdialGuiApp/ViewModel/Setting/FileClassSettingViewModel.cs
@@ -1,23 +1,26 @@
 ﻿using CompMs.App.Msdial.Model.Setting;
-using CompMs.CommonMVVM;
+using CompMs.Graphics.UI;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
+using System.Windows.Input;
 
 namespace CompMs.App.Msdial.ViewModel.Setting
 {
-    internal sealed class FileClassSetViewModel : ViewModelBase
+    internal sealed class FileClassSetViewModel : SettingDialogViewModel
     {
         private readonly FileClassSetModel _model;
+        private ReactiveCommand _applyCommand, _cancelCommand;
 
         public FileClassSetViewModel(FileClassSetModel model) {
             _model = model;
             FileClassProperties = _model.FileClassProperties.ToReadOnlyReactiveCollection(m => new FileClassPropertyViewModel(m)).AddTo(Disposables);
-            ApplyCommand = new ReactiveCommand().WithSubscribe(Apply).AddTo(Disposables);
-            CancelCommand = new ReactiveCommand().WithSubscribe(Cancel).AddTo(Disposables);
+            _applyCommand = new ReactiveCommand().WithSubscribe(Apply).AddTo(Disposables);
+            _cancelCommand = new ReactiveCommand().WithSubscribe(Cancel).AddTo(Disposables);
         }
 
         public ReadOnlyReactiveCollection<FileClassPropertyViewModel> FileClassProperties { get; }
-        public ReactiveCommand ApplyCommand { get; }
+        public override ICommand ApplyCommand => _applyCommand;
+        public override ICommand FinishCommand => _applyCommand;
 
         private void Apply() {
             foreach (var property in FileClassProperties) {
@@ -25,7 +28,7 @@ namespace CompMs.App.Msdial.ViewModel.Setting
             }
         }
 
-        public ReactiveCommand CancelCommand { get; }
+        public override ICommand CancelCommand => _cancelCommand;
         private void Cancel() {
             foreach (var property in FileClassProperties) {
                 property.Discard();
