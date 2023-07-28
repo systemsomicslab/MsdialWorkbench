95e68ac221a4a9d50566e17a190ccdd1e1e0a9cd
htsugawa <htsugawa@go.tuat.ac.jp>
2023-07-28 17:34:03 +0900

    Fixed lysope/etherpe annotation filtering

--- src/Common/CommonStandard/Lipidomics/LipidEieioMsmsCharacterization.cs
+++ src/Common/CommonStandard/Lipidomics/LipidEieioMsmsCharacterization.cs
@@ -928,6 +928,7 @@ namespace CompMs.Common.Lipidomics
             int Sn1Carbon, int Sn2Carbon, int Sn1Double, int Sn2Double,
             AdductIon adduct)
         {
+            if (totalCarbon <= 28) return null; // currently carbon <= 28 is recognized as Lyso PE
             var spectrum = msScanProp.Spectrum;
             if (spectrum == null || spectrum.Count == 0) return null;
             if (adduct.IonMode == IonMode.Positive)
