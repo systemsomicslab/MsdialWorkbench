2326d880472081a3961a414b98c1e328a0b5f33e
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-07-28 11:55:21 +0900

    Update Omega3nChainGenerator

--- src/Common/CommonStandard/Lipidomics/Omega3nChainGenerator.cs
+++ src/Common/CommonStandard/Lipidomics/Omega3nChainGenerator.cs
@@ -51,10 +51,10 @@ namespace CompMs.Common.Lipidomics
             }
             var result = InnerEnumerateDoubleBond(carbon, doubleBond);
             if (carbon == 14 && doubleBond.Count == 1 && doubleBond.UnDecidedCount == 1) {
-                result = result.Prepend(new DoubleBond(DoubleBondInfo.Z(9)));
+                result = result.Prepend(new DoubleBond(DoubleBondInfo.Create(9)));
             }
             if (carbon == 16 && doubleBond.Count == 1 && doubleBond.UnDecidedCount == 1) {
-                result = result.Prepend(new DoubleBond(DoubleBondInfo.Z(9)));
+                result = result.Prepend(new DoubleBond(DoubleBondInfo.Create(9)));
             }
             if (carbon == 15 && doubleBond.Count == 1 && doubleBond.UnDecidedCount == 1) {
                 result = result.Prepend(new DoubleBond(DoubleBondInfo.Create(10)));
