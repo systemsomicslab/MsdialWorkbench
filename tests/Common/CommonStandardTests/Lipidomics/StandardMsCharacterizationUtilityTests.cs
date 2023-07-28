f3453b622d2940de86cb2103f7dbfc09e11fb776
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-07-28 11:43:31 +0900

    Update StandardMsCharacterizationUtitlityTests

--- tests/Common/CommonStandardTests/Lipidomics/StandardMsCharacterizationUtilityTests.cs
+++ tests/Common/CommonStandardTests/Lipidomics/StandardMsCharacterizationUtilityTests.cs
@@ -24,5 +24,51 @@ namespace CompMs.Common.Lipidomics.Tests
                 yield return new object[] { lipid, new LipidMsCharacterizationResult { IsChainIonsExisted = true, IsDoubleBondIonsExisted = true, }, expected3, };
             }
         }
+
+        [DataTestMethod()]
+        [DynamicData(nameof(RemoveEZForMGTestData))]
+        public void RemoveEZForMGTest(ILipid lipid, LipidMsCharacterizationResult characterizationResult, ILipid expected) {
+            (var actual, _) = StandardMsCharacterizationUtility.GetDefaultCharacterizationResultForGlycerophospholipid(lipid, characterizationResult);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        public static IEnumerable<object[]> RemoveEZForMGTestData {
+            get {
+                var lipid = new Lipid(Enum.LbmClass.MG, 100d, new PositionLevelChains(new AcylChain(16, new DoubleBond(DoubleBondInfo.Z(9)), new Oxidized(0))));
+                var expected1 = new Lipid(Enum.LbmClass.MG, 100d, new TotalChain(16, 1, 0, 1, 0, 0));
+                var expected2 = new Lipid(Enum.LbmClass.MG, 100d, new PositionLevelChains(new AcylChain(16, new DoubleBond(1), new Oxidized(0))));
+                var expected3 = new Lipid(Enum.LbmClass.MG, 100d, new PositionLevelChains(new AcylChain(16, new DoubleBond(DoubleBondInfo.Create(9)), new Oxidized(0))));
+
+                yield return new object[] { lipid, new LipidMsCharacterizationResult {  IsChainIonsExisted = false, IsPositionIonsExisted = false, IsDoubleBondIonsExisted = false, }, expected1, };
+                yield return new object[] { lipid, new LipidMsCharacterizationResult { IsChainIonsExisted = false, IsPositionIonsExisted = false, IsDoubleBondIonsExisted = true, }, expected1, };
+                yield return new object[] { lipid, new LipidMsCharacterizationResult { IsChainIonsExisted = false, IsPositionIonsExisted = true, IsDoubleBondIonsExisted = false, }, expected1, };
+                yield return new object[] { lipid, new LipidMsCharacterizationResult { IsChainIonsExisted = false, IsPositionIonsExisted = true, IsDoubleBondIonsExisted = true, }, expected1, };
+                yield return new object[] { lipid, new LipidMsCharacterizationResult { IsChainIonsExisted = true, IsPositionIonsExisted = false, IsDoubleBondIonsExisted = false, }, expected2, };
+                yield return new object[] { lipid, new LipidMsCharacterizationResult { IsChainIonsExisted = true, IsPositionIonsExisted = false, IsDoubleBondIonsExisted = true, }, expected3, };
+                yield return new object[] { lipid, new LipidMsCharacterizationResult { IsChainIonsExisted = true, IsPositionIonsExisted = true, IsDoubleBondIonsExisted = false, }, expected2, };
+                yield return new object[] { lipid, new LipidMsCharacterizationResult { IsChainIonsExisted = true, IsPositionIonsExisted = true, IsDoubleBondIonsExisted = true, }, expected3, };
+            }
+        }
+
+        [DataTestMethod()]
+        [DynamicData(nameof(RemoveEZForCETestData))]
+        public void RemoveEZForCETest(ILipid lipid, LipidMsCharacterizationResult characterizationResult, ILipid expected) {
+            (var actual, _) = StandardMsCharacterizationUtility.GetDefaultCharacterizationResultForSingleAcylChainLipid(lipid, characterizationResult);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        public static IEnumerable<object[]> RemoveEZForCETestData {
+            get {
+                var lipid = new Lipid(Enum.LbmClass.CE, 100d, new PositionLevelChains(new AcylChain(16, new DoubleBond(DoubleBondInfo.Z(9)), new Oxidized(0))));
+                var lipid2 = new Lipid(Enum.LbmClass.CE, 100d, new MolecularSpeciesLevelChains(new AcylChain(16, new DoubleBond(DoubleBondInfo.Z(9)), new Oxidized(0))));
+                var expected1 = new Lipid(Enum.LbmClass.CE, 100d, new TotalChain(16, 1, 0, 1, 0, 0));
+                var expected2 = new Lipid(Enum.LbmClass.CE, 100d, new PositionLevelChains(new AcylChain(16, new DoubleBond(1), new Oxidized(0))));
+                var expected3 = new Lipid(Enum.LbmClass.CE, 100d, new PositionLevelChains(new AcylChain(16, new DoubleBond(DoubleBondInfo.Create(9)), new Oxidized(0))));
+                yield return new object[] { lipid, new LipidMsCharacterizationResult { IsDoubleBondIonsExisted = false, }, expected2, };
+                yield return new object[] { lipid, new LipidMsCharacterizationResult { IsDoubleBondIonsExisted = true, }, expected3, };
+                yield return new object[] { lipid2, new LipidMsCharacterizationResult { IsDoubleBondIonsExisted = false, }, expected2, };
+                yield return new object[] { lipid2, new LipidMsCharacterizationResult { IsDoubleBondIonsExisted = true, }, expected3, };
+            }
+        }
     }
 }
