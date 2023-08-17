6c85aa52e823ac4b96bd05c64cebd9fda9e35da5
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-08-17 19:45:30 +0900

    Add AdductIon.GetAdductIon test.

---
+++ tests/Common/CommonStandardTests/DataObj/Property/AdductIonTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CompMs.Common.DataObj.Property.Tests
+{
+    [TestClass()]
+    public class AdductIonTests
+    {
+        [DataTestMethod()]
+        [DynamicData(nameof(GetAdductIonTestData))]
+        public void GetAdductIonTest(string adductName) {
+            var adductIon = AdductIon.GetAdductIon(adductName);
+            var newAdduct = AdductIon.GetAdductIon(adductIon.AdductIonName);
+            Assert.AreEqual(adductIon, newAdduct);
+        }
+
+        public static IEnumerable<object[]> GetAdductIonTestData {
+            get {
+                yield return new object[] { "[M+H]+", };
+                yield return new object[] { "[M-H]-", };
+                yield return new object[] { "[M+2H]2+", };
+                yield return new object[] { "[M+Na]+", };
+            }
+        }
+    }
+}
