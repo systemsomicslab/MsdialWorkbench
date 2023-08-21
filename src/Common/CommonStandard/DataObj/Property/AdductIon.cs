0f0c50af2d1e820a613e82373a9ce87dba990a2b
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-08-21 13:59:35 +0900

    Fix a bug of adduct ion setting.

--- src/Common/CommonStandard/DataObj/Property/AdductIon.cs
+++ src/Common/CommonStandard/DataObj/Property/AdductIon.cs
@@ -85,7 +85,7 @@ namespace CompMs.Common.DataObj.Property
         /// <param name="adductName">Add the formula string such as "C6H12O6"</param>
         /// <returns>AdductIon</returns>
         public static AdductIon GetAdductIon(string adductName) {
-            return ADDUCT_IONS.GetOrAdd(adductName);
+            return ADDUCT_IONS.GetOrAdd(adductName ?? string.Empty);
         }
 
         private static AdductIon GetAdductIonCore(string adductName) {
