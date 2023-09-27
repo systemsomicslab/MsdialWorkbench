ceafb20780a349239a703cec7659884c6918564d
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-09-27 19:42:51 +0900

    Replace MsSpectrumModel with AlignmentMs2Spectrummodel in imms project.

--- src/MSDIAL5/MsdialGuiApp/View/Imms/ImmsMainView.xaml.cs
+++ src/MSDIAL5/MsdialGuiApp/View/Imms/ImmsMainView.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Linq;
-using System.Reactive.Linq;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace CompMs.App.Msdial.View.Imms
 {
@@ -14,38 +10,6 @@ namespace CompMs.App.Msdial.View.Imms
     {
         public ImmsMainView() {
             InitializeComponent();
-
-            var mouseRDown = Observable.FromEvent<MouseButtonEventHandler, MouseButtonEventArgs>(
-                h => (s, e) => h(e),
-                h => MouseRightButtonDown += h,
-                h => MouseRightButtonDown -= h);
-            var mouseRUp = Observable.FromEvent<MouseButtonEventHandler, MouseButtonEventArgs>(
-                h => (s, e) => h(e),
-                h => MouseRightButtonUp += h,
-                h => MouseRightButtonUp -= h);
-            var mouseMove = Observable.FromEvent<MouseEventHandler, MouseEventArgs>(
-                h => (s, e) => h(e),
-                h => MouseMove += h,
-                h => MouseMove -= h);
-            var drag = mouseMove
-                .SkipUntil(mouseRDown)
-                .TakeUntil(mouseRUp)
-                .Repeat();
-
-            var mouseLeave = mouseRDown.Select(e => e.GetPosition(null))
-                .CombineLatest(drag.Select(e => e.GetPosition(null)), (p, q) => (p, q))
-                .Where(pq => Math.Abs(pq.p.X - pq.q.X) > SystemParameters.MinimumHorizontalDragDistance
-                            || Math.Abs(pq.p.Y - pq.q.Y) > SystemParameters.MinimumVerticalDragDistance);
-
-            var contextMenuOpening = Observable.FromEvent<ContextMenuEventHandler, ContextMenuEventArgs>(
-                h => (s, e) => h(e),
-                h => ContextMenuOpening += h,
-                h => ContextMenuOpening -= h);
-
-            contextMenuOpening
-                .TakeUntil(mouseLeave)
-                .Repeat()
-                .Subscribe(e => e.Handled = true);
         }
 
         private void TabItem_DtMzPairwisePlotPeakView_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e) {
