5d825553e8a3b1d1bce816123c049d4700932aa1
YukiMatsuzawa <122433968+YukiMatsuzawa@users.noreply.github.com>
2023-10-03 18:09:30 +0900

    Improve code behind of the MainWindow.xaml.cs (#223)
    * Change MainWindow.xaml.cs
    
    * Add SettingDialogViewModel
    
    * Add ShowChildSettingDialog method to MainWindow.xaml.cs
    
    * Change AnalysisFilePropertyResettingWindow and ViewModel.
    
    * Rename AnalysisFilePropertyResettingWindow.
    
    * Remove analysisFIlePropertyResetService.
    
    * Remove processSettingService from MainWindowVM.
    
    * Update FileClassSettingViewModel
    
    * Update MainWindow.xaml.cs
    
    * Update InternalStandardSetViewModel
    
    * Update FindTargetCompoundsSpotViewModel

---
+++ src/Common/ChartDrawing/UI/SettingDialogViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CompMs.CommonMVVM;
+using System.Windows.Input;
+
+namespace CompMs.Graphics.UI
+{
+    public class SettingDialogViewModel : ViewModelBase
+    {
+        public bool? Result {
+            get => _result;
+            set => SetProperty(ref _result, value);
+        }
+        private bool? _result;
+
+        public virtual ICommand ApplyCommand { get; }
+        public virtual ICommand CancelCommand { get; }
+        public virtual ICommand FinishCommand { get; }
+    }
+}
