fe27004389d00705a1d2fdb9041dc11dfb853456
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-10-11 21:57:13 +0900

    Add ColorToBrushConverter

---
+++ src/Common/ChartDrawing/Converter/ColorToBrushConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CompMs.Graphics.Converter
+{
+    [ValueConversion(typeof(Color), typeof(SolidColorBrush))]
+    public class ColorToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is Color color) {
+                var brush = new SolidColorBrush(color);
+                brush.Freeze();
+                return brush;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is SolidColorBrush brush) {
+                return brush.Color;
+            }
+            return Binding.DoNothing;
+        }
+    }
+}
