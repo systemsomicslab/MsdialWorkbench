52fd022224a364d04f4b4827657b9791341d6b03
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-10-19 10:12:26 +0900

    Add PeakFeatureSearchValueModel

---
+++ src/MSDIAL5/MsdialGuiApp/Model/Setting/PeakFeatureSearchValueModel.cs
@@ -0,0 +1,18 @@
+﻿using CompMs.CommonSourceGenerator.MVVM;
+using CompMs.MsdialCore.DataObj;
+
+namespace CompMs.App.Msdial.Model.Setting
+{
+    [WrapToBindableType(typeof(PeakFeatureSearchValue))]
+    internal partial class PeakFeatureSearchValueModel
+    {
+        public void ClearTitle() {
+            Title = string.Empty;
+        }
+
+        public void ClearMassInformation() {
+            Mass = 0d;
+            MassTolerance = 0d;
+        }
+    }
+}
