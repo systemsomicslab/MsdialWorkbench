8d332ce8982b7fc7bf202c992fe97c0e835a5882
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-10-19 12:19:27 +0900

    Add Clear methods to PeakFeatureSearchValueModel.

--- src/MSDIAL5/MsdialGuiApp/Model/Setting/PeakFeatureSearchValueModel.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/Setting/PeakFeatureSearchValueModel.cs
@@ -6,13 +6,17 @@ namespace CompMs.App.Msdial.Model.Setting
     [WrapToBindableType(typeof(PeakFeatureSearchValue))]
     internal partial class PeakFeatureSearchValueModel
     {
-        public void ClearTitle() {
+        public void ClearChromatogramSearchQuery() {
             Title = string.Empty;
-        }
-
-        public void ClearMassInformation() {
             Mass = 0d;
             MassTolerance = 0d;
+        }
+
+        public void ClearPeakSearchQuery() {
+            Mass = 0d;
+            MassTolerance = 0d;
+            RelativeIntensityCutoff = 0d;
+            PeakFeatureSearchType = PeakFeatureSearchType.ProductIon;
         }
     }
 }
