6e96670c8812b621fa0fa458800ba73afad1abf5
Bujinlkham BUYANTOGTOKH <s238586u@st.me.tuat.ac.jp>
2023-10-20 15:43:20 +0900

    Button that generates and displays a graph and saves as a image.

--- src/MSDIAL5/MsdialGuiApp/View/RibbonControl/MsdialProcessingTab.xaml.cs
+++ src/MSDIAL5/MsdialGuiApp/View/RibbonControl/MsdialProcessingTab.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows.Controls.Ribbon;
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls.Ribbon;
+using System.Windows.Media.Imaging;
+using RDotNet;
+using System.Drawing;
 
 namespace CompMs.App.Msdial.View.RibbonControl
 {
@@ -7,9 +13,32 @@ namespace CompMs.App.Msdial.View.RibbonControl
     /// </summary>
     public partial class MsdialProcessingTab : RibbonTab
     {
+
         public MsdialProcessingTab()
         {
             InitializeComponent();
+        }
+        private void generateGraph_Click(object sender, RoutedEventArgs e)
+        {
+            REngine.SetEnvironmentVariables();
+            var engine = REngine.GetInstance();
+            engine.Initialize();
+            engine.Evaluate("x <- c(1, 2, 3, 4, 5)");
+            engine.Evaluate("y <- c(10, 15, 13, 18, 20)");
+            engine.Evaluate("plot(x, y, type='l')");
+
+            engine.Evaluate("dev.copy(png, 'graph.png')");
+            engine.Evaluate("dev.off()");
+
+            MessageBox.Show("Graph generated and saved as 'my_graph.png'");
+        }
+        private void buttonExit_Click(object sender, RoutedEventArgs e)
+        {
+            REngine.SetEnvironmentVariables();
+            var engine = REngine.GetInstance();
+            engine.Initialize();
+            engine.Dispose();
+            Application.Current.Shutdown();
         }
     }
 }
