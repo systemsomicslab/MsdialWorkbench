67ca27e32055485a70a04067977929e39c22bcd9
Bujinlkham BUYANTOGTOKH <s238586u@st.me.tuat.ac.jp>
2023-10-20 16:16:34 +0900

    Notame.cs

--- src/MSDIAL5/MsdialGuiApp/View/RibbonControl/MsdialProcessingTab.xaml.cs
+++ src/MSDIAL5/MsdialGuiApp/View/RibbonControl/MsdialProcessingTab.xaml.cs
@@ -18,27 +18,5 @@ namespace CompMs.App.Msdial.View.RibbonControl
         {
             InitializeComponent();
         }
-        private void generateGraph_Click(object sender, RoutedEventArgs e)
-        {
-            REngine.SetEnvironmentVariables();
-            var engine = REngine.GetInstance();
-            engine.Initialize();
-            engine.Evaluate("x <- c(1, 2, 3, 4, 5)");
-            engine.Evaluate("y <- c(10, 15, 13, 18, 20)");
-            engine.Evaluate("plot(x, y, type='l')");
-
-            engine.Evaluate("dev.copy(png, 'graph.png')");
-            engine.Evaluate("dev.off()");
-
-            MessageBox.Show("Graph generated and saved as 'my_graph.png'");
-        }
-        private void buttonExit_Click(object sender, RoutedEventArgs e)
-        {
-            REngine.SetEnvironmentVariables();
-            var engine = REngine.GetInstance();
-            engine.Initialize();
-            engine.Dispose();
-            Application.Current.Shutdown();
-        }
     }
 }
