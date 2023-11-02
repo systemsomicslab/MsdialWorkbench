ffb5fdda8865610cf8347e5b285bb551696a8c8b
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-11-02 14:47:07 +0900

    Add alignment peak spots bar chart model to gcms project

--- src/MSDIAL5/MsdialGuiApp/Model/Gcms/GcmsAlignmentModel.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/Gcms/GcmsAlignmentModel.cs
@@ -1,17 +1,28 @@
 ﻿using CompMs.App.Msdial.Model.Chart;
 using CompMs.App.Msdial.Model.Core;
 using CompMs.App.Msdial.Model.DataObj;
+using CompMs.App.Msdial.Model.Loader;
 using CompMs.App.Msdial.Model.Search;
 using CompMs.App.Msdial.Model.Service;
+using CompMs.App.Msdial.Model.Statistics;
+using CompMs.App.Msdial.Utility;
 using CompMs.Common.Components;
 using CompMs.Common.DataObj.Result;
+using CompMs.Common.Enum;
+using CompMs.Graphics.Base;
+using CompMs.Graphics.Design;
 using CompMs.MsdialCore.Algorithm.Annotation;
+using CompMs.MsdialCore.DataObj;
 using CompMs.MsdialCore.Parser;
 using CompMs.MsdialGcMsApi.Parameter;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
 using Reactive.Bindings.Notifiers;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
 
 namespace CompMs.App.Msdial.Model.Gcms
 {
@@ -20,15 +31,20 @@ namespace CompMs.App.Msdial.Model.Gcms
         private readonly AlignmentFileBeanModel _alignmentFileBean;
         private readonly IMessageBroker _broker;
         private readonly UndoManager _undoManager;
+        private readonly ReactiveProperty<BarItemsLoaderData> _barItemsLoaderDataProperty;
 
         public GcmsAlignmentModel(
             AlignmentFileBeanModel alignmentFileBean,
             IMatchResultEvaluator<MsScanMatchResult> evaluator,
             PeakSpotFiltering<AlignmentSpotPropertyModel> peakSpotFiltering,
             PeakFilterModel peakFilterModel,
+            DataBaseMapper mapper,
             MsdialGcmsParameter parameter,
+            ProjectBaseParameterModel projectBaseParameter,
+            List<AnalysisFileBean> files,
+            AnalysisFileBeanModelCollection fileCollection,
             IMessageBroker broker)
-            :base(alignmentFileBean, broker)
+            : base(alignmentFileBean, broker)
         {
             _alignmentFileBean = alignmentFileBean;
             _broker = broker;
@@ -39,6 +55,9 @@ namespace CompMs.App.Msdial.Model.Gcms
 
             var spotsSource = new AlignmentSpotSource(alignmentFileBean, Container, chromatogramSpotSerializer).AddTo(Disposables);
             var ms1Spots = spotsSource.Spots.Items;
+
+            InternalStandardSetModel = new InternalStandardSetModel(spotsSource.Spots.Items, TargetMsMethod.Gcms).AddTo(Disposables);
+            NormalizationSetModel = new NormalizationSetModel(Container, files, fileCollection, mapper, evaluator, InternalStandardSetModel, parameter, broker).AddTo(Disposables);
 
             var filterRegistrationManager = new FilterRegistrationManager<AlignmentSpotPropertyModel>(ms1Spots, peakSpotFiltering).AddTo(Disposables);
             var PeakSpotNavigatorModel = filterRegistrationManager.PeakSpotNavigatorModel;
@@ -57,9 +76,39 @@ namespace CompMs.App.Msdial.Model.Gcms
                 HorizontalTitle = "Retention time [min]", // TODO: RI
                 VerticalTitle = "m/z",
             }.AddTo(Disposables);
+
+
+            // Class intensity bar chart
+            var classBrush = projectBaseParameter.ClassProperties
+                .CollectionChangedAsObservable().ToUnit()
+                .StartWith(Unit.Default)
+                .SelectSwitch(_ => projectBaseParameter.ClassProperties.Select(prop => prop.ObserveProperty(p => p.Color).Select(_2 => prop)).CombineLatest())
+                .Select(lst => new KeyBrushMapper<string>(lst.ToDictionary(item => item.Name, item => item.Color)))
+                .ToReactiveProperty().AddTo(Disposables);
+            var barBrush = classBrush.Select(bm => bm.Contramap((BarItem item) => item.Class));
+
+            var fileIdToClassNameAsObservable = projectBaseParameter.ObserveProperty(p => p.FileIdToClassName).ToReadOnlyReactivePropertySlim().AddTo(Disposables);
+            var peakSpotAxisLabelAsObservable = target.OfType<AlignmentSpotPropertyModel>().SelectSwitch(t => t.ObserveProperty(t_ => t_.IonAbundanceUnit).Select(t_ => t_.ToLabel())).ToReadOnlyReactivePropertySlim().AddTo(Disposables);
+            var normalizedAreaZeroLoader = new BarItemsLoaderData("Normalized peak area above zero", peakSpotAxisLabelAsObservable, new NormalizedAreaAboveZeroBarItemsLoader(fileIdToClassNameAsObservable, fileCollection), NormalizationSetModel.IsNormalized);
+            var normalizedAreaBaselineLoader = new BarItemsLoaderData("Normalized peak area above base line", peakSpotAxisLabelAsObservable, new NormalizedAreaAboveBaseLineBarItemsLoader(fileIdToClassNameAsObservable, fileCollection), NormalizationSetModel.IsNormalized);
+            var normalizedHeightLoader = new BarItemsLoaderData("Normalized peak height", peakSpotAxisLabelAsObservable, new NormalizedHeightBarItemsLoader(fileIdToClassNameAsObservable, fileCollection), NormalizationSetModel.IsNormalized);
+            var areaZeroLoader = new BarItemsLoaderData("Peak area above zero", "Area", new AreaAboveZeroBarItemsLoader(fileIdToClassNameAsObservable, fileCollection));
+            var areaBaselineLoader = new BarItemsLoaderData("Peak area above base line", "Area", new AreaAboveBaseLineBarItemsLoader(fileIdToClassNameAsObservable, fileCollection));
+            var heightLoader = new BarItemsLoaderData("Peak height", "Height", new HeightBarItemsLoader(fileIdToClassNameAsObservable, fileCollection));
+            var barItemLoaderDatas = new[]
+            {
+                heightLoader, areaBaselineLoader, areaZeroLoader,
+                normalizedHeightLoader, normalizedAreaBaselineLoader, normalizedAreaZeroLoader,
+            };
+            var barItemsLoaderDataProperty = NormalizationSetModel.Normalized.ToConstant(normalizedHeightLoader).ToReactiveProperty(NormalizationSetModel.IsNormalized.Value ? normalizedHeightLoader : heightLoader).AddTo(Disposables);
+            _barItemsLoaderDataProperty = barItemsLoaderDataProperty;
+            BarChartModel = new BarChartModel(target, barItemsLoaderDataProperty, barItemLoaderDatas, barBrush, projectBaseParameter, fileCollection, projectBaseParameter.ClassProperties).AddTo(Disposables);
         }
 
         public AlignmentPeakPlotModel PlotModel { get; }
+        public BarChartModel BarChartModel { get; }
+        public InternalStandardSetModel InternalStandardSetModel { get; }
+        public NormalizationSetModel NormalizationSetModel { get; }
 
         public override void InvokeMoleculerNetworkingForTargetSpot() {
             throw new NotImplementedException();
