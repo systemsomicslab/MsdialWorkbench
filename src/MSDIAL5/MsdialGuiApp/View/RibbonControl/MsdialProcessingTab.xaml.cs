3acfc75a7850aa8428b926ff021fdc2353bedea9
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-11-10 11:09:34 +0900

    Update MsidalProcessingTab

--- src/MSDIAL5/MsdialGuiApp/View/RibbonControl/MsdialProcessingTab.xaml.cs
+++ src/MSDIAL5/MsdialGuiApp/View/RibbonControl/MsdialProcessingTab.xaml.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls.Ribbon;
-using RDotNet;
-using System.Windows.Media.Imaging;
-using System.Drawing;
-using CompMs.App.Msdial.ViewModel.Core;
+﻿using System.Windows.Controls.Ribbon;
 
 namespace CompMs.App.Msdial.View.RibbonControl
 {
@@ -14,7 +7,6 @@ namespace CompMs.App.Msdial.View.RibbonControl
     /// </summary>
     public partial class MsdialProcessingTab : RibbonTab
     {
-
         public MsdialProcessingTab()
         {
             InitializeComponent();
