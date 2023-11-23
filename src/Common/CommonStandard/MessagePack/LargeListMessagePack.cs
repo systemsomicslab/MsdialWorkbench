1992c1fbbfc8f594087c6f4eda667826b0d36872
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-11-23 18:26:25 +0900

    Update LargeListMessagePack

--- src/Common/CommonStandard/MessagePack/LargeListMessagePack.cs
+++ src/Common/CommonStandard/MessagePack/LargeListMessagePack.cs
@@ -1,5 +1,4 @@
 ﻿using MessagePack;
-using MessagePack.Resolvers;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,43 +9,7 @@ namespace CompMs.Common.MessagePack
 {
     public static class LargeListMessagePack
     {
-        public static int OffsetCutoff = 1073741824;
         public const sbyte ExtensionTypeCode = 99;
-        public const int NotCompressionSize = 64;
-        public const int HeaderSize = 11;
-
-        static IFormatterResolver defaultResolver;
-
-        static byte[] buffer = null;
-        static byte[] bufferLz = null;
-
-        public static byte[] GetBuffer()
-        {
-            if (buffer == null)
-            {
-                buffer = new byte[65536];
-            }
-            return buffer;
-        }
-
-        public static byte[] GetBufferLZ4()
-        {
-            if (bufferLz == null)
-            {
-                bufferLz = new byte[65536];
-            }
-            return bufferLz;
-        }
-
-        public static IFormatterResolver DefaultResolver {
-            get {
-                if (defaultResolver == null)
-                {
-                    return StandardResolver.Instance;
-                }
-                return defaultResolver;
-            }
-        }
 
         public static void Serialize<T>(Stream stream, IReadOnlyList<T> value) {
             var pipeWriter = PipeWriter.Create(stream, new StreamPipeWriterOptions(leaveOpen: true));
@@ -73,8 +36,6 @@ namespace CompMs.Common.MessagePack
                     pipeWriter.Complete();
 
                     writer.WriteExtensionFormat(new ExtensionResult(ExtensionTypeCode, memory.ToArray()));
-                    //writer.WriteExtensionFormatHeader(new ExtensionHeader(ExtensionTypeCode, 9000));
-                    //writer.WriteInt32(53); // dummy value
                 }
             }
         }
@@ -86,11 +47,6 @@ namespace CompMs.Common.MessagePack
                 var reader = new MessagePackReader(new ReadOnlyMemory<byte>(memory.GetBuffer(), 0, (int)memory.Length));
                 return DeserializeCore<T>(ref reader, MessagePackSerializerOptions.Standard);
             }
-        }
-
-        public static List<T> Deserialize<T>(ref MessagePackReader reader, MessagePackSerializerOptions options)
-        {
-            return DeserializeCore<T>(ref reader, options);
         }
 
         public static IEnumerable<List<T>> DeserializeIncremental<T>(Stream stream) {
@@ -136,11 +92,11 @@ namespace CompMs.Common.MessagePack
                 if (header.TypeCode == ExtensionTypeCode)
                 {
                     // decode lz4
-                    var _length = reader.ReadInt32();
+                    //var _length = reader.ReadInt32();
                     if (!reader.End)
                     {
                         // LZ4 Decode
-                        var newOptions = options.WithCompression(MessagePackCompression.Lz4Block);
+                        var newOptions = options.WithCompression(MessagePackCompression.Lz4Block).WithCompressionMinLength(1);
                         return DeserializeList<T>(ref reader, newOptions);
                     }
                 }
@@ -156,14 +112,7 @@ namespace CompMs.Common.MessagePack
             }
             else
             {
-                var formatter = options.Resolver.GetFormatterWithVerify<T>();
-                var len = reader.ReadArrayHeader();
-                var list = new List<T>(len);
-                for (int i = 0; i < len; i++)
-                {
-                    list.Add(formatter.Deserialize(ref reader, options));
-                }
-                return list;
+                return MessagePackSerializer.Deserialize<List<T>>(ref reader, options);
             }
         }
 
@@ -196,13 +145,11 @@ namespace CompMs.Common.MessagePack
                 if (header.TypeCode == ExtensionTypeCode)
                 {
                     // decode lz4
-                    var _length = reader.ReadInt32();
-                    var newOptions = options.WithCompression(MessagePackCompression.Lz4Block);
-                    var formatter = newOptions.Resolver.GetFormatterWithVerify<T[]>();
+                    var newOptions = options.WithCompression(MessagePackCompression.Lz4Block).WithCompressionMinLength(1);
                     if (!reader.End)
                     {
                         // LZ4 Decode
-                        var results = formatter.Deserialize(ref reader, newOptions);
+                        var results = MessagePackSerializer.Deserialize<T[]>(ref reader, newOptions);
                         skipArraySize = results.Length;
                         if (skipArraySize > index) {
                             result = results[index];
