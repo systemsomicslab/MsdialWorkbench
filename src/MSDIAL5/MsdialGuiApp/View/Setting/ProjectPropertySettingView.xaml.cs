b60f68016688e5d5ecbac6a5d68fa398cd79b421
mikiko.takahashi <mikiko.takahashi@riken.jp>
2023-12-01 08:13:15 +0900

    Merge branch 'master' into mikiko.takahashi
    # Conflicts:
    #	src/MSDIAL5/MsdialGuiApp/Properties/Resources.resx

---
+++ src/MSDIAL5/MsdialGuiApp/View/Setting/ProjectPropertySettingView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace CompMs.App.Msdial.View.Setting
+{
+    /// <summary>
+    /// Interaction logic for ProjectPropertySettingView.xaml
+    /// </summary>
+    public partial class ProjectPropertySettingView : UserControl
+    {
+        public ProjectPropertySettingView() {
+            InitializeComponent();
+        }
+    }
+}
