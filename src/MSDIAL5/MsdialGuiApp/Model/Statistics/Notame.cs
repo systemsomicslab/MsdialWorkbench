d9b3aee7141221c352b4c0b58ee524f06f0fa9aa
Bujinlkham BUYANTOGTOKH <s238586u@st.me.tuat.ac.jp>
2023-12-04 13:23:32 +0900

    Binded Notame C# to Notame Rscript

--- src/MSDIAL5/MsdialGuiApp/Model/Statistics/Notame.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/Statistics/Notame.cs
@@ -1,41 +1,13 @@
-//using RDotNet;
-//using System.Windows;
-
-//namespace CompMs.App.Msdial.Model.Statistics
-//{
-//    public sealed class Notame
-//    {
-//        public void Run()
-//        {
-//            REngine.SetEnvironmentVariables();
-//            REngine.SetEnvironmentVariables("c:/program files/r/r-4.3.2/bin/x64", "c:/program files/r/r-4.3.2");
-//            var engine = REngine.GetInstance();
-//            //engine.Initialize();
-//            engine.Evaluate("Sys.setenv(PATH = paste(\"C:/Program Files/R/R-4.3.2/bin/x64\", Sys.getenv(\"PATH\"), sep=\";\"))");
-//            engine.Evaluate("library(stats)");
-//            engine.Evaluate("source('c:/src/myscript.r')");
-
-//            //engine.Evaluate("x <- c(1, 2, 3, 4, 5)");
-//            //engine.Evaluate("y <- c(10, 15, 13, 18, 20)");
-//            //engine.Evaluate("plot(x, y, type='l')");
-
-//            //engine.Evaluate("dev.copy(png, 'graph.png')");
-//            //engine.Evaluate("dev.off()");
-
-//            MessageBox.Show("Graph generated and saved as 'graph.png'");
-//        }
-//    }
-//}
-
+using Newtonsoft.Json;
 using RDotNet;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.IO.Pipes;
 using System.Text;
 using System.Windows.Forms;
-using Newtonsoft.Json;
 
 namespace CompMs.App.Msdial.Model.Statistics
 {
@@ -113,6 +85,7 @@ namespace CompMs.App.Msdial.Model.Statistics
             string groupingNameValue = _groupingName.Text;
 
             SaveParametersAsJson(pathValue, fileNameValue, ionModValue, groupingNameValue);
+            SendParametersToNotame(pathValue, fileNameValue, ionModValue, groupingNameValue);
         }
 
         private void SaveParametersAsJson(string path, string fileName, string ionMod, string groupingName)
@@ -129,22 +102,60 @@ namespace CompMs.App.Msdial.Model.Statistics
             string filePath = "C:/src/NotameParameters.json";
             File.WriteAllText(filePath, jsonParameter);
             MessageBox.Show("Parameters saved to the JSON file.");
-
-            SendParametersToNotame(jsonParameter);
         }
 
-        private void SendParametersToNotame(string jsonParameter)
+        private void SendParametersToNotame(string path, string fileName, string ionMod, string groupingName)
         {
-            using (var pipeClient = new NamedPipeClientStream("notameParameters"))
-            {
-                pipeClient.Connect();
+            REngine.SetEnvironmentVariables();
+            REngine.SetEnvironmentVariables("c:/program files/r/r-4.3.2/bin/x64", "c:/program files/r/r-4.3.2");
+            var engine = REngine.GetInstance();
+            engine.Evaluate("Sys.setenv(PATH = paste(\"C:/Program Files/R/R-4.3.2/bin/x64\", Sys.getenv(\"PATH\"), sep=\";\"))");
+            engine.Evaluate("library(notame)");
+            engine.Evaluate("library(doParallel)");
+            engine.Evaluate("library(dplyr)");
+            engine.Evaluate("library(openxlsx)");
+            engine.SetSymbol("path", engine.CreateCharacter(path));
+            engine.SetSymbol("file_name", engine.CreateCharacter(fileName));
+            engine.SetSymbol("ion_mod", engine.CreateCharacter(ionMod));
+            engine.SetSymbol("grouping_name", engine.CreateCharacter(groupingName));
 
-                using (var writer = new StreamWriter(pipeClient))
-                {
-                    writer.Write(jsonParameter);
-                }
-            }
-            MessageBox.Show("Parameters sent to the Notame.");
+            string rScript = @"
+                data <- notame::read_from_excel(file = paste0(path, file_name), sheet = 1, name = ion_mod)
+
+                rownames(data$exprs) <- gsub("" "", ""_"", rownames(data$exprs))
+                rownames(data$feature_data) <- gsub("" "", ""_"", rownames(data$feature_data))
+                data$feature_data$Feature_ID <- gsub("" "", ""_"", data$feature_data$Feature_ID)
+                data$feature_data$Split <- gsub("" "", ""_"", data$feature_data$Split)
+
+                modes <- notame::construct_metabosets(exprs = data$exprs, pheno_data = data$pheno_data,
+                                             feature_data = data$feature_data,
+                                             group_col = grouping_name)
+
+                metaboset <- notame::merge_metabosets(modes)
+
+                num_cores <- parallel::detectCores() - 5
+                cl <- parallel::makeCluster(num_cores)
+                doParallel::registerDoParallel(cl)
+
+                metaboset <- notame::mark_nas(metaboset, value = 0)
+                metaboset <- notame::flag_detection(metaboset)
+
+                corrected <- notame::correct_drift(metaboset)
+
+                corrected <- notame::flag_quality(corrected)
+
+                merged_no_qc <- notame::drop_qcs(corrected)
+
+                set.seed(1234567)
+                imputed <- notame::impute_rf(merged_no_qc)
+                imputed <- notame::impute_rf(imputed, all_features = TRUE)
+
+                parallel::stopCluster(cl)
+
+                saveRDS(imputed, file = paste0(path, ""full_data.RDS""))
+                write_to_excel(imputed, file = paste0(path, ""full_data.xlsx""))
+            ";
+            engine.Evaluate(rScript);
         }
     }
 }
