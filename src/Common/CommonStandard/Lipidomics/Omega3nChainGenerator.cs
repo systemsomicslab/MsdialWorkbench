2345cbf72a67e2516dae240ac042fcdecea1c3af
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-12-06 18:56:45 +0900

    Update code

--- src/Common/CommonStandard/Lipidomics/Omega3nChainGenerator.cs
+++ src/Common/CommonStandard/Lipidomics/Omega3nChainGenerator.cs
@@ -252,7 +252,7 @@ namespace CompMs.Common.Lipidomics
             public IEnumerable<IDoubleBond> Append(int carbon, IDoubleBond baseBond) {
                 if (_map.TryGetValue((carbon, baseBond.Count), out var bonds)) {
                     if (baseBond.DecidedCount >= 1) {
-                        return bonds.Where(b => b.Includes(baseBond));
+                        return bonds.Where(baseBond.Includes);
                     }
                     return bonds;
                 }
