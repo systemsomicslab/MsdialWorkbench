07c4d70718a5b93e3e0a7218b8b473c05401a16d
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-12-13 01:55:36 +0900

    Add LoadProjectPresenter

---
+++ src/CompMs.App/MsdialServer/Presenter/ILoadProjectPresenter.cs
@@ -0,0 +1,9 @@
+﻿using CompMs.App.MsdialServer.Entity;
+using Reactive.Bindings;
+
+namespace CompMs.App.MsdialServer.Presenter;
+
+public interface ILoadProjectPresenter {
+    IReadOnlyReactiveProperty<ProjectDataFile[]> Entries { get; }
+    Task SearchEntriesAsync();
+}
---
+++ src/CompMs.App/MsdialServer/Presenter/LoadProjectPresenter.cs
@@ -0,0 +1,14 @@
+﻿using CompMs.App.MsdialServer.Entity;
+using CompMs.App.MsdialServer.Usecase;
+using Reactive.Bindings;
+
+namespace CompMs.App.MsdialServer.Presenter;
+
+public sealed class LoadProjectPresenter(IListEntriesUsecase listEntries) : ILoadProjectPresenter
+{
+    private readonly IListEntriesUsecase _listEntries = listEntries;
+
+    public IReadOnlyReactiveProperty<ProjectDataFile[]> Entries => _listEntries.Entries;
+
+    public Task SearchEntriesAsync() => _listEntries.SearchEntriesAsync();
+}
