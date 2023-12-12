3d9a67e5aad7c84b5ff3238c966a6550eb1fee63
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-12-13 01:55:24 +0900

    Add ListEntriesUsecase

---
+++ src/CompMs.App/MsdialServer/Usecase/IListEntriesUsecase.cs
@@ -0,0 +1,9 @@
+﻿using CompMs.App.MsdialServer.Entity;
+using Reactive.Bindings;
+
+namespace CompMs.App.MsdialServer.Usecase;
+
+public interface IListEntriesUsecase {
+    public IReadOnlyReactiveProperty<ProjectDataFile[]> Entries { get; }
+    public Task SearchEntriesAsync();
+}
---
+++ src/CompMs.App/MsdialServer/Usecase/ListEntriesUsecase.cs
@@ -0,0 +1,30 @@
+﻿using CompMs.App.MsdialServer.Entity;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System.Reactive.Disposables;
+
+namespace CompMs.App.MsdialServer.Usecase;
+public sealed class ListEntriesUsecase : IListEntriesUsecase, IDisposable
+{
+    private readonly ReactivePropertySlim<ProjectDataFile[]> _entries;
+    private CompositeDisposable _disposables = new();
+
+    public ListEntriesUsecase()
+    {
+        _entries = new ReactivePropertySlim<ProjectDataFile[]>([]).AddTo(_disposables);
+    }
+
+    public IReadOnlyReactiveProperty<ProjectDataFile[]> Entries => _entries;
+
+    public Task SearchEntriesAsync() {
+        _entries.Value = Directory.GetFileSystemEntries(@"D:\MS-DIAL demo files\2023_software\wine_metabolomics\pos")
+            .Where(entry => entry.EndsWith(".mdproject"))
+            .Select(entry => new ProjectDataFile(entry))
+            .ToArray();
+        return Task.CompletedTask;
+    }
+
+    public void Dispose() {
+        _disposables.Dispose();
+    }
+}
