0d6b252004b14ebd89b069c29715b3a4cbb57524
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-12-13 01:54:38 +0900

    Add ProjectDataFile

---
+++ src/CompMs.App/MsdialServer/Entity/ProjectDataFile.cs
@@ -0,0 +1,18 @@
+﻿namespace CompMs.App.MsdialServer.Entity;
+
+public sealed class ProjectDataFile
+{
+    public ProjectDataFile(string path)
+    {
+        if (!path.EndsWith(".mdproject")) {
+            throw new ArgumentException("Invalid project file.");
+        }
+
+        Path = path;
+        Name = System.IO.Path.GetFileNameWithoutExtension(path);
+    }
+
+    public string Name { get; }
+
+    public string Path { get; }
+}
