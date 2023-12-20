0e3beb888a89a1180bff4e9dc38accae3518966b
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-12-20 22:28:45 +0900

    Add PeakSpotModel class

---
+++ src/MSDIAL5/MsdialGuiApp/Model/DataObj/PeakSpotModel.cs
@@ -0,0 +1,17 @@
+﻿using CompMs.MsdialCore.MSDec;
+
+namespace CompMs.App.Msdial.Model.DataObj
+{
+    internal sealed class PeakSpotModel
+    {
+        public PeakSpotModel(ChromatogramPeakFeatureModel peakSpot, MSDecResult msdec)
+        {
+            PeakSpot = peakSpot;
+            MSDecResult = msdec;
+        }
+
+        public IPeakSpotModel PeakSpot { get; }
+
+        public MSDecResult MSDecResult { get; }
+    }
+}
