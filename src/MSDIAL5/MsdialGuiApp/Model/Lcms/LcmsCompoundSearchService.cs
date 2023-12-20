a8d6031f18b3f9332d6ed87c695a02f08b4a0ae3
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-12-20 22:29:03 +0900

    Add LcmsCompoundSearchService class

---
+++ src/MSDIAL5/MsdialGuiApp/Model/Lcms/LcmsCompoundSearchService.cs
@@ -0,0 +1,38 @@
+﻿using CompMs.App.Msdial.Model.DataObj;
+using CompMs.App.Msdial.Model.Search;
+using CompMs.Common.DataObj;
+using CompMs.CommonMVVM;
+using CompMs.MsdialCore.DataObj;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompMs.App.Msdial.Model.Lcms
+{
+    internal sealed class LcmsCompoundSearchService : BindableBase
+    {
+        public LcmsCompoundSearchService(IReadOnlyList<CompoundSearcher> compoundSearchers) {
+            CompoundSearchers = compoundSearchers;
+        }
+
+        public IReadOnlyList<CompoundSearcher> CompoundSearchers { get; }
+
+        public CompoundSearcher SelectedCompoundSearcher {
+            get => _compoundSearcher;
+            set => SetProperty(ref _compoundSearcher, value);
+        }
+        private CompoundSearcher _compoundSearcher;
+
+        public LcmsCompoundResult[] Search(PeakSpotModel peakSpot) {
+            var results = SelectedCompoundSearcher?.Search(
+                peakSpot.PeakSpot.MSIon,
+                peakSpot.MSDecResult,
+                new List<RawPeakElement>(),
+                new IonFeatureCharacter { IsotopeWeightNumber = 0, } // Assume this is not isotope.
+            );
+            if (results is null) {
+                return System.Array.Empty<LcmsCompoundResult>();
+            }
+            return results.Select(r => new LcmsCompoundResult(r)).ToArray();
+        }
+    }
+}
