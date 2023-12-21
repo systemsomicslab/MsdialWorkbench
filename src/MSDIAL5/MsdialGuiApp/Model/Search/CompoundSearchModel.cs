c1e05fffb03bcea9b8ca43cecc6bbe29febd6081
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2023-12-21 11:32:49 +0900

    Update CompoundSearchModel

--- src/MSDIAL5/MsdialGuiApp/Model/Search/CompoundSearchModel.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/Search/CompoundSearchModel.cs
@@ -1,6 +1,4 @@
-﻿using Accord;
-using CompMs.App.Msdial.Common;
-using CompMs.App.Msdial.Model.Chart;
+﻿using CompMs.App.Msdial.Model.Chart;
 using CompMs.App.Msdial.Model.DataObj;
 using CompMs.App.Msdial.Model.Service;
 using CompMs.App.Msdial.Utility;
@@ -9,19 +7,14 @@ using CompMs.Common.Components;
 using CompMs.Common.DataObj;
 using CompMs.Common.DataObj.Result;
 using CompMs.CommonMVVM;
-using CompMs.Graphics.AxisManager.Generic;
-using CompMs.Graphics.Core.Base;
 using CompMs.MsdialCore.DataObj;
-using CompMs.MsdialCore.Export;
 using CompMs.MsdialCore.MSDec;
-using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
-using System.Windows.Media;
 
 namespace CompMs.App.Msdial.Model.Search
 {
@@ -54,6 +47,7 @@ namespace CompMs.App.Msdial.Model.Search
         private readonly MSDecResult _msdecResult;
         private readonly UndoManager _undoManager;
         private readonly IPeakSpotModel _peakSpot;
+        private readonly PlotComparedMsSpectrumService _plotService;
 
         public CompoundSearchModel(IFileBean fileBean, IPeakSpotModel peakSpot, MSDecResult msdecResult, CompoundSearcherCollection compoundSearchers, UndoManager undoManager)
             : this(fileBean, peakSpot, msdecResult, compoundSearchers.Items, undoManager) {
@@ -68,41 +62,13 @@ namespace CompMs.App.Msdial.Model.Search
             SelectedCompoundSearcher = CompoundSearchers.FirstOrDefault();
             _msdecResult = msdecResult ?? throw new ArgumentNullException(nameof(msdecResult));
 
-            var referenceSpectrum = this.ObserveProperty(m => m.SelectedReference)
-                .SkipNull()
-                .Select(c => new MsSpectrum(c.Spectrum))
-                .ToReadOnlyReactivePropertySlim()
-                .AddTo(Disposables);
-            var scorer = this.ObserveProperty(m => m.SelectedCompoundSearcher)
+            _plotService = new PlotComparedMsSpectrumService(msdecResult).AddTo(Disposables);
+            this.ObserveProperty(m => SelectedReference)
+                .Subscribe(_plotService.UpdateReference).AddTo(Disposables);
+            this.ObserveProperty(m => SelectedCompoundSearcher)
                 .SkipNull()
                 .Select(s => new Ms2ScanMatching(s.MsRefSearchParameter))
-                .ToReadOnlyReactivePropertySlim()
-                .AddTo(Disposables);
-            GraphLabels msGraphLabels = new GraphLabels(string.Empty, "m/z", "Abundance", nameof(SpectrumPeak.Mass), nameof(SpectrumPeak.Intensity));
-            ObservableMsSpectrum upperObservableMsSpectrum = new ObservableMsSpectrum(Observable.Return(new MsSpectrum(msdecResult.Spectrum)), null, Observable.Return((ISpectraExporter)null)).AddTo(Disposables);
-            ObservableMsSpectrum lowerObservableMsSpectrum = new ObservableMsSpectrum(referenceSpectrum, new ReadOnlyReactivePropertySlim<bool>(Observable.Return(true)).AddTo(Disposables), Observable.Return((ISpectraExporter)null)).AddTo(Disposables);
-            PropertySelector<SpectrumPeak, double> horizontalPropertySelector = new PropertySelector<SpectrumPeak, double>(peak => peak.Mass);
-            PropertySelector<SpectrumPeak, double> verticalPropertySelector = new PropertySelector<SpectrumPeak, double>(peak => peak.Intensity);
-            ChartHueItem upperSpectrumHueItem = new ChartHueItem(nameof(SpectrumPeak.SpectrumComment), ChartBrushes.GetBrush(Brushes.Blue));
-            SingleSpectrumModel upperSpectrumModel = new SingleSpectrumModel(
-                upperObservableMsSpectrum,
-                upperObservableMsSpectrum.CreateAxisPropertySelectors(horizontalPropertySelector, "m/z", "m/z"),
-                upperObservableMsSpectrum.CreateAxisPropertySelectors2(verticalPropertySelector, "abundance"),
-                upperSpectrumHueItem,
-                msGraphLabels).AddTo(Disposables);
-            ChartHueItem lowerSpectrumHueItem = new ChartHueItem(nameof(SpectrumPeak.SpectrumComment), ChartBrushes.GetBrush(Brushes.Red));
-            SingleSpectrumModel lowerSpectrumModel = new SingleSpectrumModel(
-                lowerObservableMsSpectrum,
-                lowerObservableMsSpectrum.CreateAxisPropertySelectors(horizontalPropertySelector, "m/z", "m/z"),
-                lowerObservableMsSpectrum.CreateAxisPropertySelectors2(verticalPropertySelector, "abundance"),
-                lowerSpectrumHueItem,
-                msGraphLabels).AddTo(Disposables);
-            MsSpectrumModel = new MsSpectrumModel(upperSpectrumModel, lowerSpectrumModel, scorer)
-            {
-                GraphTitle = string.Empty,
-                HorizontalTitle = "m/z",
-                VerticalTitle = "Abundance",
-            }.AddTo(Disposables);
+                .Subscribe(_plotService.UpdateMatchingScorer).AddTo(Disposables);
         }
 
         public IReadOnlyList<CompoundSearcher> CompoundSearchers { get; }
@@ -117,7 +83,7 @@ namespace CompMs.App.Msdial.Model.Search
 
         public IPeakSpotModel PeakSpot => _peakSpot;
 
-        public MsSpectrumModel MsSpectrumModel { get; }
+        public MsSpectrumModel MsSpectrumModel => _plotService.MsSpectrumModel;
 
         public MoleculeMsReference SelectedReference { 
             get => _selectedReference;
