3b4c2d596aa32008ca5d1b57357994d29a328615
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-01-05 15:27:13 +0900

    Add ChromatogramsView

---
+++ src/MSDIAL5/MsdialGuiApp/View/Chart/ChromatogramsView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace CompMs.App.Msdial.View.Chart
+{
+    /// <summary>
+    /// Interaction logic for ChromatogramsView.xaml
+    /// </summary>
+    public partial class ChromatogramsView : UserControl
+    {
+        public ChromatogramsView() {
+            InitializeComponent();
+        }
+    }
+}
