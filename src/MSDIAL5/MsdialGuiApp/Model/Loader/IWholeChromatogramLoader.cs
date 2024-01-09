485a76bba5e5a67c43145c4309ad94635515d747
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-01-09 11:58:04 +0900

    Add IWholeChromatogramLoader

---
+++ src/MSDIAL5/MsdialGuiApp/Model/Loader/IWholeChromatogramLoader.cs
@@ -0,0 +1,13 @@
+﻿using CompMs.App.Msdial.Model.DataObj;
+using System.Collections.Generic;
+
+namespace CompMs.App.Msdial.Model.Loader
+{
+    public interface IWholeChromatogramLoader {
+        List<PeakItem> LoadChromatogram();
+    }
+
+    public interface IWholeChromatogramLoader<T> {
+        List<PeakItem> LoadChromatogram(T state);
+    }
+}
