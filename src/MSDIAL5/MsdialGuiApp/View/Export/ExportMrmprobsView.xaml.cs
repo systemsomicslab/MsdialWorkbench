1fee2064a0bfaf376cec509078399b512f6981a6
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-01-11 15:02:54 +0900

    Add ExportMrmprobsView

---
+++ src/MSDIAL5/MsdialGuiApp/View/Export/ExportMrmprobsView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace CompMs.App.Msdial.View.Export
+{
+    /// <summary>
+    /// Interaction logic for ExportMrmprobsView.xaml
+    /// </summary>
+    public partial class ExportMrmprobsView : UserControl
+    {
+        public ExportMrmprobsView() {
+            InitializeComponent();
+        }
+    }
+}
