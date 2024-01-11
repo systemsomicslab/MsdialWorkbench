c7aeaf821986d9e99c0b289c0f1ec3fc4561fb6f
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-01-11 11:42:44 +0900

    Update ExportMrmprobsUsecase

--- src/MSDIAL5/MsdialGuiApp/Model/Export/ExportMrmprobsModel.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/Export/ExportMrmprobsModel.cs
@@ -1,7 +1,6 @@
 ﻿using CompMs.App.Msdial.Model.DataObj;
-using CompMs.Common.DataObj.Result;
+using CompMs.App.Msdial.Model.Search;
 using CompMs.CommonMVVM;
-using CompMs.MsdialCore.Algorithm.Annotation;
 using CompMs.MsdialCore.Export;
 using CompMs.MsdialCore.Parameter;
 using Reactive.Bindings;
@@ -69,11 +68,10 @@ namespace CompMs.App.Msdial.Model.Export
         MrmprobsExportBaseParameter ExportParameter { get; }
     }
 
-    internal sealed class AlignmentSpotExportMrmprobsUsecase : IExportMrmprobsUsecase
+    internal sealed class AlignmentSpotExportMrmprobsUsecase : BindableBase, IExportMrmprobsUsecase
     {
         private readonly AlignmentSpotSource _spots;
         private readonly AlignmentFileBeanModel _alignmentFile;
-        private readonly IAnnotationQueryFactory<MsScanMatchResult> _queryFactory;
         private readonly EsiMrmprobsExporter _exporter;
         private readonly IReadOnlyReactiveProperty<AlignmentSpotPropertyModel> _target;
 
@@ -81,19 +79,28 @@ namespace CompMs.App.Msdial.Model.Export
             MrmprobsExportBaseParameter parameter,
             AlignmentSpotSource spots,
             AlignmentFileBeanModel alignmentFile,
-            IAnnotationQueryFactory<MsScanMatchResult> queryFactory,
+            CompoundSearcherCollection compoundSearchers,
             EsiMrmprobsExporter exporter,
             IReadOnlyReactiveProperty<AlignmentSpotPropertyModel> target)
         {
             ExportParameter = parameter;
             _spots = spots;
             _alignmentFile = alignmentFile;
-            _queryFactory = queryFactory;
+            CompoundSearchers = compoundSearchers;
+            SelectedCompoundSearcher = compoundSearchers.Items.FirstOrDefault();
             _exporter = exporter;
             _target = target;
         }
 
         public MrmprobsExportBaseParameter ExportParameter { get; }
+
+        public CompoundSearcherCollection CompoundSearchers { get; }
+
+        public CompoundSearcher SelectedCompoundSearcher {
+            get => _selectedCompoundSearcher;
+            set => SetProperty(ref _selectedCompoundSearcher, value);
+        }
+        private CompoundSearcher _selectedCompoundSearcher;
 
         public async Task BatchExportAsync(Stream stream, CancellationToken token) {
             var loader = _alignmentFile.CreateTemporaryMSDecLoader();
@@ -101,14 +108,13 @@ namespace CompMs.App.Msdial.Model.Export
             if (ExportParameter.MpIsFocusedSpotOutput) {
                 await Task.Run(() => _exporter.ExportExperimentalMsms(stream, spots, loader, ExportParameter), token).ConfigureAwait(false);
             }
-            else if (ExportParameter.MpIsExportOtherCandidates) {
-                var searchParameter = _queryFactory.PrepareParameter();
+            else if (ExportParameter.MpIsExportOtherCandidates && SelectedCompoundSearcher != null) {
+                var queryFactory = SelectedCompoundSearcher.QueryFactory;
+                var searchParameter = queryFactory.PrepareParameter();
                 searchParameter.TotalScoreCutoff = ExportParameter.MpIdentificationScoreCutOff;
-                await Task.Run(() => _exporter.ExportReferenceMsms(stream, spots, ExportParameter, _queryFactory, loader, searchParameter), token).ConfigureAwait(false);
+                await Task.Run(() => _exporter.ExportReferenceMsms(stream, spots, ExportParameter, queryFactory, loader, searchParameter), token).ConfigureAwait(false);
             }
             else {
-                var searchParameter = _queryFactory.PrepareParameter();
-                searchParameter.TotalScoreCutoff = ExportParameter.MpIdentificationScoreCutOff;
                 await Task.Run(() => _exporter.ExportReferenceMsms(stream, spots, ExportParameter), token).ConfigureAwait(false);
             }
         }
@@ -119,24 +125,22 @@ namespace CompMs.App.Msdial.Model.Export
                 var msdec = loader.LoadMSDecResult(_target.Value.innerModel.MSDecResultIdUsed);
                 await Task.Run(() => _exporter.ExportExperimentalMsms(stream, _target.Value.innerModel, msdec, ExportParameter), token).ConfigureAwait(false);
             }
-            else if (ExportParameter.MpIsExportOtherCandidates) {
-                var searchParameter = _queryFactory.PrepareParameter();
+            else if (ExportParameter.MpIsExportOtherCandidates && SelectedCompoundSearcher != null) {
+                var queryFactory = SelectedCompoundSearcher.QueryFactory;
+                var searchParameter = queryFactory.PrepareParameter();
                 searchParameter.TotalScoreCutoff = ExportParameter.MpIdentificationScoreCutOff;
-                await Task.Run(() => _exporter.ExportReferenceMsms(stream, _target.Value.innerModel, ExportParameter, _queryFactory, loader, searchParameter), token).ConfigureAwait(false);
+                await Task.Run(() => _exporter.ExportReferenceMsms(stream, _target.Value.innerModel, ExportParameter, queryFactory, loader, searchParameter), token).ConfigureAwait(false);
             }
             else {
-                var searchParameter = _queryFactory.PrepareParameter();
-                searchParameter.TotalScoreCutoff = ExportParameter.MpIdentificationScoreCutOff;
                 await Task.Run(() => _exporter.ExportReferenceMsms(stream, _target.Value.innerModel, ExportParameter), token).ConfigureAwait(false);
             }
         }
     }
 
-    internal sealed class ChromatogramPeakExportMrmprobsUsecase : IExportMrmprobsUsecase
+    internal sealed class ChromatogramPeakExportMrmprobsUsecase : BindableBase, IExportMrmprobsUsecase
     {
         private readonly IReadOnlyList<ChromatogramPeakFeatureModel> _peaks;
         private readonly AnalysisFileBeanModel _analysisFile;
-        private readonly IAnnotationQueryFactory<MsScanMatchResult> _queryFactory;
         private readonly EsiMrmprobsExporter _exporter;
         private readonly IReadOnlyReactiveProperty<ChromatogramPeakFeatureModel> _target;
 
@@ -144,33 +148,41 @@ namespace CompMs.App.Msdial.Model.Export
             MrmprobsExportBaseParameter parameter,
             IReadOnlyList<ChromatogramPeakFeatureModel> peaks,
             AnalysisFileBeanModel analysisFile,
-            IAnnotationQueryFactory<MsScanMatchResult> queryFactory,
+            CompoundSearcherCollection compoundSearchers,
             EsiMrmprobsExporter exporter,
             IReadOnlyReactiveProperty<ChromatogramPeakFeatureModel> target)
         {
             ExportParameter = parameter;
             _peaks = peaks;
             _analysisFile = analysisFile;
-            _queryFactory = queryFactory;
+            CompoundSearchers = compoundSearchers;
+            SelectedCompoundSearcher = compoundSearchers.Items.FirstOrDefault();
             _exporter = exporter;
             _target = target;
         }
 
         public MrmprobsExportBaseParameter ExportParameter { get; }
 
+        public CompoundSearcherCollection CompoundSearchers { get; }
+
+        public CompoundSearcher SelectedCompoundSearcher {
+            get => _selectedCompoundSearcher;
+            set => SetProperty(ref _selectedCompoundSearcher, value);
+        }
+        private CompoundSearcher _selectedCompoundSearcher;
+
         public async Task BatchExportAsync(Stream stream, CancellationToken token) {
             var spots = _peaks.Select(s => s.InnerModel).ToArray();
             if (ExportParameter.MpIsFocusedSpotOutput) {
                 await Task.Run(() => _exporter.ExportExperimentalMsms(stream, spots, _analysisFile.MSDecLoader, ExportParameter), token).ConfigureAwait(false);
             }
-            else if (ExportParameter.MpIsExportOtherCandidates) {
-                var searchParameter = _queryFactory.PrepareParameter();
+            else if (ExportParameter.MpIsExportOtherCandidates && SelectedCompoundSearcher != null) {
+                var queryFactory = SelectedCompoundSearcher.QueryFactory;
+                var searchParameter = queryFactory.PrepareParameter();
                 searchParameter.TotalScoreCutoff = ExportParameter.MpIdentificationScoreCutOff;
-                await Task.Run(() => _exporter.ExportReferenceMsms(stream, spots, ExportParameter, _queryFactory, _analysisFile.MSDecLoader, searchParameter), token).ConfigureAwait(false);
+                await Task.Run(() => _exporter.ExportReferenceMsms(stream, spots, ExportParameter, queryFactory, _analysisFile.MSDecLoader, searchParameter), token).ConfigureAwait(false);
             }
             else {
-                var searchParameter = _queryFactory.PrepareParameter();
-                searchParameter.TotalScoreCutoff = ExportParameter.MpIdentificationScoreCutOff;
                 await Task.Run(() => _exporter.ExportReferenceMsms(stream, spots, ExportParameter), token).ConfigureAwait(false);
             }
         }
@@ -180,14 +192,13 @@ namespace CompMs.App.Msdial.Model.Export
                 var msdec = _analysisFile.MSDecLoader.LoadMSDecResult(_target.Value.InnerModel.MSDecResultIdUsed);
                 await Task.Run(() => _exporter.ExportExperimentalMsms(stream, _target.Value.InnerModel, msdec, ExportParameter), token).ConfigureAwait(false);
             }
-            else if (ExportParameter.MpIsExportOtherCandidates) {
-                var searchParameter = _queryFactory.PrepareParameter();
+            else if (ExportParameter.MpIsExportOtherCandidates && SelectedCompoundSearcher != null) {
+                var queryFactory = SelectedCompoundSearcher.QueryFactory;
+                var searchParameter = queryFactory.PrepareParameter();
                 searchParameter.TotalScoreCutoff = ExportParameter.MpIdentificationScoreCutOff;
-                await Task.Run(() => _exporter.ExportReferenceMsms(stream, _target.Value.InnerModel, ExportParameter, _queryFactory, _analysisFile.MSDecLoader, searchParameter), token).ConfigureAwait(false);
+                await Task.Run(() => _exporter.ExportReferenceMsms(stream, _target.Value.InnerModel, ExportParameter, queryFactory, _analysisFile.MSDecLoader, searchParameter), token).ConfigureAwait(false);
             }
             else {
-                var searchParameter = _queryFactory.PrepareParameter();
-                searchParameter.TotalScoreCutoff = ExportParameter.MpIdentificationScoreCutOff;
                 await Task.Run(() => _exporter.ExportReferenceMsms(stream, _target.Value.InnerModel, ExportParameter), token).ConfigureAwait(false);
             }
         }
