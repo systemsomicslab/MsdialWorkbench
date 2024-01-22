e3f083ef587e3bcb9e7c874aab748e562eb72c1c
Bujinlkham BUYANTOGTOKH <s238586u@st.me.tuat.ac.jp>
2024-01-22 17:21:33 +0900

    Changed NotameViewModel (able to choose alignment result file and parameters)

--- src/MSDIAL5/MsdialGuiApp/ViewModel/Statistics/NotameViewModel.cs
+++ src/MSDIAL5/MsdialGuiApp/ViewModel/Statistics/NotameViewModel.cs
@@ -1,4 +1,6 @@
-﻿using CompMs.App.Msdial.Model.Statistics;
+﻿using CompMs.App.Msdial.Model.Export;
+using CompMs.App.Msdial.Model.Statistics;
+using CompMs.App.Msdial.ViewModel.Export;
 using CompMs.CommonMVVM;
 using CompMs.CommonMVVM.Validator;
 using CompMs.Graphics.UI;
@@ -7,38 +9,98 @@ using Reactive.Bindings.Extensions;
 using Reactive.Bindings.Notifiers;
 using System.ComponentModel.DataAnnotations;
 using System.Windows.Input;
+using System.Windows.Data;
+using System.Linq;
+using System.Reactive.Linq;
+using System.ComponentModel;
+using CompMs.App.Msdial.Model.DataObj;
+using System.Threading.Tasks;
+using System;
 
 namespace CompMs.App.Msdial.ViewModel.Statistics
 {
-    public sealed class NotameViewModel : SettingDialogViewModel
+    internal sealed class NotameViewModel : SettingDialogViewModel
     {
         private readonly Notame _notame;
         private readonly IMessageBroker _broker;
-
+        
         public NotameViewModel(Notame notame, IMessageBroker broker) {
             _notame = notame;
             _broker = broker;
+
+            AlignmentFiles = CollectionViewSource.GetDefaultView(notame.AlignmentFilesForExport.Files);
+            if (notame.AlignmentFilesForExport.SelectedFile != null)
+            {
+                AlignmentFiles.MoveCurrentTo(notame.AlignmentFilesForExport.SelectedFile);
+            }
+
+            Groups = notame.Groups.ToReadOnlyReactiveCollection(MapToViewModel).AddTo(Disposables);
+            if (Groups.Any())
+            {
+                Groups.First().IsExpanded = true;
+            }
+
+            ExportCommand = Groups.Select(g => (IObservable<bool>)g.CanExport).Append(
+                this.ErrorsChangedAsObservable().Select(_ => !HasValidationErrors).Prepend(!HasValidationErrors)
+            ).CombineLatestValuesAreAllTrue()
+            .ToAsyncReactiveCommand()
+            .WithSubscribe(ExportAlignmentResultAsync)
+            .AddTo(Disposables);
+
             RunNotameCommand = new DelegateCommand(RunNotame);
             ShowSettingViewCommand = new DelegateCommand(ShowSettingView);
-            Path = notame.Path;
+
+            AlignmentFile = notame.AlignmentFilesForExport.SelectedFile;
+            ExportDirectory = notame.ExportDirectory;
         }
         
         [Required(ErrorMessage = "Please browse a folder for result export.")]
         [PathExists(ErrorMessage = "This folder does not exist.", IsDirectory = true)]
-        public string Path { 
-            get => _path; 
+        public string ExportDirectory { 
+            get => _exportDirectory; 
             set
             {
-                if (SetProperty(ref _path, value))
+                if (SetProperty(ref _exportDirectory, value))
                 {
-                    if (!ContainsError(nameof(Path)))
+                    if (!ContainsError(nameof(_exportDirectory)))
                     {
-                        _notame.Path = _path;
+                        _notame.ExportDirectory = _exportDirectory;
                     }
                 }
             }
         }
-        private string _path;
+        private string _exportDirectory;
+
+        [Required(ErrorMessage = "Please select alignment file.")]
+        public AlignmentFileBeanModel AlignmentFile
+        {
+            get => _alignmentFile;
+            set
+            {
+                if (SetProperty(ref _alignmentFile, value))
+                {
+                    if (!ContainsError(nameof(AlignmentFile)))
+                    {
+                        _notame.AlignmentFilesForExport.SelectedFile = _alignmentFile;
+                    }
+                }
+            }
+        }
+        private AlignmentFileBeanModel _alignmentFile;
+
+        public ICollectionView AlignmentFiles { get; }
+
+        public ReadOnlyReactiveCollection<IAlignmentResultExportViewModel> Groups { get; }
+
+        public bool UseFilter
+        {
+            get => _notame.PeakSpotSupplyer.UseFilter;
+            set
+            {
+                _notame.PeakSpotSupplyer.UseFilter = value;
+                OnPropertyChanged(nameof(UseFilter));
+            }
+        }
 
         public DelegateCommand BrowseDirectoryCommand => _browseDirectoryCommand ?? (_browseDirectoryCommand = new DelegateCommand(BrowseDirectory));
         private DelegateCommand _browseDirectoryCommand;
@@ -52,7 +114,33 @@ namespace CompMs.App.Msdial.ViewModel.Statistics
 
             if (win.ShowDialog() == Graphics.Window.DialogResult.OK)
             {
-                Path = win.SelectedPath;
+                ExportDirectory = win.SelectedPath;
+            }
+        }
+
+        public AsyncReactiveCommand ExportCommand { get; }
+
+        private Task ExportAlignmentResultAsync()
+        {
+            return _notame.ExportAlignmentResultAsync(_broker);
+        }
+
+        private static IAlignmentResultExportViewModel MapToViewModel(IAlignmentResultExportModel model)
+        {
+            switch (model)
+            {
+                case AlignmentExportGroupModel m:
+                    return new AlignmentExportGroupViewModel(m);
+                case ProteinGroupExportModel m:
+                    return new ProteinGroupExportViewModel(m);
+                case AlignmentSpectraExportGroupModel m:
+                    return new AlignmentSpectraExportGroupViewModel(m);
+                case AlignmentMatchedSpectraExportModel m:
+                    return new AlignmentMatchedSpectraExportViewModel(m);
+                case AlignmentResultMassBankRecordExportModel m:
+                    return new AlignmentResultMassBankRecordExportViewModel(m);
+                default:
+                    throw new NotSupportedException(model.GetType().FullName);
             }
         }
 
@@ -68,7 +156,7 @@ namespace CompMs.App.Msdial.ViewModel.Statistics
             _broker.Publish(this);
         }
 
-        public override ICommand ApplyCommand => null;
+        public override ICommand ApplyCommand => ExportCommand;
         public override ICommand FinishCommand => RunNotameCommand;
     }
 }
