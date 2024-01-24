708a2518f114d21d80c10a17aa9e19e8deb6c57c
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-01-24 11:05:20 +0900

    Change to sync clicked spot in MsPeakSpotsCheckView with  spectrum in RawMsSpectrumCheckView

--- src/RawDataApp/RawDataViewer/ViewModel/MsPeakSpotsCheckViewModel.cs
+++ src/RawDataApp/RawDataViewer/ViewModel/MsPeakSpotsCheckViewModel.cs
@@ -4,10 +4,11 @@ using CompMs.CommonMVVM;
 using CompMs.MsdialCore.DataObj;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
+using System.Reactive.Linq;
 
 namespace CompMs.App.RawDataViewer.ViewModel
 {
-    public class MsPeakSpotsCheckViewModel : ViewModelBase
+    public sealed class MsPeakSpotsCheckViewModel : ViewModelBase
     {
         public MsPeakSpotsCheckViewModel(MsPeakSpotsCheckModel model) {
             AnalysisFile = model.ObserveProperty(m => m.AnalysisFile)
@@ -22,11 +23,15 @@ namespace CompMs.App.RawDataViewer.ViewModel
             Distribution = model.ObserveProperty(m => m.Distribution)
                 .ToReadOnlyReactivePropertySlim()
                 .AddTo(Disposables);
+            SelectedPeak = new ReactivePropertySlim<ChromatogramPeakFeature>().AddTo(Disposables);
+            SelectedPeakSpectrumID = SelectedPeak.Select(p => p?.MS1RawSpectrumIdTop ?? 0).ToReadOnlyReactivePropertySlim().AddTo(Disposables);
         }
 
         public ReadOnlyReactivePropertySlim<AnalysisFileBean> AnalysisFile { get; }
         public ReadOnlyReactivePropertySlim<MachineCategory> MachineCategory { get; }
         public ReadOnlyReactivePropertySlim<MsPeakSpotsSummary> Summary { get; }
         public ReadOnlyReactivePropertySlim<MsSnDistribution> Distribution { get; }
+        public ReactivePropertySlim<ChromatogramPeakFeature> SelectedPeak { get; }
+        public ReadOnlyReactivePropertySlim<int> SelectedPeakSpectrumID { get; }
     }
 }
