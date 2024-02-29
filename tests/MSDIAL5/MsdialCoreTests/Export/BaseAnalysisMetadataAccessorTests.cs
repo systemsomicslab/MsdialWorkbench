eacb9456d62da4b59beb48f74c92bbae671d2b0c
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-02-29 18:09:29 +0900

    Update tests

--- tests/MSDIAL5/MsdialCoreTests/Export/BaseAnalysisMetadataAccessorTests.cs
+++ tests/MSDIAL5/MsdialCoreTests/Export/BaseAnalysisMetadataAccessorTests.cs
@@ -26,6 +26,7 @@ namespace CompMs.MsdialCore.Export.Tests
             "Scan",
             "Height",
             "Area",
+            "Model masses",
             "Adduct",
             "Isotope",
             "Comment",
@@ -121,7 +122,7 @@ namespace CompMs.MsdialCore.Export.Tests
             Assert.AreEqual("0.90", content["Matched peaks percentage"]);
             Assert.AreEqual("0.64", content["Total score"]);
             Assert.AreEqual("6.78", content["S/N"]);
-            Assert.AreEqual("700.00000 1000;701.00467 100", content["MS1 isotopes"]);
+            Assert.AreEqual("700.00000 1000;701.00467 100;702.00934 0", content["MS1 isotopes"]);
             Assert.AreEqual("700.00001 1000;500.00002 200;200.00003 50", content["MSMS spectrum"]);
         }
 
