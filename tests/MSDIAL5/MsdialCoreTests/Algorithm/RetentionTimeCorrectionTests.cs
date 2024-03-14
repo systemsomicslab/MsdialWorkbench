52295b5db1cceab9b9cea03773b5298df1167dac
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-03-14 15:51:56 +0900

    Update test

--- tests/MSDIAL5/MsdialCoreTests/Algorithm/RetentionTimeCorrectionTests.cs
+++ tests/MSDIAL5/MsdialCoreTests/Algorithm/RetentionTimeCorrectionTests.cs
@@ -19,11 +19,11 @@ public class RetentionTimeCorrectionTests
         var references = new List<MoleculeMsReference> { new(), new(), new(), };
         var stds = new List<StandardPair>
             {
-                new() { SamplePeakAreaBean = new() { ChromXs = new ChromXs(new RetentionTime(2d)) }, Reference = references[0] },
-                new() { SamplePeakAreaBean = new() { ChromXs = new ChromXs(new RetentionTime(1d)) }, Reference = references[1] },
-                new() { SamplePeakAreaBean = new() { ChromXs = new ChromXs(new RetentionTime(3d)) }, Reference = references[2] },
+                new() { SamplePeakAreaBean = new() { ChromXs = new ChromXs(new RetentionTime(3d)) }, Reference = references[0] },
+                new() { SamplePeakAreaBean = new() { ChromXs = new ChromXs(new RetentionTime(2d)) }, Reference = references[1] },
+                new() { SamplePeakAreaBean = new() { ChromXs = new ChromXs(new RetentionTime(1d)) }, Reference = references[2] },
             };
-        var commons = new List<CommonStdData> { new(references[1]) { AverageRetentionTime = 1.5f }, };
+        var commons = new List<CommonStdData> { new(references[2]) { AverageRetentionTime = .8f }, new(references[1]) { AverageRetentionTime = 1.5f }, };
         var parameter = new RetentionTimeCorrectionParam { InterpolationMethod = (InterpolationMethod)(-1) /*fake interpolation method */ };
 
         _ = RetentionTimeCorrection.GetRetentionTimeCorrectionBean_SampleMinusAverage(parameter, stds, [], commons);
