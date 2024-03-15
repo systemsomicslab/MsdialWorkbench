77822239e4bc5f8c21844403a124be50be4fd2b7
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-03-15 15:05:03 +0900

    Update ResizableItemsControl

--- src/Common/ChartDrawing/UI/ResizableItemsControl.cs
+++ src/Common/ChartDrawing/UI/ResizableItemsControl.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CompMs.Graphics.UI
 {
@@ -64,17 +56,69 @@ namespace CompMs.Graphics.UI
             set => SetValue(OrientationProperty, value);
         }
 
+        protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue) {
+            if (oldValue is INotifyCollectionChanged oldCollection) {
+                oldCollection.CollectionChanged -= OnItemsSourceCollectionChanged;
+            }
+
+            base.OnItemsSourceChanged(oldValue, newValue);
+
+            if (newValue is INotifyCollectionChanged newCollection) {
+                newCollection.CollectionChanged += OnItemsSourceCollectionChanged;
+            }
+
+            SetCurrentItems();
+        }
+
+        private void OnItemsSourceCollectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
+            switch (e.Action) {
+                case NotifyCollectionChangedAction.Add:
+                    for (int i = 0; i < e.NewItems.Count; i++) {
+                        InsertItem(e.NewItems[i], (e.NewStartingIndex + i) * 2);
+                    }
+                    ResetIndex(e.NewStartingIndex * 2);
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    foreach (var item in e.OldItems) {
+                        RemoveItemAt(e.OldStartingIndex * 2);
+                    }
+                    ResetIndex(Math.Max(e.OldStartingIndex * 2 - 1, 0));
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    foreach (var item in e.OldItems) {
+                        RemoveItemAt(e.OldStartingIndex * 2);
+                    }
+                    for (int i = 0; i < e.NewItems.Count; i++) {
+                        InsertItem(e.NewItems[i], (e.NewStartingIndex + i) * 2);
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    ResetIndex(e.NewStartingIndex * 2);
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    _grid?.Children.Clear();
+                    SetCurrentItems();
+                    break;
+            }
+        }
+
         public override void OnApplyTemplate() {
             base.OnApplyTemplate();
 
             _grid = GetTemplateChild("PART_Grid") as Grid;
 
-            foreach (var item in Items) {
-                AddItem(item);
+            SetCurrentItems();
+        }
+
+        private void SetCurrentItems() {
+            if (_grid is not null) {
+                foreach (var item in Items) {
+                    InsertItem(item, _grid.Children.Count);
+                }
             }
         }
 
-        private void AddItem(object item) {
+        private void InsertItem(object item, int index) {
             var visualItem = ItemTemplate?.LoadContent() as FrameworkElement;
             if (visualItem is not null) {
                 visualItem.DataContext = item;
@@ -94,14 +138,16 @@ namespace CompMs.Graphics.UI
                         return;
                 }
             }
-            
-            if (visualItem is not null) {
 
-                if (_grid is not null) {
-                    switch (Orientation) {
-                        case Orientation.Horizontal:
-                            if (_grid.ColumnDefinitions.Count >= 1) {
-                                _grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto, });
+            if (visualItem is not null && _grid is not null) {
+                switch (Orientation) {
+                    case Orientation.Horizontal:
+                        if (index % 2 == 0) {
+                            _grid.ColumnDefinitions.Insert(index, new ColumnDefinition { Width = new GridLength(1d, GridUnitType.Star), });
+                            Grid.SetColumn(visualItem, index);
+                            _grid.Children.Insert(index, visualItem);
+                            if (_grid.ColumnDefinitions.Count >= 2) {
+                                _grid.ColumnDefinitions.Insert(index + 1, new ColumnDefinition { Width = GridLength.Auto, });
                                 var splitter = new GridSplitter
                                 {
                                     ResizeDirection = GridResizeDirection.Columns,
@@ -110,16 +156,34 @@ namespace CompMs.Graphics.UI
                                     VerticalAlignment = VerticalAlignment.Stretch,
                                     Background = Brushes.LightGray,
                                 };
-                                Grid.SetColumn(splitter, _grid.ColumnDefinitions.Count - 1);
-                                _grid.Children.Add(splitter);
+                                Grid.SetColumn(splitter, index + 1);
+                                _grid.Children.Insert(index + 1, splitter);
                             }
-                            _grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1d, GridUnitType.Star), });
-                            Grid.SetColumn(visualItem, _grid.ColumnDefinitions.Count - 1);
-                            _grid.Children.Add(visualItem);
-                            break;
-                        case Orientation.Vertical:
-                            if (_grid.RowDefinitions.Count >= 1) {
-                                _grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto, });
+                        }
+                        else {
+                            _grid.ColumnDefinitions.Insert(index, new ColumnDefinition { Width = GridLength.Auto, });
+                            var splitter = new GridSplitter
+                            {
+                                ResizeDirection = GridResizeDirection.Columns,
+                                Width = 3d,
+                                HorizontalAlignment = HorizontalAlignment.Stretch,
+                                VerticalAlignment = VerticalAlignment.Stretch,
+                                Background = Brushes.LightGray,
+                            };
+                            Grid.SetColumn(splitter, index);
+                            _grid.Children.Insert(index, splitter);
+                            _grid.ColumnDefinitions.Insert(index + 1, new ColumnDefinition { Width = new GridLength(1d, GridUnitType.Star), });
+                            Grid.SetColumn(visualItem, index + 1);
+                            _grid.Children.Insert(index + 1, visualItem);
+                        }
+                        break;
+                    case Orientation.Vertical:
+                        if (index % 2 == 0) {
+                            _grid.RowDefinitions.Insert(index, new RowDefinition { Height = new GridLength(1d, GridUnitType.Star), });
+                            Grid.SetRow(visualItem, index);
+                            _grid.Children.Insert(index, visualItem);
+                            if (_grid.RowDefinitions.Count >= 2) {
+                                _grid.RowDefinitions.Insert(index + 1, new RowDefinition { Height = GridLength.Auto, });
                                 var splitter = new GridSplitter
                                 {
                                     ResizeDirection = GridResizeDirection.Rows,
@@ -128,14 +192,85 @@ namespace CompMs.Graphics.UI
                                     VerticalAlignment = VerticalAlignment.Stretch,
                                     Background = Brushes.LightGray,
                                 };
-                                Grid.SetRow(splitter, _grid.RowDefinitions.Count - 1);
-                                _grid.Children.Add(splitter);
+                                Grid.SetRow(splitter, index + 1);
+                                _grid.Children.Insert(index + 1, splitter);
                             }
-                            _grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1d, GridUnitType.Star), });
-                            Grid.SetRow(visualItem, _grid.RowDefinitions.Count - 1);
-                            _grid.Children.Add(visualItem);
-                            break;
+                        }
+                        else {
+                            _grid.RowDefinitions.Insert(index, new RowDefinition { Height = GridLength.Auto, });
+                            var splitter = new GridSplitter
+                            {
+                                ResizeDirection = GridResizeDirection.Rows,
+                                Height = 3d,
+                                HorizontalAlignment = HorizontalAlignment.Stretch,
+                                VerticalAlignment = VerticalAlignment.Stretch,
+                                Background = Brushes.LightGray,
+                            };
+                            Grid.SetRow(splitter, index);
+                            _grid.Children.Insert(index, splitter);
+                            _grid.RowDefinitions.Insert(index + 1, new RowDefinition { Height = new GridLength(1d, GridUnitType.Star), });
+                            Grid.SetRow(visualItem, index + 1);
+                            _grid.Children.Insert(index + 1, visualItem);
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void RemoveItemAt(int index) {
+            if ((uint)index < _grid.Children.Count && _grid is not null) {
+                if (Orientation == Orientation.Horizontal) {
+                    RemoveColumnDefinition(index);
+                }
+                else {
+                    RemoveRowDefinition(index);
+                }
+            }
+        }
+
+        private void RemoveColumnDefinition(int index) {
+            if (_grid.ColumnDefinitions.Count >= 1) {
+                _grid.ColumnDefinitions.RemoveAt(index);
+                _grid.Children.RemoveAt(index);
+                if (_grid.ColumnDefinitions.Count >= 1) {
+                    if (index > 0) {
+                        _grid.ColumnDefinitions.RemoveAt(index - 1);
+                        _grid.Children.RemoveAt(index - 1);
                     }
+                    else {
+                        _grid.ColumnDefinitions.RemoveAt(index);
+                        _grid.Children.RemoveAt(index);
+                    }
+                }
+            }
+        }
+
+        private void RemoveRowDefinition(int index) {
+            if (_grid.RowDefinitions.Count >= 1) {
+                _grid.RowDefinitions.RemoveAt(index);
+                _grid.Children.RemoveAt(index);
+                if (_grid.RowDefinitions.Count >= 1) {
+                    if (index > 0) {
+                        _grid.RowDefinitions.RemoveAt(index - 1);
+                        _grid.Children.RemoveAt(index - 1);
+                    }
+                    else {
+                        _grid.RowDefinitions.RemoveAt(index);
+                        _grid.Children.RemoveAt(index);
+                    }
+                }
+            }
+        }
+
+        private void ResetIndex(int index) {
+            for (int i = index; i < _grid.Children.Count; i++) {
+                switch (Orientation) {
+                    case Orientation.Horizontal:
+                        Grid.SetColumn(_grid.Children[i], i);
+                        break;
+                    case Orientation.Vertical:
+                        Grid.SetRow(_grid.Children[i], i);
+                        break;
                 }
             }
         }
--- tests/Common/ChartDrawingUiTest/UI/ResizableItemsControl.xaml.cs
+++ tests/Common/ChartDrawingUiTest/UI/ResizableItemsControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Controls;
 
 namespace ChartDrawingUiTest.UI
 {
@@ -7,10 +9,24 @@ namespace ChartDrawingUiTest.UI
     /// </summary>
     public partial class ResizableItemsControl : Page
     {
+        private ObservableCollection<int> _items;
+
         public ResizableItemsControl() {
             InitializeComponent();
 
-            DataContext = new[] {1, 2, 3, 4, 5 };
+            DataContext = _items = [1, 2, 3, 4, 5];
+        }
+
+        private void Add_Click(object sender, System.Windows.RoutedEventArgs e) {
+            var button = (Button)sender;
+            var index = (int)button.Tag;
+            _items.Insert(index, _items.Max() + 1);
+        }
+
+        private void Remove_Click(object sender, System.Windows.RoutedEventArgs e) {
+            var button = (Button)sender;
+            var index = (int)button.Tag;
+            _items.RemoveAt(index);
         }
     }
 }
