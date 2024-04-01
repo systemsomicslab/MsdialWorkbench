fa04f4f39c66639af40d62e87e4e74693df9297e
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-04-01 14:22:45 +0900

    Add ExpressionHelper.GetPropertyGetterExpressions method

--- src/Common/ChartDrawing/Helper/ExpressionHelper.cs
+++ src/Common/ChartDrawing/Helper/ExpressionHelper.cs
@@ -3,8 +3,8 @@ using System;
 using System.Linq.Expressions;
 using System.Reflection;
 
-namespace CompMs.Graphics.Helper
-{
+namespace CompMs.Graphics.Helper;
+
     internal static class ExpressionHelper
     {
         public static Expression<Func<object, IAxisManager, AxisValue>> GetConvertToAxisValueExpression(Type type, string property) {
@@ -47,6 +47,34 @@ namespace CompMs.Graphics.Helper
             var prop = Expression.Convert(curValue, typeof(object));
             return Expression.Lambda<Func<object, object>>(prop, parameter);
         }
+
+    /// <summary>
+    /// Generates an array of <see cref="LambdaExpression"/> objects representing getter expressions for the specified property or nested properties of a given type.
+    /// </summary>
+    /// <remarks>
+    /// This method allows for accessing nested properties by specifying a dot-separated string of property names (e.g., "Property.SubProperty").
+    /// Each <see cref="LambdaExpression"/> in the resulting array corresponds to a getter for the respective property in the chain, starting from the root type.
+    /// </remarks>
+    /// <param name="type">The root type from which the property getters should be created. This type must not be null.</param>
+    /// <param name="property">A dot-separated string representing the property or nested properties to access. Each property in the chain must exist and be accessible from the previous one.</param>
+    /// <returns>An array of <see cref="LambdaExpression"/> objects, each representing a getter for the specified property or one of the nested properties. The length of the array matches the number of properties specified.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="type"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="property"/> is null, empty, or if any property in the chain does not exist on the expected type.</exception>
+    public static LambdaExpression[] GetPropertyGetterExpressions(Type type, string property) {
+        if (type is null) {
+            throw new ArgumentNullException(nameof(type));
+        }
+
+        var properties = property.Split('.');
+        var result = new LambdaExpression[properties.Length];
+        var parameter = Expression.Parameter(type);
+        Expression curValue = parameter;
+        for (int i = 0; i < properties.Length; i++) {
+            curValue = Expression.Property(curValue, properties[i]);
+            result[i] = Expression.Lambda(curValue, parameter);
+        }
+        return result;
+    }
 
         public static bool ValidatePropertyString(Type type, string property) {
             if (string.IsNullOrEmpty(property)) {
---
+++ tests/Common/ChartDrawingTests/Helper/ExpressionHelperTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace CompMs.Graphics.Helper.Tests;
+
+[TestClass()]
+public class ExpressionHelperTests
+{
+    [TestMethod()]
+    public void GetPropertyGetterExpressionsTest() {
+        A a = new(1);
+        B b = new(a);
+        C c = new(b);
+
+        var expressions = ExpressionHelper.GetPropertyGetterExpressions(typeof(C), "B.A.X");
+        var getters = expressions.Select(exp => exp.Compile()).ToArray();
+
+        Assert.AreEqual(3, expressions.Length);
+        Assert.AreSame(b, ((Func<C, B>)getters[0]).Invoke(c));
+        Assert.AreSame(a, ((Func<C, A>)getters[1]).Invoke(c));
+        Assert.AreEqual(1, ((Func<C, int>)getters[2]).Invoke(c));
+
+        Assert.AreSame(b, getters[0].DynamicInvoke(c));
+        Assert.AreSame(a, getters[1].DynamicInvoke(c));
+        Assert.AreEqual(1, getters[2].DynamicInvoke(c));
+    }
+
+    [TestMethod()]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void GetPropertyGetterExpressions_NullType_ThrowsException() {
+        _ = ExpressionHelper.GetPropertyGetterExpressions(null, "");
+    }
+
+    [TestMethod()]
+    [ExpectedException(typeof(ArgumentException))]
+    public void GetPropertyGetterExpressions_EmptyProperty_ThrowsException() {
+        _ = ExpressionHelper.GetPropertyGetterExpressions(typeof(C), "");
+    }
+
+    [TestMethod()]
+    [ExpectedException(typeof(ArgumentException))]
+    public void GetPropertyGetterExpressions_InvalidProperty_ThrowsException() {
+        _ = ExpressionHelper.GetPropertyGetterExpressions(typeof(C), "NonExistentProperty");
+    }
+
+    [TestMethod()]
+    public void GetPropertyGetterExpressions_SingleProperty_Success() {
+        C c = new C(new B(new A(1)));
+        var expressions = ExpressionHelper.GetPropertyGetterExpressions(typeof(C), "B");
+        var getter = (Func<C, B>)expressions.Single().Compile();
+
+        Assert.AreEqual(c.B, getter(c));
+    }
+
+    [TestMethod()]
+    [ExpectedException(typeof(InvalidCastException))]
+    public void GetPropertyGetterExpressions_TypeMismatch_ThrowsException() {
+        C c = new C(new B(new A(1)));
+        var expressions = ExpressionHelper.GetPropertyGetterExpressions(typeof(C), "B.A.X");
+        var getter = (Func<C, string>)expressions.Last().Compile();
+
+        getter(c);
+    }
+
+    class A(int X)
+    {
+        public int X { get; } = X;
+    }
+
+    class B(A A) {
+        public A A { get; } = A;
+    }
+
+    class C(B B) {
+        public B B { get; } = B;
+    }
+}
