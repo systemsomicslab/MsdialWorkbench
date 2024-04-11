af44fc08a89fcda1b309f79f122ded328f68588b
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-04-11 15:10:14 +0900

    Add InsertAlignmentMetadataAccessorDecorator

---
+++ src/MSDIAL5/MsdialCore/Export/AlignmentMetadataAccessorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using CompMs.Common.Interfaces;
+using CompMs.MsdialCore.DataObj;
+using CompMs.MsdialCore.Export.Internal;
+using System;
+
+namespace CompMs.MsdialCore.Export;
+
+public static class AlignmentMetadataAccessorExtensions {
+    public static IMetadataAccessor Insert(this IMetadataAccessor accessor, string additionalColumn, int insertPosition, Func<AlignmentSpotProperty, IMSScanProperty, string> getter) {
+        return new InsertAlignmentMetadataAccessorDecorator(accessor, additionalColumn, insertPosition, getter);
+    }
+}
