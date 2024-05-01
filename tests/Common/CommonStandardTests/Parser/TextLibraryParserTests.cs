9c5ccb7ee13c1747e7d660911bddbe52a84f635e
YukiMatsuzawa <122433968+YukiMatsuzawa@users.noreply.github.com>
2024-05-01 16:01:04 +0900

    Fixed Crash When Reading Text Library with Invalid Formula (#353)
    * Add Formula.IsCorrectlyImported test
    
    * Set Formula.IsCorrectlyImported property when formula string is valid
    
    * Add test for TextLibraryParser when formula is invalid
    
    * Check formula is valid before calculating isotopes

--- tests/Common/CommonStandardTests/Parser/TextLibraryParserTests.cs
+++ tests/Common/CommonStandardTests/Parser/TextLibraryParserTests.cs
@@ -423,6 +423,16 @@ Cer 33:1;2O(d7)|Cer 18:1;2O/15:0(d7)	531.5476588	9.34	[M+H]+	HBULQAPKKLNTLT-BXLQ
             var results = TextLibraryParser.TextLibraryReader(sr, out _);
             Assert.AreEqual("PC 36:2|PC 18:0_18:2(9,12)", results[0].Name);
         }
+
+
+        [TestMethod]
+        [DeploymentItem(@"Resources\Parser\txt_invalid_formula.txt", @"Resources\Parser\")]
+        [DataRow(@"Resources\Parser\txt_invalid_formula.txt")]
+        public void TextLibraryReaderWorks(string library) {
+            var results = TextLibraryParser.TextLibraryReader(library, out var error);
+            Debug.WriteLine($"Error message: {error}");
+            Debug.WriteLine($"Count: {results.Count}");
+        }
     }
 
 }
