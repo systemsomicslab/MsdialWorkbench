70b5bbf7b09fdf3ee6a5d61e6ed48d7845167e74
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-05-09 10:41:53 +0900

    Add AlignedChromatograms class

---
+++ src/MSDIAL5/MsdialGuiApp/Model/DataObj/AlignedChromatograms.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompMs.App.Msdial.Model.DataObj
+{
+    internal sealed class AlignedChromatograms
+    {
+        public AlignedChromatograms(AlignmentSpotPropertyModel spot, IObservable<List<PeakChromatogram>> chromatograms) {
+            Spot = spot;
+            Chromatograms = chromatograms;
+        }
+
+        public AlignmentSpotPropertyModel Spot { get; }
+        public IObservable<List<PeakChromatogram>> Chromatograms { get; }
+    }
+}
