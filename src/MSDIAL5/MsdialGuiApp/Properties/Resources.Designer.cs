50fd35c8d99074539ec69a3efb356300fa0a30f4
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-05-30 19:25:19 +0900

    Update Msdial version to 5.3-beta

--- src/MSDIAL5/MsdialGuiApp/Properties/Resources.Designer.cs
+++ src/MSDIAL5/MsdialGuiApp/Properties/Resources.Designer.cs
@@ -88,7 +88,7 @@ namespace CompMs.App.Msdial.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 5.3.240328-alpha.5.
+        ///   Looks up a localized string similar to 5.3.240530-beta.
         /// </summary>
         public static string VERSION {
             get {
