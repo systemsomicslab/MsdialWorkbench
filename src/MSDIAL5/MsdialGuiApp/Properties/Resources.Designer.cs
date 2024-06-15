5c743f46e689b0c42ea19c2b88fe557041bab070
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-06-15 23:49:12 +0900

    Update version

--- src/MSDIAL5/MsdialGuiApp/Properties/Resources.Designer.cs
+++ src/MSDIAL5/MsdialGuiApp/Properties/Resources.Designer.cs
@@ -88,7 +88,7 @@ namespace CompMs.App.Msdial.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 5.3.240612-beta.2.
+        ///   Looks up a localized string similar to 5.3.240617.
         /// </summary>
         public static string VERSION {
             get {
