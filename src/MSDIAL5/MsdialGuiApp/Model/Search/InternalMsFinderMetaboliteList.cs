50dcfe1486d27de3a1bcdc6a31ae1e8a182fe598
Bujee <s238586u@st.go.tuat.ac.jp>
2024-07-01 11:22:40 +0900

    Added find formula

--- src/MSDIAL5/MsdialGuiApp/Model/Search/InternalMsFinderMetaboliteList.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/Search/InternalMsFinderMetaboliteList.cs
@@ -1,33 +1,27 @@
 ﻿using CompMs.App.Msdial.Model.Chart;
 using CompMs.App.Msdial.Model.DataObj;
-using CompMs.App.Msdial.Model.Loader;
 using CompMs.App.Msdial.Model.Setting;
-using CompMs.App.Msdial.Utility;
-using CompMs.Common.Components;
 using CompMs.Common.DataObj;
+using CompMs.Common.DataObj.Ion;
 using CompMs.Common.DataObj.Property;
 using CompMs.Common.Enum;
+using CompMs.Common.FormulaGenerator;
+using CompMs.Common.FormulaGenerator.DataObj;
 using CompMs.Common.FormulaGenerator.Parser;
 using CompMs.Common.Parameter;
+using CompMs.Common.StructureFinder.DataObj;
+using CompMs.Common.Utility;
 using CompMs.CommonMVVM;
-using CompMs.Graphics.AxisManager;
-using CompMs.Graphics.AxisManager.Generic;
-using CompMs.Graphics.Core.Base;
 using CompMs.MsdialCore.DataObj;
 using CompMs.MsdialCore.Export;
-using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Reactive.Subjects;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Documents;
 
 namespace CompMs.App.Msdial.Model.Search {
     internal class InternalMsFinderMetaboliteList : DisposableModelBase {
@@ -73,24 +67,30 @@ namespace CompMs.App.Msdial.Model.Search {
         private readonly MsfinderQueryFile _queryFile;
         private readonly AnalysisParamOfMsfinder _parameter;
         private RawData? _spotData;
+        public List<FormulaResult> _formulaData;
+
+
+        private static readonly List<ProductIon> productIonDB = FragmentDbParser.GetProductIonDB(
+                @"Resources\msfinderLibrary\ProductIonLib_vs1.pid", out string _);
+        private static readonly List<NeutralLoss> neutralLossDB = FragmentDbParser.GetNeutralLossDB(
+            @"Resources\msfinderLibrary\NeutralLossDB_vs2.ndb", out string _);
+        private static readonly List<ExistFormulaQuery> existFormulaDB = ExistFormulaDbParcer.ReadExistFormulaDB(
+            @"Resources\msfinderLibrary\MsfinderFormulaDB-VS13.efd", out string _);
+        private static readonly List<ExistStructureQuery> existStructureDB = FileStorageUtility.GetExistStructureDB();
 
         public string metaboliteName {
             get => _spotData.Name;
-            set
-            {
-                if (_spotData.Name != value)
-                {
+            set {
+                if (_spotData.Name != value) {
                     _spotData.Name = value;
                     OnPropertyChanged(nameof(metaboliteName));
                 }
             }
         }
-        public int alignmentID { 
+        public int alignmentID {
             get => _spotData.ScanNumber;
-            set
-            {
-                if (_spotData?.ScanNumber != value)
-                {
+            set {
+                if (_spotData?.ScanNumber != value) {
                     _spotData.ScanNumber = value;
                     OnPropertyChanged(nameof(alignmentID));
                 }
@@ -98,153 +98,120 @@ namespace CompMs.App.Msdial.Model.Search {
         }
         public double retentionTime {
             get => _spotData.RetentionTime;
-            set
-            {
-                if (_spotData.RetentionTime != value)
-                {
+            set {
+                if (_spotData.RetentionTime != value) {
                     _spotData.RetentionTime = value;
                     OnPropertyChanged(nameof(retentionTime));
                 }
             }
         }
-        public double centralCcs {  
+        public double centralCcs {
             get => _spotData.Ccs;
-            set 
-            {
-                if (_spotData.Ccs != value)
-                {
+            set {
+                if (_spotData.Ccs != value) {
                     _spotData.Ccs = value;
                     OnPropertyChanged(nameof(centralCcs));
                 }
-            } 
+            }
         }
-        public double mass {  
+        public double mass {
             get => _spotData.PrecursorMz;
-            set 
-            {
-                if (_spotData.PrecursorMz != value)
-                {
+            set {
+                if (_spotData.PrecursorMz != value) {
                     _spotData.PrecursorMz = value;
                     OnPropertyChanged(nameof(mass));
                 }
             }
         }
-        public string adduct { 
+        public string adduct {
             get => _spotData.PrecursorType;
-            set 
-            {
-                if (_spotData.PrecursorType != value)
-                {
+            set {
+                if (_spotData.PrecursorType != value) {
                     _spotData.PrecursorType = value;
                     OnPropertyChanged(nameof(adduct));
                 }
-            } 
+            }
         }
-        public string formula {  
+        public string formula {
             get => _spotData.Formula;
-            set 
-            {
-                if (_spotData?.Formula != value)
-                {
+            set {
+                if (_spotData?.Formula != value) {
                     _spotData.Formula = value;
                     OnPropertyChanged(nameof(formula));
                 }
             }
         }
-        public string ontology {  
+        public string ontology {
             get => _spotData.Ontology;
-            set 
-            {
-                if (_spotData.Ontology != value)
-                {
+            set {
+                if (_spotData.Ontology != value) {
                     _spotData.Ontology = value;
                     OnPropertyChanged(nameof(ontology));
                 }
-            } 
+            }
         }
-        public string smiles {  
+        public string smiles {
             get => _spotData.Smiles;
-            set 
-            {
-                if (_spotData.Smiles != value)
-                {
+            set {
+                if (_spotData.Smiles != value) {
                     _spotData.Smiles = value;
                     OnPropertyChanged(nameof(smiles));
                 }
-            } 
-        } 
-        public string inchikey { 
+            }
+        }
+        public string inchikey {
             get => _spotData.InchiKey;
-            set 
-            {
-                if (_spotData.InchiKey != value)
-                {
+            set {
+                if (_spotData.InchiKey != value) {
                     _spotData.InchiKey = value;
                     OnPropertyChanged(nameof(inchikey));
                 }
-            } 
+            }
         }
-        public string comment { 
+        public string comment {
             get => _spotData.Comment;
-            set 
-            {
-                if (_spotData.Comment != value)
-                {
+            set {
+                if (_spotData.Comment != value) {
                     _spotData.Comment = value;
                     OnPropertyChanged(nameof(comment));
                 }
-            } 
+            }
         }
-        public IonMode ionMode { 
+        public IonMode ionMode {
             get => _spotData.IonMode;
-            set 
-            {
-                if (_spotData.IonMode != value)
-                {
+            set {
+                if (_spotData.IonMode != value) {
                     _spotData.IonMode = value;
                     OnPropertyChanged(nameof(ionMode));
                     OnPropertyChanged(nameof(adductIons));
                 }
             }
         }
-        public List<AdductIon> adductIons
-        {
-            get 
-            {
-                if (ionMode == IonMode.Positive)
-                {
+        public List<AdductIon> adductIons {
+            get {
+                if (ionMode == IonMode.Positive) {
                     return _parameter.MS1PositiveAdductIonList;
-                }
-                if (ionMode == IonMode.Negative)
-                {
+                } if (ionMode == IonMode.Negative) {
                     return _parameter.MS1NegativeAdductIonList;
-                }
-                else 
-                {
+                } else {
                     return _parameter.MS1PositiveAdductIonList.Concat(_parameter.MS1NegativeAdductIonList).ToList();
                 }
             }
         }
-        public MSDataType spectrumType
-        {
+        public MSDataType spectrumType {
             get => _spotData.SpectrumType;
-            set
-            {
-                if (_spotData.SpectrumType != value)
-                {
+            set {
+                if (_spotData.SpectrumType != value) {
                     _spotData.SpectrumType = value;
                     OnPropertyChanged(nameof(spectrumType));
                 }
             }
         }
 
-        public double collisionEnergy
-        {
+        public double collisionEnergy {
             get => _spotData.CollisionEnergy;
-            set
-            {
-                if (_spotData.CollisionEnergy != value)
-                {
+            set {
+                if (_spotData.CollisionEnergy != value) {
                     _spotData.CollisionEnergy = value;
                     OnPropertyChanged(nameof(collisionEnergy));
                 }
@@ -257,6 +224,17 @@ namespace CompMs.App.Msdial.Model.Search {
         public int ms2Num {
             get => _spotData.Ms2PeakNumber;
         }
+
+        public List<FormulaResult>? formulaList {
+            get => _formulaData;
+            set {
+                if (_formulaData != value) {
+                    _formulaData = value;
+                    OnPropertyChanged(nameof(formulaList));
+                }
+            }
+        }
+
         public MsSpectrum ms1Spectrum { get; private set; }
         public MsSpectrum ms2Spectrum { get; private set; }
 
@@ -264,17 +242,34 @@ namespace CompMs.App.Msdial.Model.Search {
             _queryFile = queryFile;
             _parameter = parameter;
             Load();
+            LoadFormulaFile();
         }
 
-        public ReadOnlyReactivePropertySlim<bool>? SpectrumLoaded { get; }
-        public IObservable<ISpectraExporter> Exporter { get; }
         public AlignmentSpotPropertyModel Spot { get; private set; }
         public InternalMsfinderSettingModel SettingModel { get; }
+
+        public DelegateCommand RunFindFormula => _runFindFormula = new DelegateCommand(FindFormula);
+        private DelegateCommand _runFindFormula;
 
         private void Load() {
             _spotData = RawDataParcer.RawDataFileReader(_queryFile.RawDataFilePath, _parameter);
             ms1Spectrum = new MsSpectrum(_spotData.Ms1Spectrum);
             ms2Spectrum = new MsSpectrum(_spotData.Ms2Spectrum);
+        }
+
+        public void LoadFormulaFile() {
+            var error = string.Empty;
+            if (_queryFile.FormulaFileExists) {
+                _formulaData = FormulaResultParcer.FormulaResultReader(_queryFile.FormulaFilePath, out error);
+            }
+        }
+
+        public void FindFormula() {
+            var error = string.Empty;
+            var rawData = RawDataParcer.RawDataFileReader(_queryFile.RawDataFilePath, _parameter);
+            var formulaResults = MolecularFormulaFinder.GetMolecularFormulaList(productIonDB, neutralLossDB, existFormulaDB, rawData, _parameter);
+            FormulaResultParcer.FormulaResultsWriter(_queryFile.FormulaFilePath, formulaResults);
+            _formulaData = FormulaResultParcer.FormulaResultReader(_queryFile.FormulaFilePath, out error);
         }
 
         public Task ClearAsync(CancellationToken token = default) {
--- src/MSDIAL5/MsdialGuiApp/ViewModel/Search/InternalMsFinderViewModel.cs
+++ src/MSDIAL5/MsdialGuiApp/ViewModel/Search/InternalMsFinderViewModel.cs
@@ -1,15 +1,8 @@
-﻿using CompMs.App.Msdial.Model.DataObj;
-using CompMs.App.Msdial.Model.Search;
+﻿using CompMs.App.Msdial.Model.Search;
 using CompMs.App.Msdial.ViewModel.Chart;
-using CompMs.Common.DataObj.Property;
-using CompMs.Common.Enum;
 using CompMs.CommonMVVM;
-using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
 using Reactive.Bindings.Notifiers;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Windows.Input;
 
 namespace CompMs.App.Msdial.ViewModel.Search
 {
@@ -25,20 +18,10 @@ namespace CompMs.App.Msdial.ViewModel.Search
             InternalMsFinderMetaboliteList = model.InternalMsFinderMetaboliteList;
             spectrumMs1ViewModel = new SingleSpectrumViewModel(model.spectrumModelMs1).AddTo(Disposables);
             spectrumMs2ViewModel = new SingleSpectrumViewModel(model.spectrumModelMs2).AddTo(Disposables);
-
-            LoadAsyncCommand = new DelegateCommand(LoadAsync);
         }
 
         public InternalMsFinderMetaboliteList InternalMsFinderMetaboliteList { get; }
         public SingleSpectrumViewModel spectrumMs1ViewModel {  get; }
         public SingleSpectrumViewModel spectrumMs2ViewModel { get; }
-
-        public DelegateCommand LoadAsyncCommand { get; }
-
-        private void LoadAsync()  {
-            Mouse.OverrideCursor = Cursors.Wait;
-
-            Mouse.OverrideCursor = null;
-        }
     }
 }
