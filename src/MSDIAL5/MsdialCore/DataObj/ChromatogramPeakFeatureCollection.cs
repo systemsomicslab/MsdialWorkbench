fb03f57a0ffd40558d575bb3b371592ebca5e10e
htsugawa <htsugawa@go.tuat.ac.jp>
2024-08-15 13:09:14 +0900

    fixed null exception in chromatograpmpeakfeaturecollection_flatten function

--- src/MSDIAL5/MsdialCore/DataObj/ChromatogramPeakFeatureCollection.cs
+++ src/MSDIAL5/MsdialCore/DataObj/ChromatogramPeakFeatureCollection.cs
@@ -66,7 +66,8 @@ namespace CompMs.MsdialCore.DataObj
 
         public ChromatogramPeakFeatureCollection Flatten()
         {
-            var flatten = _items.SelectMany(item => item.DriftChromFeatures.Any() ? item.DriftChromFeatures : [item]).ToList();
+            
+            var flatten = _items.SelectMany(item => item.DriftChromFeatures != null && item.DriftChromFeatures.Any() ? item.DriftChromFeatures : [item]).ToList();
             return new ChromatogramPeakFeatureCollection(flatten);
         }
 
