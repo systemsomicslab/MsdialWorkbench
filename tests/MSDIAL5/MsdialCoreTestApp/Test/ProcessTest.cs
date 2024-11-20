5b730579877f3067fcde43dd0b8d8242a8695002
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2024-11-20 16:01:22 +0900

    Remove MsdialCore/Utility/RawDataDump

--- tests/MSDIAL5/MsdialCoreTestApp/Test/ProcessTest.cs
+++ tests/MSDIAL5/MsdialCoreTestApp/Test/ProcessTest.cs
@@ -314,4 +314,20 @@ namespace CompMs.App.MsdialConsole.Test {
             (22, 5), (22, 6),
         };
     }
+
+    sealed class RawDataDump {
+        private RawDataDump() { }
+
+        public static void Dump(string filepath) {
+            var allSpectra = DataAccess.GetAllSpectra(filepath);
+            foreach (var spec in allSpectra) {
+                var precursorMz = spec.Precursor == null ? -1 : spec.Precursor.SelectedIonMz;
+                // var specString = Common.Utility.ComponentsConverter.GetSpectrumString(spec.Spectrum);
+                //Console.WriteLine("ID={0}, Time={1}, Polarity={2}, MS level={3}, Precursor mz={4}, Spectrum={5}",
+                //    spec.ScanNumber, spec.ScanStartTime, spec.ScanPolarity, spec.MsLevel, precursorMz, specString);
+                Console.WriteLine("ID={0}, Time={1}, Polarity={2}, MS level={3}, Precursor mz={4}, SpecCount={5}",
+                    spec.ScanNumber, spec.ScanStartTime, spec.ScanPolarity, spec.MsLevel, precursorMz, spec.Spectrum.Length);
+            }
+        }
+    }
 }
