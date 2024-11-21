37c1b2f8e1208f312a6b0c422ac3a614a6a60ff7
Bujee <s238586u@st.go.tuat.ac.jp>
2024-11-21 18:20:46 +0900

    Deleted Reflect to Msdial from Msfinder batch processing

--- src/MSDIAL5/MsdialGuiApp/Model/Search/MsfinderObservedMetabolite.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/Search/MsfinderObservedMetabolite.cs
@@ -1,5 +1,4 @@
 ﻿using CompMs.App.Msdial.Model.Chart;
-using CompMs.App.Msdial.Model.DataObj;
 using CompMs.App.Msdial.Model.Information;
 using CompMs.App.Msdial.View.Search;
 using CompMs.App.Msdial.ViewModel.Information;
@@ -7,12 +6,10 @@ using CompMs.Common.Components;
 using CompMs.Common.DataObj;
 using CompMs.Common.DataObj.Ion;
 using CompMs.Common.DataObj.Property;
-using CompMs.Common.DataObj.Result;
 using CompMs.Common.Enum;
 using CompMs.Common.FormulaGenerator;
 using CompMs.Common.FormulaGenerator.DataObj;
 using CompMs.Common.FormulaGenerator.Parser;
-using CompMs.Common.Interfaces;
 using CompMs.Common.Parameter;
 using CompMs.Common.StructureFinder.DataObj;
 using CompMs.Common.StructureFinder.Parser;
@@ -42,8 +39,6 @@ namespace CompMs.App.Msdial.Model.Search {
         private readonly BehaviorSubject<AxisRange?> _spectrumRange;
         private readonly BehaviorSubject<MsSpectrum> _ms1SpectrumSubject;
         private readonly BehaviorSubject<MsSpectrum> _ms2SpectrumSubject;
-        private readonly MsScanMatchResultContainerModel _msScanMatchResultContainer;
-        private readonly ChromatogramPeakFeatureModel _chromatogram;
 
         private static readonly List<ProductIon> productIonDB = CompMs.Common.FormulaGenerator.Parser.FragmentDbParser.GetProductIonDB(
                 @"Resources\msfinderLibrary\ProductIonLib_vs1.pid", out string _);
@@ -431,41 +426,6 @@ namespace CompMs.App.Msdial.Model.Search {
             substructure.Closed += (s, e) => vm.Dispose();
             substructure.Show();
             Mouse.OverrideCursor = null;
-        }
-
-        public DelegateCommand ReflectToMsdial => _reflectToMsdial ??= new DelegateCommand(ReflectToMsdialAsync);
-        private DelegateCommand? _reflectToMsdial;
-        public void ReflectToMsdialAsync() {
-            if (SelectedStructure is not null) {
-                var moleculeMsReference = new MoleculeMsReference() {
-                    ChromXs = new ChromXs() { RT = new RetentionTime(SelectedStructure.RetentionTime) },
-                    Spectrum = _refSpectrum.Value?.Spectrum ?? [],
-                    Formula = new Formula() { FormulaString = SelectedStructure.Formula },
-                    AdductType = _chromatogram.AdductType,
-                    PrecursorMz = SelectedStructure.PrecursorMz,
-                    Name = SelectedStructure.Title,
-                    InChIKey = SelectedStructure.Inchikey,
-                    SMILES = SelectedStructure.Smiles,
-                    Ontology = SelectedStructure.Ontology,
-                };
-                mspDB.Add(moleculeMsReference);
-                var matchResult = new MsScanMatchResult {
-                    AnnotatorID = "MS-FINDER",
-                    Source = SourceType.Manual,
-                    Name = SelectedStructure.Title,
-                    InChIKey = SelectedStructure.Inchikey,
-                    TotalScore = ((float)SelectedStructure.TotalScore),
-                    AcurateMassSimilarity = ((float)SelectedStructure.TotalMaLikelihood),
-                    RtSimilarity = ((float)SelectedStructure.RtSimilarityScore),
-                    RiSimilarity = ((float)SelectedStructure.RiSimilarityScore),
-                    LibraryID = moleculeMsReference.ScanID,
-                };
-                _msScanMatchResultContainer.AddResult(matchResult);
-            }
-            else
-            {
-                MessageBox.Show("Please select structure to reflect to the MS-DIAL.");
-            }
         }
     }
 }
