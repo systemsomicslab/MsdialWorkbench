6435804c6076b84cd7f3940c02a3eb51a710c675
Bujee <s238586u@st.go.tuat.ac.jp>
2024-11-21 09:25:41 +0900

    Update Msfinder batch processing

--- src/MSDIAL5/MsdialGuiApp/Model/Search/InternalMsFinder.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/Search/InternalMsFinder.cs
@@ -25,21 +25,24 @@ namespace CompMs.App.Msdial.Model.Search {
             ObservedMetabolites = new ReadOnlyObservableCollection<MsfinderObservedMetabolite>(_observedMetabolites);
             _selectedObservedMetabolite = ObservedMetabolites.FirstOrDefault();
 
-            var internalMsFinderMs1 = new ObservableMsSpectrum(_selectedObservedMetabolite._ms1SpectrumSubject, null, Observable.Return<ISpectraExporter?>(null)).AddTo(Disposables);
+            var ms1 = this.ObserveProperty(m => m.SelectedObservedMetabolite).Select(m => m?._ms1SpectrumSubject.Value);
+            var internalMsFinderMs1 = new ObservableMsSpectrum(ms1, null, Observable.Return<ISpectraExporter?>(null)).AddTo(Disposables);
             var ms1HorizontalAxis = internalMsFinderMs1.CreateAxisPropertySelectors(new PropertySelector<SpectrumPeak, double>(p => p.Mass), "m/z", "m/z");
             var ms1VerticalAxis = internalMsFinderMs1.CreateAxisPropertySelectors2(new PropertySelector<SpectrumPeak, double>(p => p.Intensity), "Intensity");
 
-            var internalMsFinderMs2 = new ObservableMsSpectrum(_selectedObservedMetabolite._ms2SpectrumSubject, null, Observable.Return<ISpectraExporter?>(null)).AddTo(Disposables);
+            var ms2 = this.ObserveProperty(m => m.SelectedObservedMetabolite).Select(m => m?._ms2SpectrumSubject.Value);
+            var internalMsFinderMs2 = new ObservableMsSpectrum(ms2, null, Observable.Return<ISpectraExporter?>(null)).AddTo(Disposables);
             var ms2HorizontalAxis = internalMsFinderMs2.CreateAxisPropertySelectors(new PropertySelector<SpectrumPeak, double>(p => p.Mass), "m/z", "m/z");
             var ms2VerticalAxis = internalMsFinderMs2.CreateAxisPropertySelectors2(new PropertySelector<SpectrumPeak, double>(p => p.Intensity), "Intensity");
 
-            var structureMs2 = new ObservableMsSpectrum(_selectedObservedMetabolite._ms2SpectrumSubject, null, Observable.Return<ISpectraExporter?>(null)).AddTo(Disposables);
+            var structureMs2 = new ObservableMsSpectrum(ms2, null, Observable.Return<ISpectraExporter?>(null)).AddTo(Disposables);
             var ms2RefRange = _selectedObservedMetabolite._spotData.Ms2Spectrum.IsEmptyOrNull()
                 ? null
                 : new AxisRange(_selectedObservedMetabolite._spotData.Ms2Spectrum.Min(p => p.Mass), _selectedObservedMetabolite._spotData.Ms2Spectrum.Max(p => p.Mass));
             var ms2VerticalAxis2 = structureMs2.CreateAxisPropertySelectors2(new PropertySelector<SpectrumPeak, double>(p => p.Intensity), "Intensity");
 
-            var structureRef = new ObservableMsSpectrum(_selectedObservedMetabolite._refSpectrum, null, Observable.Return<ISpectraExporter?>(null)).AddTo(Disposables);
+            var refMs = this.ObserveProperty(m => m.SelectedObservedMetabolite).Select(m => m?._refSpectrum.Value);
+            var structureRef = new ObservableMsSpectrum(refMs, null, Observable.Return<ISpectraExporter?>(null)).AddTo(Disposables);
             var refVerticalAxis = structureRef.CreateAxisPropertySelectors2(new PropertySelector<SpectrumPeak, double>(p => p.Intensity), "Intensity");
             var horizontalAxis = _selectedObservedMetabolite._spectrumRange.Select(range => AxisRange.Union(range, ms2RefRange) ?? new AxisRange(0d, 1d)).ToReactiveContinuousAxisManager<double>(new ConstantMargin(40d)).AddTo(Disposables);
             var itemSelector = new AxisItemSelector<double>(new AxisItemModel<double>("m/z", horizontalAxis, "m/z")).AddTo(Disposables);
@@ -54,7 +57,6 @@ namespace CompMs.App.Msdial.Model.Search {
             var ms2SpectrumModel = new SingleSpectrumModel(structureMs2, refMs2HorizontalAxis, ms2VerticalAxis2, new ChartHueItem(string.Empty, new ConstantBrushMapper(Brushes.Blue)), msGraphLabels);
             var refSpectrumModel = new SingleSpectrumModel(structureRef, refMs2HorizontalAxis, refVerticalAxis, new ChartHueItem(string.Empty, new ConstantBrushMapper(Brushes.Red)), msGraphLabels);
             RefMs2SpectrumModel = new MsSpectrumModel(ms2SpectrumModel, refSpectrumModel, Observable.Return<Ms2ScanMatching?>(null)).AddTo(Disposables);
-            
         }
 
         public SingleSpectrumModel SpectrumModelMs1 { get; }
@@ -75,7 +77,7 @@ namespace CompMs.App.Msdial.Model.Search {
 
         public MsfinderObservedMetabolite? SelectedObservedMetabolite {
             get => _selectedObservedMetabolite;
-            set => SetProperty(ref _selectedObservedMetabolite, value);
+            set => SetProperty(ref _selectedObservedMetabolite, value); 
         }
         private MsfinderObservedMetabolite? _selectedObservedMetabolite;
     }
