a552075eb7cd49d32d7698f323303d60a5301b07
Bujee <s238586u@st.go.tuat.ac.jp>
2024-12-04 15:30:27 +0900

    Update msfinder parameter setting for single, batch analysis

--- src/MSDIAL5/MsdialGuiApp/View/Setting/InternalMsfinderSettingView.xaml.cs
+++ src/MSDIAL5/MsdialGuiApp/View/Setting/InternalMsfinderSettingView.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace CompMs.App.Msdial.View.Setting
 {
@@ -23,29 +12,6 @@ namespace CompMs.App.Msdial.View.Setting
         public InternalMsfinderSettingView()
         {
             InitializeComponent();
-        }
-
-        private void Button_ExistProjectPath_Browse_Click(object sender, RoutedEventArgs e)
-        {
-            var fbd = new Graphics.Window.SelectFolderDialog
-            {
-                Title = "Chose a MSFINDER project folder.",
-            };
-            if (fbd.ShowDialog() == Graphics.Window.DialogResult.OK)
-            {
-                TextBox_ExistProjectPath.Text = fbd.SelectedPath;
-            }
-
-            //System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog();
-            //fbd.RootFolder = Environment.SpecialFolder.Desktop;
-            //fbd.Description = "Chose a MSFINDER project folder.";
-            //fbd.SelectedPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-
-            //if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            //{
-            //    TextBox_ExistProjectPath.Text = fbd.SelectedPath;
-            //}
-
         }
 
         private void Button_RtInChiKeyDictionaryFilepath_Browse_Click(object sender, RoutedEventArgs e)
@@ -95,6 +61,5 @@ namespace CompMs.App.Msdial.View.Setting
                 TextBox_CcsAdductInChIKeyDictionaryFilepath.Text = ofd.FileName;
             }
         }
-
     }
 }
