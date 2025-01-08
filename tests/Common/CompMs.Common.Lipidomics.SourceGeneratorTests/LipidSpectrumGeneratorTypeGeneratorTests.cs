504acb2acc5ca7ef7a9a1dc2da2f744f8bdc30eb
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-01-08 14:10:45 +0900

    Add LipidSpectrumGeneratorTest

---
+++ tests/Common/CompMs.Common.Lipidomics.SourceGeneratorTests/LipidSpectrumGeneratorTypeGeneratorTests.cs
@@ -0,0 +1,27 @@
+﻿using CompMs.Common.Parser;
+
+namespace CompMs.Common.Lipidomics.SourceGenerator.Tests;
+
+[TestClass]
+public class LipidSpectrumGeneratorTypeGeneratorTests
+{
+    [TestMethod]
+    [DeploymentItem("LipidSpectrumGeneratorTest.msp")]
+    public void GenreratedGeneratorTest() {
+        var lib = MspFileParser.MspFileReader("LipidSpectrumGeneratorTest.msp");
+
+        var generator = new PCLipidSpectrumGenerator();
+
+        foreach (var reference in lib) {
+            var lipid = FacadeLipidParser.Default.Parse(reference.Name);
+            var spectrum = generator.Generate((Lipid)lipid, reference.AdductType)?.OrderBy(s => s.Mass).ToList();
+
+            Assert.IsNotNull(spectrum);
+            Assert.AreEqual(reference.Spectrum.Count, spectrum.Count);
+            for (int i = 0; i < spectrum.Count; i++) {
+                Assert.AreEqual(reference.Spectrum[i].Mass, spectrum[i].Mass, 1e-4);
+                Assert.AreEqual(reference.Spectrum[i].Intensity, spectrum[i].Intensity);
+            }
+        }
+    }
+}
