311830d77292bb25ae8f1728154ecb9b6e90e166
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-03-18 19:39:21 +0900

    Add SpectraSimilarityMapViewModel with reactive properties
    A new ViewModel class `SpectraSimilarityMapViewModel` has been added to the `CompMs.App.Msdial.ViewModel.Information` namespace. This class is designed to work with a model of type `SpectraSimilarityMapModel` and includes properties `MzBin`, `MzBegin`, `MzEnd`, and `Result`, which are synchronized with the model using ReactiveProperty. An `UpdateCommand` property of type `AsyncReactiveCommand` has been added, bound to the `UpdateSimilaritiesAsync` method of the model. The class inherits from `ViewModelBase` and uses Reactive Extensions for property synchronization and command handling. Necessary using directives have also been added.

---
+++ src/MSDIAL5/MsdialGuiApp/ViewModel/Information/SpectraSimilarityMapViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CompMs.App.Msdial.Model.Information;
+using CompMs.CommonMVVM;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace CompMs.App.Msdial.ViewModel.Information;
+
+internal sealed class SpectraSimilarityMapViewModel : ViewModelBase
+{
+    private readonly SpectraSimilarityMapModel _model;
+
+    public SpectraSimilarityMapViewModel(SpectraSimilarityMapModel model) {
+        _model = model;
+
+        MzBin = model.ToReactivePropertyAsSynchronized(m => m.MzBin).AddTo(Disposables);
+        MzBegin = model.ToReactivePropertyAsSynchronized(m => m.MzBegin).AddTo(Disposables);
+        MzEnd = model.ToReactivePropertyAsSynchronized(m => m.MzEnd).AddTo(Disposables);
+        Result = model.ObserveProperty(m => m.Result).ToReadOnlyReactivePropertySlim([]).AddTo(Disposables);
+
+        UpdateCommand = new AsyncReactiveCommand().WithSubscribe(() => model.UpdateSimilaritiesAsync()).AddTo(Disposables);
+    }
+
+    public ReactiveProperty<double> MzBin { get; }
+    public ReactiveProperty<double> MzBegin { get; }
+    public ReactiveProperty<double> MzEnd { get; }
+
+    public ReadOnlyReactivePropertySlim<SimilarityMatrixItem[]> Result { get; }
+
+    public AsyncReactiveCommand UpdateCommand { get; }
+}
