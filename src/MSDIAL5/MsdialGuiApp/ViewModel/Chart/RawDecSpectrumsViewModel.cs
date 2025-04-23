d731d59d4fd05ac8d47c334cb16f3a194574b7f6
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-04-23 15:10:37 +0900

    Add Q1DecLoader support to RawDecSpectrumsViewModel
    Added properties and logic to handle Q1Dec MS2 IDs and reference
    spectrum models in RawDecSpectrumsViewModel. Introduced null
    checks for Q1DecLoader, initializing or binding properties
    accordingly. Declared new reactive properties for Q1Dec data
    management and ensured proper disposal of resources.

--- src/MSDIAL5/MsdialGuiApp/ViewModel/Chart/RawDecSpectrumsViewModel.cs
+++ src/MSDIAL5/MsdialGuiApp/ViewModel/Chart/RawDecSpectrumsViewModel.cs
@@ -28,15 +28,30 @@ namespace CompMs.App.Msdial.ViewModel.Chart
                 SelectedMs2Id = model.RawLoader.Ms2IdSelector;
             }
 
+            if (model.Q1DecLoader is null) {
+                Q1DecMs2IdList = Observable.Return(new List<MsSelectionItem>(0)).ToReadOnlyReactivePropertySlim().AddTo(Disposables);
+                Q1DecSelectedMs2Id = new ReactivePropertySlim<MsSelectionItem?>().AddTo(Disposables);
+            }
+            else {
+                Q1DecMs2IdList = model.Q1DecLoader.Ms2List.ToReadOnlyReactivePropertySlim().AddTo(Disposables);
+                Q1DecSelectedMs2Id = model.Q1DecLoader.Ms2IdSelector;
+            }
+
             RawRefSpectrumViewModels = new MsSpectrumViewModel(model.RawRefSpectrumModels, focusAction: focusAction, isFocused: isFocused).AddTo(Disposables);
+            if (model.Q1DecRefSpectrumModels is not null) {
+                Q1DecRefSpectrumViewModels = new MsSpectrumViewModel(model.Q1DecRefSpectrumModels, focusAction: focusAction, isFocused: isFocused).AddTo(Disposables);
+            }
             DecRefSpectrumViewModels = new MsSpectrumViewModel(model.DecRefSpectrumModels, focusAction: focusAction, isFocused: isFocused).AddTo(Disposables);
         }
 
         public MsSpectrumViewModel RawRefSpectrumViewModels { get; }
+        public MsSpectrumViewModel? Q1DecRefSpectrumViewModels { get; }
         public MsSpectrumViewModel DecRefSpectrumViewModels { get; }
 
         public ReadOnlyReactivePropertySlim<List<MsSelectionItem>> Ms2IdList { get; }
         public ReactivePropertySlim<MsSelectionItem?> SelectedMs2Id { get; }
+        public ReadOnlyReactivePropertySlim<List<MsSelectionItem>> Q1DecMs2IdList { get; }
+        public ReactivePropertySlim<MsSelectionItem?> Q1DecSelectedMs2Id { get; }
         public ReactivePropertySlim<AxisItemModel<double>> LowerVerticalAxisItem => _model.DecRefSpectrumModels.LowerVerticalAxisItem;
         public ObservableCollection<AxisItemModel<double>> LowerVerticalAxisItemCollection => _model.DecRefSpectrumModels.LowerVerticalAxisItemCollection;
 
