463f3568a2496c97bb28ebf6c5dd8607de76eec2
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-04-23 15:09:26 +0900

    Add IMultiMsmsSpectrumLoader interface with new properties
    Introduced the `IMultiMsmsSpectrumLoader<T>` interface in the
    `CompMs.App.Msdial.Model.Loader` namespace, extending
    `IMsSpectrumLoader<T>`. Added `Ms2IdSelector` and `Ms2List`
    properties to support reactive selection and observation of
    `MsSelectionItem` objects. Updated `using` directives to include
    necessary namespaces.

---
+++ src/MSDIAL5/MsdialGuiApp/Model/Loader/IMultiMsmsSpectrumLoader.cs
@@ -0,0 +1,10 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+
+namespace CompMs.App.Msdial.Model.Loader;
+
+interface IMultiMsmsSpectrumLoader<T> : IMsSpectrumLoader<T> {
+    ReactivePropertySlim<MsSelectionItem?> Ms2IdSelector { get; }
+    IObservable<List<MsSelectionItem>> Ms2List { get; }
+}
