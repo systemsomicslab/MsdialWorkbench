15bd7000218414e7bafc0425d7d676cc870b4342
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-04-24 15:50:57 +0900

    Add LoadProductIonMapUsecase for loading product ion spectra
    Introduce `LoadProductIonMapUsecase` in the `CompMs.App.Msdial.Model.MsResult` namespace to handle product ion spectra loading. The class is `internal sealed` and depends on `IDataProvider`.
    
    Key features:
    - Added `MzTolerance` property with a default value of `0.01d`.
    - Implemented `LoadProductIonSpectraAsync` method to load, filter, and aggregate product ion spectra using `SpectraLoadingQuery` and `_provider`.
    - Added necessary `using` directives for required namespaces.

---
+++ src/MSDIAL5/MsdialGuiApp/Model/MsResult/LoadProductIonMapUsecase.cs
@@ -0,0 +1,36 @@
+﻿using CompMs.App.Msdial.Model.DataObj;
+using CompMs.Common.DataObj;
+using CompMs.Common.Interfaces;
+using CompMs.Raw.Abstractions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CompMs.App.Msdial.Model.MsResult;
+
+internal sealed class LoadProductIonMapUsecase(IDataProvider provider)
+{
+    private readonly IDataProvider _provider = provider;
+
+    public double MzTolerance { get; set; } = .01d;
+
+    public async Task<List<MappedIon>> LoadProductIonSpectraAsync(IChromatogramPeakFeature peak, MzRange productIonRange, CancellationToken token = default) {
+        var query = new SpectraLoadingQuery
+        {
+            MSLevel = 2,
+            PrecursorMzRange = new() { Mz = peak.Mass, Tolerance = 20d, },
+            ScanTimeRange = new() { Start = peak.ChromXsLeft.RT.Value, End = peak.ChromXsRight.RT.Value, },
+        };
+
+        var spectra = await _provider.LoadMSSpectraAsync(query, token).ConfigureAwait(false);
+
+        token.ThrowIfCancellationRequested();
+        return spectra.SelectMany(
+            s => s.Spectrum.Where(p => productIonRange.IsWithinRange(p.Mz)),
+            (s, p) => new MappedIon(s.Id, s.ExperimentID, p.Intensity))
+            .GroupBy(ion => (ion.ID, ion.ExperimentID))
+            .Select(g => new MappedIon(g.Key.ID, g.Key.ExperimentID, g.Sum(ion => ion.Intensity)))
+            .ToList();
+    }
+}
