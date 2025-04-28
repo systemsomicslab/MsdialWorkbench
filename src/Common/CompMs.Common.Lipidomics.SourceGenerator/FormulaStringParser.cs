c105277204842445b192b842ac65eec8e4d0aa70
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-04-28 15:27:56 +0900

    Add FormulaStringParser and related tests
    Added the `FormulaStringParser` class to parse chemical formula strings into a dictionary representation, with methods for validation and conversion. Introduced corresponding tests in `FormulaStringParserTests` to ensure functionality. Updated `CompMs.Common.Lipidomics.SourceGenerator.csproj` with an `InternalsVisibleTo` directive for test project access.

---
+++ src/Common/CompMs.Common.Lipidomics.SourceGenerator/FormulaStringParser.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CompMs.Common.Lipidomics.SourceGenerator;
+
+internal sealed class FormulaStringParser
+{
+    private static readonly Regex _atomRegex, _rawFormulaRegex;
+
+    static FormulaStringParser() {
+        var atom = @"[A-Z][a-z]?|\[\d+[A-Z][a-z]?\]";
+        var atoms = @$"({atom})(\d*)";
+        var rawFormula = $@"^\s*({atoms})+\s*$";
+
+        _atomRegex = new Regex(atoms, RegexOptions.Compiled);
+        _rawFormulaRegex = new Regex(rawFormula, RegexOptions.Compiled);
+    }
+
+    public static bool CanConvertToFormulaDictionary(string formulaString) {
+        return _rawFormulaRegex.IsMatch(formulaString);
+    }
+
+    public static Dictionary<string, int> ConvertToFormulaDictionary(string formulaString) {
+        var matches = _atomRegex.Matches(formulaString);
+        var res = new List<(string, int)>();
+        foreach (Match match in matches) {
+            var element = match.Groups[1].Value;
+            var number = match.Groups[2].Value;
+
+            res.Add((element, int.TryParse(number, out var result) ? result : 1));
+        }
+        return ToFormulaDictionary(res);
+    }
+
+    private static Dictionary<string, int> ToFormulaDictionary(IEnumerable<(string, int)> elements) {
+        var result = new Dictionary<string, int>();
+        foreach ((var element, var number) in elements) {
+            if (result.ContainsKey(element)) {
+                result[element] += number;
+            }
+            else {
+                result[element] = number;
+            }
+        }
+        return result;
+    }
+}
---
+++ tests/Common/CompMs.Common.Lipidomics.SourceGeneratorTests/FormulaStringParserTests.cs
@@ -0,0 +1,15 @@
+﻿namespace CompMs.Common.Lipidomics.SourceGenerator.Tests;
+
+[TestClass]
+public class FormulaStringParserTests
+{
+    [DataTestMethod]
+    [DataRow(true, "C12H24O12")]
+    [DataRow(false, "<Carbon>12</Carbon>")]
+    [DataRow(false, "\n<Carbon>12</Carbon>\n")]
+    [DataRow(false, "\n<Carbon>12</Carbon>\n <Hydrogen>24</Carbon>\n")]
+    public void CanConvertToFormulaDictionaryTest(bool expected, string formulaString) {
+        var result = FormulaStringParser.CanConvertToFormulaDictionary(formulaString);
+        Assert.AreEqual(expected, result);
+    }
+}
