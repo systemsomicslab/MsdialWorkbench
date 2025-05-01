23b2c5df85f3ffecce423fe4765b9047db27910c
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-05-01 15:58:17 +0900

    Add ProductIonIntensityMapModel for ion mapping
    Introduce `ProductIonIntensityMapModel` in the `CompMs.App.Msdial.Model.MsResult` namespace. This internal sealed class inherits from `BindableBase` and provides functionality for managing product ion intensity mapping. Key features include:
    
    - Private fields for managing peak data, selected range, and loaded ions.
    - Public properties `SelectedRange` and `LoadedIons` with property change notification.
    - A read-only `MsSpectrumModel` property initialized via the constructor.
    - An asynchronous method `LoadIonsAsync` to load product ion spectra based on the selected range and peak data.
    
    The class leverages observables and dependency injection for efficient and modular design.

---
+++ src/MSDIAL5/MsdialGuiApp/Model/MsResult/ProductIonIntensityMapModel.cs
@@ -0,0 +1,49 @@
+﻿using CompMs.App.Msdial.Model.Chart;
+using CompMs.Common.DataObj;
+using CompMs.Common.Interfaces;
+using CompMs.CommonMVVM;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CompMs.App.Msdial.Model.MsResult;
+
+internal sealed class ProductIonIntensityMapModel : BindableBase
+{
+    private readonly IObservable<IChromatogramPeakFeature?> _peak;
+    private readonly LoadProductIonMapUsecase _loadPIUsecase;
+
+    public ProductIonIntensityMapModel(IObservable<IChromatogramPeakFeature?> peak, MsSpectrumModel msSpectrumModel, LoadProductIonMapUsecase loadPIUsecase) {
+        _peak = peak.Replay(1).RefCount();
+        _loadPIUsecase = loadPIUsecase;
+        MsSpectrumModel = msSpectrumModel;
+    }
+
+    public MzRange? SelectedRange {
+        get => _selectedRange;
+        set => SetProperty(ref _selectedRange, value);
+    }
+    private MzRange? _selectedRange;
+
+    public List<MappedIon>? LoadedIons {
+        get => _loadedIons;
+        set => SetProperty(ref _loadedIons, value);
+    }
+    private List<MappedIon>? _loadedIons;
+
+    public MsSpectrumModel MsSpectrumModel { get; }
+
+    public async Task LoadIonsAsync(CancellationToken token = default) {
+        if (SelectedRange is null) {
+            return;
+        }
+
+        var peak = await _peak.FirstAsync();
+        if (peak is null) {
+            return;
+        }
+        LoadedIons = await _loadPIUsecase.LoadProductIonSpectraAsync(peak, SelectedRange, token).ConfigureAwait(false);
+    }
+}
