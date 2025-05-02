071485efd733458cbd98b535d6ec8f4698e7d077
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-05-02 15:24:37 +0900

    Refactor SelectedItem handling and event management
    Replaced `OnItemChanged` with `OnSelectedItemChanged` and `OnClick`
    methods for improved event handling. Removed unused methods
    (`OnItemChanged`, `FindAncestorWithProperty`) and the
    `using System.Windows.Media;` directive. Updated `ItemProperty`
    metadata to remove obsolete callback.

--- src/Common/ChartDrawing/Behavior/ItemSelectBehavior.cs
+++ src/Common/ChartDrawing/Behavior/ItemSelectBehavior.cs
@@ -1,7 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Media;
 
 namespace CompMs.Graphics.Behavior;
 
@@ -14,54 +13,47 @@ public static class ItemSelectBehavior
             typeof(ItemSelectBehavior),
             new FrameworkPropertyMetadata(
                 null,
-                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault)
+                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                OnSelectedItemChanged)
         );
 
     public static object GetSelectedItem(DependencyObject d) => d.GetValue(SelectedItemProperty);
 
     public static void SetSelectedItem(DependencyObject d, object value) => d.SetValue(SelectedItemProperty, value);
 
+    private static void OnSelectedItemChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        if (d is UIElement ui) {
+            ui.RemoveHandler(ButtonBase.ClickEvent, OnClickHandler);
+            ui.AddHandler(ButtonBase.ClickEvent, OnClickHandler);
+        }
+        if (d is ContextMenu cm) {
+            cm.RemoveHandler(MenuItem.ClickEvent, OnClickHandler);
+            cm.AddHandler(MenuItem.ClickEvent, OnClickHandler);
+        }
+        if (d is MenuItem mi) {
+            mi.RemoveHandler(MenuItem.ClickEvent, OnClickHandler);
+            mi.AddHandler(MenuItem.ClickEvent, OnClickHandler);
+        }
+    }
+
+    private static readonly RoutedEventHandler OnClickHandler = OnClick;
+
+    private static void OnClick(object sender, RoutedEventArgs e) {
+        if (e.OriginalSource is DependencyObject src && sender is DependencyObject d) {
+            var value = GetItem(src);
+            d.SetCurrentValue(SelectedItemProperty, value);
+        }
+    }
+
     public static readonly DependencyProperty ItemProperty =
         DependencyProperty.RegisterAttached(
             "Item",
             typeof(object),
             typeof(ItemSelectBehavior),
-            new PropertyMetadata(null, OnItemChanged)
+            new PropertyMetadata(null)
         );
 
     public static object GetItem(DependencyObject d) => d.GetValue(ItemProperty);
 
     public static void SetItem(DependencyObject d, object value) => d.SetValue(ItemProperty, value);
-
-    private static void OnItemChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
-        if (d is ButtonBase btn) {
-            WeakEventManager<ButtonBase, RoutedEventArgs>.RemoveHandler(btn, nameof(ButtonBase.Click), OnClick);
-            WeakEventManager<ButtonBase, RoutedEventArgs>.AddHandler(btn, nameof(ButtonBase.Click), OnClick);
-        }
-        if (d is MenuItem menu) {
-            WeakEventManager<MenuItem, RoutedEventArgs>.RemoveHandler(menu, nameof(MenuItem.Click), OnClick);
-            WeakEventManager<MenuItem, RoutedEventArgs>.AddHandler(menu, nameof(MenuItem.Click), OnClick);
-        }
-    }
-
-    private static void OnClick(object sender, RoutedEventArgs e) {
-        if (sender is DependencyObject d) {
-            var parent = FindAncestorWithProperty(d, SelectedItemProperty);
-            if (parent is not null) {
-                var value = GetItem(d);
-                parent.SetCurrentValue(SelectedItemProperty, value);
-            }
-        }
-    }
-
-    private static DependencyObject? FindAncestorWithProperty(DependencyObject child, DependencyProperty property) {
-        var parent = VisualTreeHelper.GetParent(child);
-        while (parent is not null) {
-            if (parent.ReadLocalValue(property) != DependencyProperty.UnsetValue) {
-                return parent;
-            }
-            parent = VisualTreeHelper.GetParent(parent);
-        }
-        return null;
-    }
 }
