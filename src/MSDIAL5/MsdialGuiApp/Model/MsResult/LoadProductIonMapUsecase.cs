5ec68dc57e69c55c447f1145ce4b63fea6b5bc00
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-05-08 10:57:57 +0900

    Optimize LoadProductIonSpectraAsync with caching logic
    Introduced caching for spectra in LoadProductIonSpectraAsync by
    adding `_previousPeak` and `_previousSpectra` fields. This avoids
    redundant calls to `LoadMSSpectraAsync` when processing the same
    peak multiple times, improving performance. Added the
    `System.Threading.Tasks` namespace to support async operations.

--- src/MSDIAL5/MsdialGuiApp/Model/MsResult/LoadProductIonMapUsecase.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/MsResult/LoadProductIonMapUsecase.cs
@@ -12,18 +12,24 @@ namespace CompMs.App.Msdial.Model.MsResult;
 internal sealed class LoadProductIonMapUsecase(IDataProvider provider)
 {
     private readonly IDataProvider _provider = provider;
+    private IChromatogramPeakFeature? _previousPeak = null;
+    private RawSpectrum[]? _previousSpectra = null;
 
     public double MzTolerance { get; set; } = .01d;
 
     public async Task<List<MappedIon>> LoadProductIonSpectraAsync(IChromatogramPeakFeature peak, MzRange productIonRange, CancellationToken token = default) {
+        if (peak != _previousPeak) {
+            _previousPeak = peak;
+
         var query = new SpectraLoadingQuery
         {
             MSLevel = 2,
             PrecursorMzRange = new() { Mz = peak.Mass, Tolerance = 20d, },
             ScanTimeRange = new() { Start = peak.ChromXsLeft.RT.Value, End = peak.ChromXsRight.RT.Value, },
         };
-
-        var spectra = await _provider.LoadMSSpectraAsync(query, token).ConfigureAwait(false);
+            _previousSpectra = await _provider.LoadMSSpectraAsync(query, token).ConfigureAwait(false);
+        }
+        var spectra = _previousSpectra;
 
         token.ThrowIfCancellationRequested();
         return spectra.SelectMany(
