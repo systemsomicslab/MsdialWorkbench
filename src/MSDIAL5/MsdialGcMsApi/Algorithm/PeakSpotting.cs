e65717a08bf37ab43290f7393f6da55d5574c4f5
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-05-12 18:48:31 +0900

    Enhance GCGCMS peak processing and grouping logic
    Updated `using` directives in `PeakSpotting.cs` to include necessary namespaces. Added conditional logic in the `Run` method to handle GCGCMS machine category data by grouping and merging nearby peaks using the new `IsNearBy` method. Modified `chromPeakFeatures` list to reflect the updated grouping logic. Introduced `IsNearBy` method to calculate proximity based on mass, retention time, and modulation time tolerances.

--- src/MSDIAL5/MsdialGcMsApi/Algorithm/PeakSpotting.cs
+++ src/MSDIAL5/MsdialGcMsApi/Algorithm/PeakSpotting.cs
@@ -1,11 +1,13 @@
 ﻿using CompMs.Common.Components;
 using CompMs.Common.DataObj.Database;
+using CompMs.Common.Interfaces;
 using CompMs.MsdialCore.Algorithm;
 using CompMs.MsdialCore.DataObj;
 using CompMs.MsdialCore.Utility;
 using CompMs.MsdialGcMsApi.Parameter;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace CompMs.MsdialGcMsApi.Algorithm {
@@ -22,8 +24,42 @@ namespace CompMs.MsdialGcMsApi.Algorithm {
             var coreProcess = new PeakSpottingCore(_parameter);
             var chromatogramRange = new ChromatogramRange(_parameter.RetentionTimeBegin, _parameter.RetentionTimeEnd, ChromXType.RT, ChromXUnit.Min);
             var chromPeakFeatures = coreProcess.Execute3DFeatureDetection(analysisFile, provider, _parameter.NumThreads, token, reporter, chromatogramRange);
+            if (_parameter.MachineCategory == Common.Enum.MachineCategory.GCGCMS) {
+                var orderedPeaks = chromPeakFeatures.OrderBy(p => p.PeakFeature.PeakHeightTop).ToList();
+                var topPeaks = new Dictionary<ChromatogramPeakFeature, List<ChromatogramPeakFeature>>();
+                foreach (var peak in orderedPeaks) {
+                    var merged = false;
+                    foreach (var kvp in topPeaks) {
+                        var (topPeak, childs) = (kvp.Key, kvp.Value);
+                        if (IsNearBy(peak, topPeak)) {
+                            childs.Add(peak);
+                            merged = true;
+                            break;
+                        }
+                    }
+                    if (!merged) {
+                        topPeaks.Add(peak, [peak]);
+                    }
+                }
+
+                chromPeakFeatures = topPeaks.Select(peaks => {
+                    var topPeak = peaks.Key;
+                    topPeak.PeakFeature.PeakHeightTop = peaks.Value.Select(peak => peak.PeakFeature.PeakHeightTop).Sum();
+                    return topPeak;
+                }).OrderBy(p => p.MasterPeakID)
+                .Select((p, i) => {
+                    p.MasterPeakID = i;
+                    return p;
+                }).ToList();
+            }
             IsotopeEstimator.Process(chromPeakFeatures, _parameter, _iupacDB);
             return chromPeakFeatures;
+        }
+
+        private bool IsNearBy(IChromatogramPeakFeature p1, IChromatogramPeakFeature p2) {
+            return Math.Abs(p1.Mass - p2.Mass) <= _parameter.CentroidMs1Tolerance
+                && Math.Abs(p1.ChromXsTop.RT.Value - p2.ChromXsTop.RT.Value) <= _parameter.FirstColumnRetentionTimeTolerance
+                && Math.Abs(p1.ChromXsTop.RT.Value % _parameter.ModulationTime - p2.ChromXsTop.RT.Value % _parameter.ModulationTime) <= _parameter.RetentionTimeAlignmentTolerance;
         }
     }
 }
