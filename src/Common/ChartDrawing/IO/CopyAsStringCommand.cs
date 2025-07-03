36a0c5d0b030b61e869cb1ed71f8a374f2bd16dd
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-07-03 21:58:39 +0900

    Add CopyAsStringCommand for clipboard string operations
    Introduces the `CopyAsStringCommand` class in the `CompMs.Graphics.IO` namespace. This class implements the `ICommand` interface to facilitate copying data to the clipboard as a string. It features a constructor that accepts an asynchronous `saveFunc`, a `Format` property defaulting to `DataFormats.Text`, and implementations for `CanExecute` and `Execute` methods. The `Execute` method utilizes a `MemoryStream` to store data, invokes the `saveFunc`, and sets the clipboard data using the UTF-8 encoded string from the memory stream.

---
+++ src/Common/ChartDrawing/IO/CopyAsStringCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CompMs.Graphics.IO
+{
+    public class CopyAsStringCommand(Func<Stream, Task> saveFunc) : ICommand
+    {
+        private readonly Func<Stream, Task> _saveFunc = saveFunc;
+
+        public string Format { get; set; } = DataFormats.Text;
+
+#pragma warning disable 67
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore 67
+
+        public bool CanExecute(object parameter) => true;
+
+        public async void Execute(object parameter) {
+            using var memory = new MemoryStream();
+            await _saveFunc.Invoke(memory).ConfigureAwait(false);
+            await memory.FlushAsync();
+            Application.Current?.Dispatcher.Invoke(
+                () => Clipboard.SetData(Format, Encoding.UTF8.GetString(memory.ToArray()))
+            );
+        }
+    }
+}
