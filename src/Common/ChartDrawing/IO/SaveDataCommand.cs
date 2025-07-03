981fa7fb579985f6b18c166cc7c8537d2073abfa
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-07-03 21:58:21 +0900

    Add SaveDataCommand for file saving functionality
    Introduces the `SaveDataCommand` class in the `CompMs.Graphics.IO` namespace, implementing the `ICommand` interface. This class allows for saving data to a file using an asynchronous function, with support for file type filtering and a save file dialog. The `Execute` method manages the file stream, while `CanExecute` checks if a filter is set. Necessary using directives are also included.

---
+++ src/Common/ChartDrawing/IO/SaveDataCommand.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CompMs.Graphics.IO
+{
+    public class SaveDataCommand(Func<Stream, Task> saveFunc) : ICommand
+    {
+        private readonly Func<Stream, Task> _saveFunc = saveFunc;
+
+        public string Filter { get; set; } = string.Empty;
+
+#pragma warning disable 67
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore 67
+
+        public bool CanExecute(object parameter) {
+            return !string.IsNullOrEmpty(Filter);
+        }
+
+        public async void Execute(object parameter) {
+            if (TryGetSaveFilePath(out var path)) {
+                using var fs = File.Open(path, FileMode.Create);
+                await _saveFunc.Invoke(fs).ConfigureAwait(false);
+            }
+        }
+
+        private bool TryGetSaveFilePath(out string path) {
+            var sfd = new SaveFileDialog
+            {
+                Title = "Save file dialog.",
+                Filter = Filter,
+                RestoreDirectory = true,
+            };
+
+            var result = sfd.ShowDialog() ?? false;
+            path = sfd.FileName ?? string.Empty;
+            return result;
+        } 
+    }
+}
