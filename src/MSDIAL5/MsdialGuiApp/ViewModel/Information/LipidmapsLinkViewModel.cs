9343aaa298c86171dc78642b3c1b965c9264ae4c
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-07-08 11:20:52 +0900

    Add LipidmapsLinkViewModel class for data binding
    Introduces the `LipidmapsLinkViewModel` class in the
    `CompMs.App.Msdial.ViewModel.Information` namespace.
    This class manages a private `LipidmapsLinksModel` instance
    and exposes a read-only property `CurrentItems` as a
    `ReadOnlyReactivePropertySlim` for reactive data binding.

---
+++ src/MSDIAL5/MsdialGuiApp/ViewModel/Information/LipidmapsLinkViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CompMs.App.Msdial.Model.Information;
+using CompMs.CommonMVVM;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace CompMs.App.Msdial.ViewModel.Information;
+
+public sealed class LipidmapsLinkViewModel : ViewModelBase
+{
+    private readonly LipidmapsLinksModel _model;
+
+    public LipidmapsLinkViewModel(LipidmapsLinksModel model) {
+        _model = model;
+
+        CurrentItems = model.CurrentItems.ToReadOnlyReactivePropertySlim([]).AddTo(Disposables);
+    }
+
+    public ReadOnlyReactivePropertySlim<LipidmapsLinkItem[]> CurrentItems { get; }
+}
