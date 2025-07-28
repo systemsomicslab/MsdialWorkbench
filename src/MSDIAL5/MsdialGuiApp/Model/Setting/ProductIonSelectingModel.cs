c4a475e491b47ba50d846788a0699c7e59335f31
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-07-28 14:09:57 +0900

    Add retention time properties and observable methods
    Introduced `LeftRt` and `RightRt` properties to represent retention times with change notification support. Added `GetRtRangeAsObservable` to combine these properties into a single observable tuple. Implemented `SelectPeak` method to set retention times from a `ChromatogramPeakFeatureModel`. Updated `GetRequiredProductIonsAsObservable` to reference the `SettingIons` property.

--- src/MSDIAL5/MsdialGuiApp/Model/Setting/ProductIonSelectingModel.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/Setting/ProductIonSelectingModel.cs
@@ -1,4 +1,5 @@
-﻿using CompMs.CommonMVVM;
+﻿using CompMs.App.Msdial.Model.DataObj;
+using CompMs.CommonMVVM;
 using Reactive.Bindings.Extensions;
 using System;
 using System.Linq;
@@ -15,6 +16,28 @@ internal sealed class ProductIonSelectingModel : DisposableModelBase
         }
     }
     private SettingIon[]? _settingIons = [];
+
+    public double LeftRt {
+        get => _leftRt;
+        set => SetProperty(ref _leftRt, value);
+    }
+    private double _leftRt = 0d;
+
+    public double RightRt {
+        get => _rightRt;
+        set => SetProperty(ref _rightRt, value);
+    }
+    private double _rightRt = 0d;
+
+    public IObservable<(double Left, double Right)> GetRtRangeAsObservable() {
+        return new[] { this.ObserveProperty(m => m.LeftRt), this.ObserveProperty(m => m.RightRt) }
+            .CombineLatest(range => (Left: range[0], Right: range[1]));
+    }
+
+    public void SelectPeak(ChromatogramPeakFeatureModel peak) {
+        LeftRt = peak.ChromXLeftValue;
+        RightRt = peak.ChromXRightValue;
+    }
 
     public IObservable<double[]> GetRequiredProductIonsAsObservable() {
         return this.ObserveProperty(m => m.SettingIons)
