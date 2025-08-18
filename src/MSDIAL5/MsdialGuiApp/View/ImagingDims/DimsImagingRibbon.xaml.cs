b3b27927218664874c19899bd657fe6b1b8a9fb6
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-08-18 16:46:27 +0900

    Add DimsImagingRibbon

---
+++ src/MSDIAL5/MsdialGuiApp/View/ImagingDims/DimsImagingRibbon.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace CompMs.App.Msdial.View.ImagingDims;
+
+/// <summary>
+/// Interaction logic for DimsImagingRibbon.xaml
+/// </summary>
+public partial class DimsImagingRibbon : UserControl
+{
+    public DimsImagingRibbon() {
+        InitializeComponent();
+    }
+}
