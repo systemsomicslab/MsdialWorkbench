a22ad8790a928b1f628357d6892fbe388c621482
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-08-20 15:25:07 +0900

    Implement AlignmentMztabMExportModel

--- src/MSDIAL5/MsdialGuiApp/Model/Export/AlignmentMztabMExportModel.cs
+++ src/MSDIAL5/MsdialGuiApp/Model/Export/AlignmentMztabMExportModel.cs
@@ -4,18 +4,30 @@ using CompMs.MsdialCore.DataObj;
 using CompMs.MsdialCore.Export;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CompMs.App.Msdial.Model.Export;
 
 internal sealed class AlignmentMztabMExportModel : BindableBase, IAlignmentResultExportModel
 {
-    public AlignmentMztabMExportModel(DataBaseStorage databaseStorage) {
-        var exporter = new MztabFormatExporter(databaseStorage);
-        
+    private readonly MztabFormatExporter _exporter;
+    private readonly AnalysisFileBeanModelCollection _fileModelCollection;
+    private readonly AlignmentPeakSpotSupplyer _peakSpotSupplyer;
+    private readonly List<ExportType> _types;
+    private readonly AccessPeakMetaModel _accessPeakMeta;
+
+    public AlignmentMztabMExportModel(AnalysisFileBeanModelCollection fileModelCollection, AlignmentPeakSpotSupplyer peakSpotSupplyer, DataBaseStorage databaseStorage, IEnumerable<ExportType> types, AccessPeakMetaModel accessPeakMeta) {
+        _exporter = new MztabFormatExporter(databaseStorage);
+        _fileModelCollection = fileModelCollection;
+        _peakSpotSupplyer = peakSpotSupplyer;
+        _types = [.. types];
+        _accessPeakMeta = accessPeakMeta;
+        Types = _types.AsReadOnly();
     }
+
+    public ReadOnlyCollection<ExportType> Types { get; }
 
     public bool IsSelected {
         get => _isSelected;
@@ -24,10 +36,35 @@ internal sealed class AlignmentMztabMExportModel : BindableBase, IAlignmentResul
     private bool _isSelected = false;
 
     public int CountExportFiles(AlignmentFileBeanModel alignmentFile) {
-        return IsSelected ? 1 : 0;
+        return IsSelected ? _types.Count(t => t.ShouldExport) : 0;
     }
 
     public void Export(AlignmentFileBeanModel alignmentFile, string exportDirectory, Action<string> notification) {
-        throw new NotImplementedException();
+        if (!IsSelected) {
+            return;
+        }
+        var filenameTemplate = $"{{0}}_{alignmentFile.FileName}_{DateTime.Now:yyyy_MM_dd_HH_mm_ss}.mzTab.txt";
+        var spots = _peakSpotSupplyer.Supply(alignmentFile, default); // TODO: cancellation
+        var msdecs = alignmentFile.LoadMSDecResults();
+        var accessor = _accessPeakMeta.GetAccessor();
+
+        foreach (var type in _types.Where(t => t.ShouldExport)) {
+            var filename = string.Format(filenameTemplate, type.TargetLabel);
+            var filepath = Path.Combine(exportDirectory, filename);
+
+            notification?.Invoke($"Export {filename}");
+
+            using var stream = File.Open(filepath, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            _exporter.MztabFormatExporterCore(
+                stream,
+                spots,
+                msdecs,
+                [.. _fileModelCollection.AnalysisFiles.Select(f => f.File)],
+                accessor,
+                type.QuantValueAccessor,
+                type.Stats,
+                filename
+            );
+        }
     }
 }
