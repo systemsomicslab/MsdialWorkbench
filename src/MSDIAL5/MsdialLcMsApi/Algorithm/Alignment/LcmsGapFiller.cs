3ec40b4ea268ed13801fcc2ae7bd2350ed6185b4
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-09-10 14:02:35 +0900

    Change LcmsGapFiller code style into file scoped namespace

--- src/MSDIAL5/MsdialLcMsApi/Algorithm/Alignment/LcmsGapFiller.cs
+++ src/MSDIAL5/MsdialLcMsApi/Algorithm/Alignment/LcmsGapFiller.cs
@@ -9,42 +9,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace CompMs.MsdialLcMsApi.Algorithm.Alignment
+namespace CompMs.MsdialLcMsApi.Algorithm.Alignment;
+
+public class LcmsGapFiller : GapFiller, IGapFiller
 {
-    public class LcmsGapFiller : GapFiller, IGapFiller
-    {
-        private readonly double mzTol, rtTol;
-        private readonly IonMode ionMode;
-        protected override double AxTol => rtTol;
+    private readonly double mzTol, rtTol;
+    private readonly IonMode ionMode;
+    protected override double AxTol => rtTol;
 
-        public LcmsGapFiller(double rtTol, double mzTol, IonMode ionMode, SmoothingMethod smoothingMethod, int smoothingLevel, bool isForceInsert)
-            : base(smoothingMethod, smoothingLevel, isForceInsert) {
-            this.rtTol = rtTol;
-            this.mzTol = mzTol;
-            this.ionMode = ionMode;
-        }
+    public LcmsGapFiller(double rtTol, double mzTol, IonMode ionMode, SmoothingMethod smoothingMethod, int smoothingLevel, bool isForceInsert)
+        : base(smoothingMethod, smoothingLevel, isForceInsert) {
+        this.rtTol = rtTol;
+        this.mzTol = mzTol;
+        this.ionMode = ionMode;
+    }
 
-        public LcmsGapFiller(MsdialLcmsParameter param)
-            : this(param.RetentionTimeAlignmentTolerance, param.CentroidMs1Tolerance, param.IonMode,
-                  param.SmoothingMethod, param.SmoothingLevel, param.IsForceInsertForGapFilling) { }
+    public LcmsGapFiller(MsdialLcmsParameter param)
+        : this(param.RetentionTimeAlignmentTolerance, param.CentroidMs1Tolerance, param.IonMode,
+              param.SmoothingMethod, param.SmoothingLevel, param.IsForceInsertForGapFilling) { }
 
-        protected override ChromXs GetCenter(AlignmentSpotProperty spot, IEnumerable<AlignmentChromPeakFeature> peaks) {
-            return new ChromXs(peaks.Average(peak => peak.ChromXsTop.RT.Value), ChromXType.RT, ChromXUnit.Min)
-            {
-                Mz = new MzValue(peaks.Argmax(peak => peak.PeakHeightTop).Mass),
-            };
-        }
+    protected override ChromXs GetCenter(AlignmentSpotProperty spot, IEnumerable<AlignmentChromPeakFeature> peaks) {
+        return new ChromXs(peaks.Average(peak => peak.ChromXsTop.RT.Value), ChromXType.RT, ChromXUnit.Min)
+        {
+            Mz = new MzValue(peaks.Argmax(peak => peak.PeakHeightTop).Mass),
+        };
+    }
 
-        protected override double GetPeakWidth(IEnumerable<AlignmentChromPeakFeature> peaks) {
-            return peaks.Max(peak => peak.PeakWidth(ChromXType.RT));
-        }
+    protected override double GetPeakWidth(IEnumerable<AlignmentChromPeakFeature> peaks) {
+        return peaks.Max(peak => peak.PeakWidth(ChromXType.RT));
+    }
 
-        protected override List<ChromatogramPeak> GetPeaks(Ms1Spectra ms1Spectra, RawSpectra rawSpectra, IReadOnlyList<RawSpectrum> spectrum, ChromXs center, double peakWidth, int fileID, SmoothingMethod smoothingMethod, int smoothingLevel) {
-            peakWidth = Math.Max(peakWidth, 0.2f);
+    protected override List<ChromatogramPeak> GetPeaks(Ms1Spectra ms1Spectra, RawSpectra rawSpectra, IReadOnlyList<RawSpectrum> spectrum, ChromXs center, double peakWidth, int fileID, SmoothingMethod smoothingMethod, int smoothingLevel) {
+        peakWidth = Math.Max(peakWidth, 0.2f);
 
-            var chromatogramRange = new ChromatogramRange(center.RT.Value - peakWidth * 1.5, center.RT.Value + peakWidth * 1.5, ChromXType.RT, ChromXUnit.Min);
-            var peaklist = ms1Spectra.GetMs1ExtractedChromatogram(center.Mz.Value, this.mzTol, chromatogramRange);
-            return peaklist.ChromatogramSmoothing(smoothingMethod, smoothingLevel).AsPeakArray();
-        }
+        var chromatogramRange = new ChromatogramRange(center.RT.Value - peakWidth * 1.5, center.RT.Value + peakWidth * 1.5, ChromXType.RT, ChromXUnit.Min);
+        var peaklist = ms1Spectra.GetMs1ExtractedChromatogram(center.Mz.Value, this.mzTol, chromatogramRange);
+        return peaklist.ChromatogramSmoothing(smoothingMethod, smoothingLevel).AsPeakArray();
     }
 }
