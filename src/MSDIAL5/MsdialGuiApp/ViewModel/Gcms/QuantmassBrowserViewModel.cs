94afb8d722c9da9a39085c10b7157977e7bec4a5
Yuki Matsuzawa <fr3701@go.tuat.ac.jp>
2025-10-23 16:08:06 +0900

    Add QuantmassBrowserViewModel

---
+++ src/MSDIAL5/MsdialGuiApp/ViewModel/Gcms/QuantmassBrowserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CompMs.App.Msdial.Model.DataObj;
+using CompMs.App.Msdial.Model.Gcms;
+using CompMs.CommonMVVM;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System.Collections.ObjectModel;
+
+namespace CompMs.App.Msdial.ViewModel.Gcms;
+
+public sealed class QuantmassBrowserViewModel : ViewModelBase
+{
+    private readonly QuantmassBrowserModel _model;
+
+    public QuantmassBrowserViewModel(QuantmassBrowserModel model) {
+        SelectedSpot = model.ToReactivePropertySlimAsSynchronized(m => m.SelectedSpot).AddTo(Disposables);
+        _model = model;
+    }
+
+    public ReadOnlyObservableCollection<AlignmentSpotPropertyModel> Spots => _model.Spots.Items;
+
+    public ReactivePropertySlim<AlignmentSpotPropertyModel?> SelectedSpot { get; }
+
+}
