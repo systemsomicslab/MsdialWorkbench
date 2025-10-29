b4eb7f3984a7b64dc707bbf4a7e7d8fad2c3a051
YukiMatsuzawa <122433968+YukiMatsuzawa@users.noreply.github.com>
2025-10-29 21:10:56 +0900

    Enable automatic graph updates when values change (#655)
    * Add ScatterControl test
    
    * Update ScatterControlSlim
    
    * Add NotifiableDataPoint
    
    * Update ChartDrawingUiTest csproj file
    
    * Add ExpressionHelper.GetPropertyGetterExpressions method
    
    * Add new overload of ExpressionHelper.GetConvertToAxisValueExpression method
    
    * Add ExpressionHelper.Identity method
    
    * Update ExpressionHelper
    
    * Update ExpressionHelper
    
    * Add PropertiesAccessor
    
    * Update NotifiableDataPoint
    
    * Fix ScatterControlSlimTest1
    
    * Rename PropertiesAccessor
    
    * Add IPropertiesAccessor
    
    * Add ValueHolder class
    
    * Update DynamicPropertiesAccessor
    
    * Update NotifiableDataPoint
    
    * Add test code for Annotator
    
    * Update Annotator to update the label position when the source property changed
    
    * Update Annotator test
    
    * Add IgnoreAccessor
    
    * Fix the peak spot labels in gcms project
    
    * Update PointBrush property registration in ScatterControlSlim
    
    Modified the registration of the `PointBrush` dependency property to include `FrameworkPropertyMetadataOptions.AffectsRender`. This change ensures that updates to the `PointBrush` will trigger a re-render of the control.
    
    * Fix test

---
+++ src/Common/ChartDrawing/Helper/IPropertiesAccessor.cs
@@ -0,0 +1,13 @@
+﻿using CompMs.Graphics.Core.Base;
+
+namespace CompMs.Graphics.Helper
+{
+    internal interface IPropertiesAccessor
+    {
+        string[] Properties { get; }
+
+        object Apply(int depth, object item);
+        object? Apply(object item);
+        AxisValue ConvertToAxisValue(object item, IAxisManager axis);
+    }
+}
---
+++ src/Common/ChartDrawing/Helper/TypedPropertiesAccessor.cs
@@ -0,0 +1,41 @@
+﻿using CompMs.Graphics.Core.Base;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CompMs.Graphics.Helper;
+
+internal sealed class TypedPropertiesAccessor : IPropertiesAccessor
+{
+    private readonly LambdaExpression[] _expressions;
+    private readonly Expression<Func<object, IAxisManager, AxisValue>> _getAxisValueExpression;
+
+    public TypedPropertiesAccessor(string property, Type type) {
+        Properties = ExpressionHelper.GetProperties(property);
+        _expressions = ExpressionHelper.GetPropertyGetterFromSourceExpressions(type, property);
+        _getAxisValueExpression = ExpressionHelper.GetConvertToAxisValueExpression(_expressions.LastOrDefault() ?? ExpressionHelper.Identity(type));
+    }
+
+    public string[] Properties { get; }
+
+    private Delegate[] Delegates => _delegates ??= _expressions.Select(exp => exp.Compile()).ToArray();
+    private Delegate[]? _delegates;
+
+    public object? Apply(object item) {
+        return Delegates.LastOrDefault()?.DynamicInvoke(item);
+    }
+
+    public object Apply(int depth, object item) {
+        if (Properties.Length <= depth) {
+            throw new ArgumentOutOfRangeException(nameof(depth));
+        }
+        return Delegates[depth].DynamicInvoke(item);
+    }
+
+    private Func<object, IAxisManager, AxisValue> GetAxisValue => _getAxisValue ??= _getAxisValueExpression.Compile();
+    private Func<object, IAxisManager, AxisValue>? _getAxisValue;
+
+    public AxisValue ConvertToAxisValue(object item, IAxisManager axis) {
+        return GetAxisValue(item, axis);
+    }
+}
