5cb02939086beac1e7d6108666b76c92c85fd259
Yuki Matsuzawa <matsuzawa.yuki.8k@kyoto-u.ac.jp>
2026-07-13 18:34:40 +0900

    Update LargeListMessagePack

--- src/Common/CommonStandard/MessagePack/LargeListMessagePack.cs
+++ src/Common/CommonStandard/MessagePack/LargeListMessagePack.cs
@@ -27,36 +27,27 @@ namespace CompMs.Common.MessagePack {
         }
 
         public static List<T> Deserialize<T>(Stream stream) {
-            using (var memory = new MemoryStream()) {
-                stream.CopyTo(memory);
-                var reader = new MessagePackReader(new ReadOnlyMemory<byte>(memory.GetBuffer(), 0, (int)memory.Length));
-                return DeserializeCore<T>(ref reader, MessagePackSerializerOptions.Standard);
-            }
+            return DeserializeCore<T>(stream, MessagePackSerializerOptions.Standard);
         }
 
         public static IEnumerable<List<T>> DeserializeIncremental<T>(Stream stream) {
-            using (var memory = new MemoryStream()) {
-                stream.CopyTo(memory);
-                var reader = new MessagePackReader(new ReadOnlyMemory<byte>(memory.GetBuffer(), 0, (int)memory.Length));
-                foreach (var result in DeserializeIncrementalCore<T>(ref reader, MessagePackSerializerOptions.Standard)) {
-                    yield return result;
-                }
+            foreach (var result in DeserializeIncrementalCore<T>(stream, MessagePackSerializerOptions.Standard)) {
+                yield return result;
             }
         }
 
         public static T DeserializeAt<T>(Stream stream, int index) {
-            using (var memory = new MemoryStream()) {
-                stream.CopyTo(memory);
-                var reader = new MessagePackReader(new ReadOnlyMemory<byte>(memory.GetBuffer(), 0, (int)memory.Length));
-                return DeserializeAt<T>(ref reader, MessagePackSerializerOptions.Standard, index);
-            }
+            return DeserializeAt<T>(stream, MessagePackSerializerOptions.Standard, index);
         }
 
-        public static T DeserializeAt<T>(ref MessagePackReader reader, MessagePackSerializerOptions options, int index) {
-            while (!reader.End) {
-                var success = TryDeserializeAtOrSkip<T>(ref reader, options, index, out var result, out int skipArraySize);
+        public static T DeserializeAt<T>(Stream stream, MessagePackSerializerOptions options, int index) {
+            while (index >= 0) {
+                var success = TryDeserializeAtOrSkip<T>(stream, options, index, out var result, out int skipArraySize);
                 if (success) {
                     return result;
+                }
+                else if (skipArraySize == 0) {
+                    break;
                 }
                 index -= skipArraySize;
             }
@@ -64,64 +55,74 @@ namespace CompMs.Common.MessagePack {
             return default;
         }
 
-        private static List<T> DeserializeCore<T>(ref MessagePackReader reader, MessagePackSerializerOptions options) {
-            var res = new List<List<T>>();
-            while (!reader.End) {
-                var tmp = DeserializeEach<T>(ref reader, options);
-                if (tmp != null && tmp.Count > 0) {
-                    res.Add(tmp);
+        private static List<T> DeserializeCore<T>(Stream stream, MessagePackSerializerOptions options) {
+            var res = new List<T>();
+            while (true) {
+                var tmp = DeserializeEach<T>(stream, options);
+                if (tmp is null) {
+                    break;
                 }
+                res.AddRange(tmp);
             }
-
-            return res.SelectMany(r => r).ToList();
+            return res;
         }
 
-        private static IEnumerable<List<T>> DeserializeIncrementalCore<T>(ref MessagePackReader reader, MessagePackSerializerOptions options) {
-            var results = new List<List<T>>();
-            while (!reader.End) {
-                results.Add(DeserializeEach<T>(ref reader, options));
+        private static IEnumerable<List<T>> DeserializeIncrementalCore<T>(Stream stream, MessagePackSerializerOptions options) {
+            while (true) {
+                var data = DeserializeEach<T>(stream, options);
+                if (data is null) {
+                    yield break;
+                }
+                yield return data;
             }
-
-            return results;
         }
 
-        private static List<T> DeserializeEach<T>(ref MessagePackReader reader, MessagePackSerializerOptions options) {
+        private static List<T>? DeserializeEach<T>(Stream stream, MessagePackSerializerOptions options) {
             var resolver = CompositeResolver.Create(
                 [new DeserializedDataContainer<T>.DeserializedDataContainerFormatter(),],
                 [StandardResolver.Instance]);
-            if (!reader.End) {
-                return MessagePackSerializer.Deserialize<DeserializedDataContainer<T>>(ref reader, options.WithResolver(resolver).WithCompression(MessagePackCompression.Lz4BlockArray)).Data;
+            try {
+                return MessagePackSerializer.Deserialize<DeserializedDataContainer<T>>(stream, options.WithResolver(resolver).WithCompression(MessagePackCompression.Lz4BlockArray))?.Data;
             }
-
-            return [];
+            catch (MessagePackSerializationException) {
+                return default;
+            }
         }
 
-        private static bool TryDeserializeAtOrSkip<T>(ref MessagePackReader reader, MessagePackSerializerOptions options, int index, out T result, out int skipArraySize) {
+        private static bool TryDeserializeAtOrSkip<T>(Stream stream, MessagePackSerializerOptions options, int index, out T? result, out int skipArraySize) {
             var resolver = CompositeResolver.Create(
                 [DeserializedDataContainer<T>.CreateFormatter(index),],
                 [StandardResolver.Instance]);
             var newOptions = options.WithCompression(MessagePackCompression.Lz4BlockArray).WithResolver(resolver);
-            var deserialized = MessagePackSerializer.Deserialize<DeserializedDataContainer<T>>(ref reader, newOptions);
-            result = deserialized.Data is null ? default : deserialized.Data.FirstOrDefault();
-            skipArraySize = deserialized.Length;
-            return result != null;
+            try {
+                var deserialized = MessagePackSerializer.Deserialize<DeserializedDataContainer<T>>(stream, newOptions);
+                result = deserialized.Data is null ? default : deserialized.Data.FirstOrDefault();
+                skipArraySize = deserialized.Length;
+                return result != null;
+            }
+            catch (MessagePackSerializationException) {
+                result = default;
+                skipArraySize = 0;
+                return false;
+            }
         }
 
+        [MessagePackFormatter(typeof(DataContainerFormatter<>))]
         internal sealed class SerializingDataContainer<T> {
             public sbyte VersionCode { get; } = 2;
             public Memory<T> Data { get; set; }
+        }
 
-            internal class DataContainerFormatter : IMessagePackFormatter<SerializingDataContainer<T>?> {
-                public SerializingDataContainer<T> Deserialize(ref MessagePackReader reader, MessagePackSerializerOptions options) {
-                    throw new NotSupportedException();
-                }
+        internal class DataContainerFormatter<T> : IMessagePackFormatter<SerializingDataContainer<T>?> {
+            public SerializingDataContainer<T> Deserialize(ref MessagePackReader reader, MessagePackSerializerOptions options) {
+                throw new NotSupportedException();
+            }
 
-                public void Serialize(ref MessagePackWriter writer, SerializingDataContainer<T> value, MessagePackSerializerOptions options) {
-                    writer.WriteInt8(value.VersionCode);
-                    writer.WriteArrayHeader(value.Data.Length);
-                    for (int i = 0; i < value.Data.Length; i++) {
-                        MessagePackSerializer.Serialize(ref writer, value.Data.Span[i], options);
-                    }
+            public void Serialize(ref MessagePackWriter writer, SerializingDataContainer<T> value, MessagePackSerializerOptions options) {
+                writer.WriteInt8(value.VersionCode);
+                writer.WriteArrayHeader(value.Data.Length);
+                for (int i = 0; i < value.Data.Length; i++) {
+                    MessagePackSerializer.Serialize(ref writer, value.Data.Span[i], options);
                 }
             }
         }
@@ -132,7 +133,7 @@ namespace CompMs.Common.MessagePack {
 
             public static IMessagePackFormatter<DeserializedDataContainer<T>> CreateFormatter(int index) => new SpecificDataFormatter(index);
 
-#pragma warning disable MsgPack009
+#pragma warning disable MsgPack009, MsgPack010
             internal class DeserializedDataContainerFormatter : IMessagePackFormatter<DeserializedDataContainer<T>?> {
                 public DeserializedDataContainer<T> Deserialize(ref MessagePackReader reader, MessagePackSerializerOptions options) {
                     int length;
@@ -191,7 +192,7 @@ namespace CompMs.Common.MessagePack {
                     throw new NotSupportedException();
                 }
             }
-#pragma warning restore MsgPack009
+#pragma warning restore MsgPack009, MsgPack010
         }
     }
 }
