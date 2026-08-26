1acc10688e42ed9ed2e0c5069c641193c2cef387
Hiroshi Tsugawa <tsugawa.hiroshi.2p@kyoto-u.ac.jp>
2026-08-26 12:15:42 +0900

    Add shared raw data metadata contract (#776)
    Co-authored-by: DESKTOP-382ETUR\Hiroshi Tsugawa <htsugawa@go.tuat.ac.jp>

--- src/Common/CommonStandard/DataObj/RawMeasurement.cs
+++ src/Common/CommonStandard/DataObj/RawMeasurement.cs
@@ -70,6 +70,7 @@ namespace CompMs.Common.DataObj {
         public List<double> CollisionEnergyTargets { get; set; }
         public MaldiFrameLaserInfo MaldiFrameLaserInfo { get; set; }
         public List<MaldiFrameInfo> MaldiFrames { get; set; }
+        public RawDataMetadata Metadata { get; set; }
 
         public RawMeasurement()
         {
@@ -83,6 +84,7 @@ namespace CompMs.Common.DataObj {
             MaldiFrameLaserInfo = new MaldiFrameLaserInfo();
 
             MaldiFrames = new List<MaldiFrameInfo>();
+            Metadata = new RawDataMetadata();
         }
     }
 
